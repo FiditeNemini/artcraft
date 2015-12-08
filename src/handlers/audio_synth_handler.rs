795074d1125414c8a92278f92f1b87753a9944f3
Brandon Thomas <echelon@gmail.com>
2015-12-08 03:33:27 -0500

    audio synth endpoint works

--- src/handlers/audio_synth_handler.rs
+++ src/handlers/audio_synth_handler.rs
@@ -1,5 +1,6 @@
 // Copyright (c) 2015 Brandon Thomas <bt@brand.io>
 
+use hound::{WavReader, WavSpec, WavWriter};
 use iron::Handler;
 use iron::mime::Mime;
 use iron::prelude::*;
@@ -10,6 +11,9 @@ use urlencoded::UrlEncodedQuery;
 
 use std::fs::File;
 use std::fs;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Cursor;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
@@ -43,11 +47,10 @@ impl Handler for AudioSynthHandler {
       },
     };
 
-    let words = split_sentence(sentence);
+    let result = self.create_audio(sentence);
+    let mime_type = "audio/wav".parse::<Mime>().unwrap();
 
-    /*let words = self.list_files();
-    let response = json::encode(&words).unwrap();*/
-    Ok(Response::with((status::Ok, "todo")))
+    Ok(Response::with((mime_type, status::Ok, result)))
   }
 }
 
@@ -57,10 +60,73 @@ impl AudioSynthHandler {
   }
 
   // TODO: Return errors.
-  /// Return a list of words from the audio files in the directory.
-  fn list_files(&self) -> Vec<String> {
-    let mut words = Vec::new();
-    words
+  /// Create audio from the sentence.
+  fn create_audio(&self, sentence: &str) -> Vec<u8> {
+    let words = split_sentence(sentence);
+
+    // Note: Keeping a list of buffered file readers is stupid and is simply 
+    // being done for this example. I'll create a multithreaded shared LRU cache
+    // that reads from the disk and uses the dictionary word as the lookup key.
+    let mut file_readers : Vec<WavReader<BufReader<File>>> = Vec::new();
+
+    for word in words.iter() {
+      let filename = self.get_file_path(word);
+      println!("Opening file : {}", filename.to_str().unwrap());
+      let reader = WavReader::open(filename).unwrap();
+      file_readers.push(reader);
+    }
+
+    let mut all_samples : Vec<i16> = Vec::new();
+
+    for mut reader in file_readers {
+      let samples = reader.samples::<i16>();
+      for sample in samples {
+        all_samples.push(sample.unwrap());
+      }
+    }
+
+    let spec = self.get_spec(&words[0]);
+
+    self.write_buffer(&spec, all_samples)
+  }
+
+  fn get_file_path(&self, word: &str) -> PathBuf {
+    let sound_directory = self.directory.as_path();
+    sound_directory.join(format!("{}.wav", word))
+  }
+
+  fn get_spec(&self, word: &str) -> WavSpec {
+    let filename = self.get_file_path(word);
+    let reader = WavReader::open(filename).unwrap();
+    reader.spec()
+  }
+
+  fn write_buffer(&self, spec: &WavSpec, samples: Vec<i16>) -> Vec<u8> {
+    let mut bytes : Vec<u8> = Vec::new();
+    let mut seek : Cursor<Vec<u8>> = Cursor::new(bytes);
+    let mut buffer = BufWriter::new(seek);
+
+    {
+      let mut writer = WavWriter::new(&mut buffer, *spec);
+      for s in samples {
+        writer.write_sample(s).unwrap();
+      }
+      writer.finalize().unwrap(); // TODO: Error
+    }
+
+    match buffer.into_inner() {
+      Err(_) => { Vec::new() }, // TODO: Error
+      Ok(r) => { r.get_ref().to_vec() },
+    }
   }
 }
+
+/*fn write_file(filename: &str, spec: &WavSpec, samples: Vec<i16>) {
+  let mut writer = WavWriter::create(filename, *spec).unwrap();
+  println!("Writing file : {}", filename);
+
+  for s in samples {
+    writer.write_sample(s).unwrap();
+  }
+}*/
 
