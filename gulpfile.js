aacff3c1ce7562c78cf7a94964588e3b0dd24426
Brandon Thomas <echelon@gmail.com>
2016-01-24 14:17:55 -0500

    Gulp/Bower workflow for Javascript

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    filter = require('gulp-filter'),
+    mainBowerFiles = require('main-bower-files'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch');
+
+var buildLibraries = function() {
+  // See https://gist.github.com/ktmud/9384509
+  return gulp.src(mainBowerFiles())
+    .pipe(filter('**/*.js')) // filter out CSS (bootstrap, etc.)
+    .pipe(concat('libraries.js'))
+    .pipe(gulp.dest('web/'));
+}
+
+var buildCss = function() {
+  return gulp.src('src/web/css/*.css')
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('web/'));
+}
+
+var buildJs = function() {
+  return gulp.src('src/web/js/*.js')
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('web/'));
+}
+
+gulp.task('bower', buildLibraries);
+gulp.task('js', buildJs);
+gulp.task('css', buildCss);
+
+gulp.task('watch', function() {
+  gulp.watch(['bower.json', '.bowerrc'], ['bower']);
+    watch([ 'src/web/js/*.js' ], buildJs);
+    watch([ 'src/web/css/*.css' ], buildCss);
+});
+
+gulp.task('build-development', ['bower', 'js', 'css']);
+gulp.task('build-production', ['bower', 'js', 'css', 'compress']); // TODO: Compress
+
+gulp.task('default', ['build-development']);
+
