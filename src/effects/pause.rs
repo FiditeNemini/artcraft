6e7afc677a3fedd11bf706af135cd592062dd5de
Brandon Thomas <echelon@gmail.com>
2016-09-10 17:56:30 -0400

    Forgot to check this in awhile ago

---
+++ src/effects/pause.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2016 Brandon Thomas <bt@brand.io, echelon@gmail.com>
+
+/// Create a waveform pause of the desired length.
+pub fn generate_pause(length: u16) -> Vec<i16> {
+  let mut sound = Vec::new();
+
+  let len = length * 2; // number of channels
+  for i in 0..len { sound.push(0); }
+
+  sound
+}
+
