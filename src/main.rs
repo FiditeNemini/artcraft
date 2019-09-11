73afdc7a988c41de7802499bdd13119cd25bca5f
Brandon Thomas <echelon@gmail.com>
2019-09-11 01:13:24 -0400

    add threshold for noise

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,14 @@ fn main() {
   record().unwrap();
 }
 
+const THRESHOLD : i16 = 1500;
+
 fn record() -> Result<(), AudioError> {
   // Connect to the speaker and microphone systems.
   let mut mic = MicrophoneSystem::new(SampleRate::Studio)?;
   let mut speaker = SpeakerSystem::new(SampleRate::Normal)?;
 
-  let mut buffer = VecDeque::with_capacity(30_000);
+  let mut buffer = VecDeque::with_capacity(2048);
 
 /*  // Convert a signal to its RMS.
 let signal = signal::rate(44_100.0).const_hz(440.0).sine();;
@@ -30,9 +32,19 @@ let mut rms_signal = signal.rms(ring_buffer);*/
 
   loop {
     // Record some sound.
-    mic.record(&mut |_whichmic, l, r| {
+    mic.record(&mut |_whichmic, mut l, mut r| {
+      //println!("Enqueue");
+      /*if buffer.len() < 20480 {
+        buffer.push_back((l, r));
+      }*/
+      println!("Left: {}, Right: {}", l, r);
+      if l < THRESHOLD && l > -THRESHOLD {
+        l = l / 2;
+      }
+      if r < THRESHOLD && r > -THRESHOLD {
+        r = r / 2;
+      }
       buffer.push_back((l, r));
-      //buffer.push_back((l, r));
     });
 
     let mut last_rsample = None;
@@ -40,52 +52,28 @@ let mut rms_signal = signal.rms(ring_buffer);*/
     
     // Play that sound.
     speaker.play(&mut || {
-
-      let mut sample = None;
-      let mut count = 0;
-      while sample.is_none() {
-        sample = buffer.pop_front();
-        //if let Some((lsample, rsample)) = buffer.pop_front() {
-        //let rsample = rsample.saturating_mul(2);
-        count += 1;
-        if count > 10 {
-          break;
-        }
-      }
-
-      match sample {
-        Some((lsample, rsample)) => {
-          last_rsample = Some(rsample);
-          last_lsample = Some(lsample);
-          AudioSample::stereo(lsample, rsample)
-        },
-        None => {
-          //println!("NOO");
-          //AudioSample::stereo(0,0)
-          let rsample = last_rsample.unwrap_or(0);
-          let lsample = last_lsample.unwrap_or(0);
-          AudioSample::stereo(lsample, rsample)
-        }
-      }
-
-      /*//println!("Buffer size {}", buffer.len());
       if let Some((lsample, rsample)) = buffer.pop_front() {
         //let rsample = rsample.saturating_mul(2);
         //let lsample = lsample.saturating_mul(2);
+        last_rsample = Some(rsample);
+        last_lsample = Some(lsample);
         /*let swap = rsample;
         let rsample = lsample;
         let lsample = swap;*/
-        AudioSample::stereo(lsample, rsample)
+        let mono = lsample;
+
+        //let mono = mono.saturating_mul(2);
+
+        AudioSample::stereo(mono, mono)
       } else {
         // Play silence if not enough has been recorded yet.
-        let rsample = last_rsample.unwrap_or(0);
-        let lsample = last_lsample.unwrap_or(0);
-        /*let rsample = 0;
-        let lsample = 0;*/
-        println!("zero {},{}", rsample, lsample);
+        //let rsample = last_rsample.unwrap_or(0);
+        //let lsample = last_lsample.unwrap_or(0);
+        let rsample = 0;
+        let lsample = 0;
+        println!("No data!");
         AudioSample::stereo(rsample, lsample)
-      }*/
-
+      }
     });
   }
 }
