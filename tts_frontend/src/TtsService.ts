7b2eff1387c18f675d63d287f51e5754df02a46d
Brandon Thomas <echelon@gmail.com>
2020-05-10 22:42:07 -0400

    uncommitted changes to frontend

---
+++ tts_frontend/src/TtsService.ts
@@ -0,0 +1,49 @@
+import Howl from 'howler';
+
+/// Requests to the backend.
+class TtsRequest {
+  text: String
+  speaker: String
+
+  constructor(text: String) {
+    this.text = text;
+    // LJSpeech dataset from Keith Ito & LibriVox, spoken by Linda Johnson.
+    this.speaker = 'linda'; 
+  }
+}
+
+class TtsResponse {
+  text: string
+  howl: Howl
+
+  constructor(text: string, howl: Howl) {
+    this.text = text;
+    this.howl = howl;
+  }
+}
+
+let makeTtsRequest = (sentence: string): Promise<TtsResponse> => {
+  const request = new TtsRequest(sentence);
+  const url = `/tts`;
+
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(request),
+  })
+  .then(res => res.blob())
+  .then(blob => {
+    const url = window.URL.createObjectURL(blob);
+    const howlSound = new Howl.Howl({
+      src: [url],
+      format: 'wav',
+    });
+
+    return new TtsResponse(sentence, howlSound);
+  });
+}
+
+export {TtsRequest, TtsResponse, makeTtsRequest};
