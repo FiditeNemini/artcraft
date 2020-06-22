0970e0787919b960a4185ce29fea1285f4caf808
Brandon Thomas <echelon@gmail.com>
2020-06-22 05:56:44 -0400

    add spectrograms (this isn't pretty)

--- vocodes/src/modes/speak/Form.tsx
+++ vocodes/src/modes/speak/Form.tsx
@@ -2,7 +2,20 @@ import Howl from 'howler';
 import React from 'react';
 import { SpeakRequest } from './SpeakRequest';
 import { Speaker } from '../../Speakers';
+import { createColorMap, linearScale } from "@colormap/core";
+import { VIRIDIS, CIVIDIS, PLASMA, INFERNO, MAGMA, BLACK_WHITE,  } from "@colormap/presets";
 
+class Spectrogram {
+  pixels: Uint8ClampedArray;
+  width: number;
+  height: number;
+
+  constructor(pixels: Uint8ClampedArray, width: number, height: number) {
+    this.pixels = pixels;
+    this.width = width;
+    this.height = height;
+  }
+}
 
 interface Props {
   currentSpeaker: Speaker,
@@ -18,6 +31,7 @@ interface Props {
 interface State {
   text: string,
   howl?: Howl,
+  spectrogram?: Spectrogram,
 }
 
 class Form extends React.Component<Props, State> {
@@ -31,16 +45,12 @@ class Form extends React.Component<Props, State> {
     };
   }
 
-  componentDidMount() {
-    this.textarea?.focus();
-  }
-
   public speak(sentence: string, speaker: string) {
     let request = new SpeakRequest(sentence, speaker);
 
     console.log("Making SpeakRequest:", request);
 
-    //const url = this.props.apiConfig.getEndpoint('/speak');
+    /*//const url = this.props.apiConfig.getEndpoint('/speak');
     const url = 'https://mumble.stream/speak';
 
     this.props.onSpeakRequestCallback();
@@ -80,6 +90,81 @@ class Form extends React.Component<Props, State> {
       });
       
       this.setState({howl: sound});
+      sound.play();
+
+      (window as any).sound = sound;
+    })
+    .catch(e => {
+      this.props.onSpeakErrorCallback();
+    });
+    */
+
+    //const url = this.props.apiConfig.getEndpoint('/speak_spectrogram');
+    const url = 'https://mumble.stream/speak_spectrogram';
+
+    this.props.onSpeakRequestCallback();
+    let that = this;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    })
+    .then(res => res.json())
+    .then(res => {
+      const data = `data:audio/wav;base64,${res.audio_base64}`;
+
+      // var image = new Image();
+      // image.src = `data:image/bmp;base64,${res.spectrogram.bytes_base64}`;
+      // console.log('image', image);
+
+      let scale = linearScale([0, 255], [0, 1]);
+      let colorMap = createColorMap(MAGMA, scale);
+
+      // https://stackoverflow.com/a/21797381
+      function base64ToArrayBuffer(base64string: string) : Uint8ClampedArray {
+        var binary_string = window.atob(base64string);
+        var len = binary_string.length * 4;
+        let bytes = new Uint8ClampedArray(len);
+        for (let i = 0, j = 0; i < len; i++, j += 4) {
+          let val = binary_string.charCodeAt(i);
+          if (isNaN(val)) {
+            val = 0;
+          }
+          let mapped = colorMap(val);
+          let r = Math.floor(mapped[0] * 255)
+          let g = Math.floor(mapped[1] * 255)
+          let b = Math.floor(mapped[2] * 255)
+          bytes[j+0] = r;
+          bytes[j+1] = g;
+          bytes[j+2] = b;
+          bytes[j+3] = 255;
+        }
+        return bytes;
+      }
+
+      let bytes = base64ToArrayBuffer(res.spectrogram.bytes_base64);
+
+      const spectrogram = new Spectrogram(bytes, res.spectrogram.width, res.spectrogram.height);
+
+      const sound = new Howl.Howl({
+        src: [data],
+        format: 'wav',
+        // NB: Attempting to get this working on iPhone Safari
+        // https://github.com/goldfire/howler.js/issues/1093
+        // Other issues cite needing to cache a single player 
+        // across all user interaction events.
+        html5: true,
+      });
+      
+      this.setState({
+        howl: sound,
+        spectrogram: spectrogram,
+      });
+
       sound.play();
 
       (window as any).sound = sound;
@@ -131,26 +216,69 @@ class Form extends React.Component<Props, State> {
     }
     return true;
   }
+  
+  componentDidMount() {
+    this.textarea?.focus();
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const ctx = (this.refs.canvas as any).getContext('2d');
+    // let width = 300;
+    // let height = 80;
+    // ctx.clearRect(0,0, width, height);
+
+    if (this.state.spectrogram !== undefined) {
+      let width = this.state.spectrogram!.width;
+      let height = this.state.spectrogram!.height;
+
+      var image = new ImageData(this.state.spectrogram!.pixels, width, height);
+
+      createImageBitmap(image).then(renderer => {
+        ctx.drawImage(renderer, 0, 0, width * 3, height * 3)
+      });
+    }
+  }
+
 
   public render() {
+    let width = 150 * 3;
+    let height = 80 * 3;
+
+    if (this.state.spectrogram !== undefined) {
+      width = this.state.spectrogram.width * 3;
+      height = this.state.spectrogram.height * 3;
+    }
+
+    // TODO: This needs to go way up the tree.
+    let canvas = <canvas ref="canvas" width={width} height={height} id="spectrogram" />
+
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <textarea 
-          onChange={this.handleTextChange} 
-          onKeyDown={this.handleKeyDown}
-          value={this.state.text} 
-          ref={(textarea) => { this.textarea = textarea; }} 
-          />
-        <div>
-          <div className="left">
-            <button>Speak</button>
+      <div>
+        {canvas}
+
+        <form onSubmit={this.handleFormSubmit}>
+          <textarea 
+            onChange={this.handleTextChange} 
+            onKeyDown={this.handleKeyDown}
+            value={this.state.text} 
+            ref={(textarea) => { this.textarea = textarea; }} 
+            />
+          <div>
+            <div className="left">
+              <button>Speak</button>
+            </div>
+            <div className="right">
+              <button onClick={this.handleCancelClick}>Cancel</button>
+            </div>
+            <div className="clear"></div>
           </div>
-          <div className="right">
-            <button onClick={this.handleCancelClick}>Cancel</button>
-          </div>
-          <div className="clear"></div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
