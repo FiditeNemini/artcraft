2d72c757a02595f438c1b450e3fe3a7395770eae
Brandon Thomas <echelon@gmail.com>
2020-06-23 01:04:31 -0400

    change speakers

--- vocodes/src/Speakers.ts
+++ vocodes/src/Speakers.ts
@@ -29,32 +29,37 @@ class Speaker {
 
 const SPEAKERS : Speaker[] = [
   Speaker.fromJson({
-    name: "Donald Trump",
+    name: "Donald Trump (single-speaker model)",
     slug: "trump",
     description: "The 45th President of the United States.",
   }),
   Speaker.fromJson({
-    name: "Donald Trump (Multi-speaker model)",
+    name: "Ronald Reagan (single-speaker model)",
+    slug: "glow-solo-reagan",
+    description: "The 40th President of the United States.",
+  }),
+  Speaker.fromJson({
+    name: "Donald Trump (multi-speaker model)",
     slug: "glow-multi-trump",
     description: "The 45th President of the United States.",
   }),
   Speaker.fromJson({
-    name: "John Oliver (Multi-speaker model)",
+    name: "John Oliver (multi-speaker model)",
     slug: "glow-multi-oliver",
     description: "TV news.",
   }),
   Speaker.fromJson({
-    name: "Bill Gates (Multi-speaker model)",
+    name: "Bill Gates (multi-speaker model)",
     slug: "glow-multi-gates",
     description: "Microsoft.",
   }),
   Speaker.fromJson({
-    name: "Arnold Schwarzenegger (Multi-speaker model)",
+    name: "Arnold Schwarzenegger (multi-speaker model)",
     slug: "glow-multi-arnold",
     description: "Terminator.",
   }),
   Speaker.fromJson({
-    name: "Ronald Reagan (Multi-speaker model)",
+    name: "Ronald Reagan (multi-speaker model)",
     slug: "glow-multi-reagan",
     description: "The 40th President of the United States.",
   }),
