baffe7d0b94cbb75494c5563fe91469baf3c0c2b
Brandon Thomas <echelon@gmail.com>
2020-07-04 03:26:37 -0400

    fonts and nav logo

--- vocodes/src/navigation/TopNav.tsx
+++ vocodes/src/navigation/TopNav.tsx
@@ -9,6 +9,9 @@ interface Props {
 function TopNav(props: Props) {
   return (
     <nav>
+      <span id="logo_text">
+        <span className="vo">Vo</span><span className="codes">codes</span>
+      </span>
       <a href="#speak" onClick={() => props.switchModeCallback(Mode.SPEAK_MODE)}>Speak</a>
       <a href="#history" onClick={() => props.switchModeCallback(Mode.HISTORY_MODE)}>History</a>
       <a href="#use" onClick={() => props.switchModeCallback(Mode.ABOUT_MODE)}>About</a>
