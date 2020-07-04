06c9b4af3906c9a220938a6b0005782b9a40c61f
Brandon Thomas <echelon@gmail.com>
2020-07-04 03:38:06 -0400

    simplify topnav even further

--- vocodes/src/navigation/TopNav.tsx
+++ vocodes/src/navigation/TopNav.tsx
@@ -10,9 +10,9 @@ function TopNav(props: Props) {
   return (
     <nav>
       <span id="logo_text">
-        <span className="vo">Vo</span><span className="codes">codes</span>
+        <a href="#speak" onClick={() => props.switchModeCallback(Mode.SPEAK_MODE)}><span 
+          className="vo">Vo</span><span className="codes">codes</span></a>
       </span>
-      <a href="#speak" onClick={() => props.switchModeCallback(Mode.SPEAK_MODE)}>Speak</a>
       <a href="#history" onClick={() => props.switchModeCallback(Mode.HISTORY_MODE)}>History</a>
       <a href="#use" onClick={() => props.switchModeCallback(Mode.ABOUT_MODE)}>About</a>
       <hr />
