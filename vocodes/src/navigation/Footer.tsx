63f2aa8ec110baf3bd72744554bb61f1d0eccd68
Brandon Thomas <echelon@gmail.com>
2020-09-02 03:37:45 -0400

    social media icons

--- vocodes/src/navigation/Footer.tsx
+++ vocodes/src/navigation/Footer.tsx
@@ -18,6 +18,11 @@ function Footer(props: Props) {
         <a href="#terms" 
           onClick={() => props.switchModeCallback(Mode.TERMS_MODE)}>terms of use</a>.
       </p>
+      <div className="social">
+        <a href="https://discord.gg/wr82xGc" target="_blank" rel="noopener noreferrer"><img src="/social-icons/016-discord.png" alt="Join us on Discord!" /></a>
+        <a href="https://twitter.com/echelon" target="_blank" rel="noopener noreferrer"><img src="/social-icons/096-twitter.png" alt="Twitter" /></a>
+        <a href="https://facebook.com/vocodes" target="_blank" rel="noopener noreferrer"><img src="/social-icons/024-facebook.png" alt="Facething" /></a>
+      </div>
     </footer>
   )
 }
