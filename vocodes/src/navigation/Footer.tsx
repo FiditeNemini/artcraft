e66b7d4c01bfaafd55233e7794958ecdd2adbb2a
Brandon Thomas <echelon@gmail.com>
2020-11-24 20:00:39 -0500

    patreon link

--- vocodes/src/navigation/Footer.tsx
+++ vocodes/src/navigation/Footer.tsx
@@ -22,6 +22,7 @@ function Footer(props: Props) {
         <a href="https://discord.gg/H72KFXm" target="_blank" rel="noopener noreferrer"><img src="/social-icons/016-discord.png" alt="Join us on Discord!" /></a>
         <a href="https://twitter.com/vocodes" target="_blank" rel="noopener noreferrer"><img src="/social-icons/096-twitter.png" alt="Twitter" /></a>
         <a href="https://facebook.com/vocodes" target="_blank" rel="noopener noreferrer"><img src="/social-icons/024-facebook.png" alt="Facething" /></a>
+        <a href="https://www.patreon.com/vocodes" target="_blank" rel="noopener noreferrer"><img src="/social-icons/061-patreon.png" alt="Patreon" /></a>
       </div>
     </footer>
   )
