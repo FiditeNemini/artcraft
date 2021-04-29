75e141638306f6d4595afc30942614b5e58779f9
Brandon Thomas <echelon@gmail.com>
2021-04-29 01:51:57 -0400

    passing test cases

--- src/handlers/coordinate_and_geocode_handler.rs
+++ src/handlers/coordinate_and_geocode_handler.rs
@@ -34,10 +34,13 @@ pub struct LatLong {
 ///   * "34.0659° N, 84.6769° W"
 ///   * "33.753746, -84.386330"
 pub fn parse_lat_long(input: &str) -> Option<LatLong> {
+  const NUMERAL : &'static str = r"(\-?\d+\.?\d*)\s*[NSEW]?";
   lazy_static! {
       static ref COORDINATE_REGEX: Regex =
-        Regex::new(r"^((\-?\d\.?\d*)\w*[NS]?).*,.*((\-?\d\.?\d*)\w*[EW]?)$").expect("Regex should work");
+        Regex::new(&format!(r"^({})\s*,\s*({})$", NUMERAL, NUMERAL)).expect("Regex should work");
     }
+
+  println!("{}", input);
 
   let captures = match COORDINATE_REGEX.captures(&input) {
     None => return None,
@@ -45,12 +48,22 @@ pub fn parse_lat_long(input: &str) -> Option<LatLong> {
   };
 
   let maybe_lat = captures.get(2)
-    .map(|m| m.as_str())
+    .map(|m| {
+      println!("{:?}", m);
+      m.as_str()
+    })
     .and_then(|s| s.parse::<f64>().ok());
 
   let maybe_long = captures.get(4)
-    .map(|m| m.as_str())
+    //.map(|m| m.as_str())
+    .map(|m| {
+      println!("{:?}", m);
+      m.as_str()
+    })
     .and_then(|s| s.parse::<f64>().ok());
+
+  println!("{:?}", maybe_lat);
+  println!("{:?}", maybe_long);
 
   let latitude = match maybe_lat {
     None => return None,
@@ -81,10 +94,23 @@ mod tests {
 
   #[test]
   fn parse_lat_long_raw_coordinates() {
-    expect!(parse_lat_long("33.753746, -84.386330")).to(be_eq(Some(LatLong {
-      latitude:33.753746,
-      longitude: -84.386330,
-    })));
+    expect!(parse_lat_long("33.753746, -84.386330")).to(
+      be_eq(Some(LatLong {
+        latitude: 33.753746,
+        longitude: -84.386330,
+      })));
+
+    expect!(parse_lat_long("-123, -456")).to(
+      be_eq(Some(LatLong {
+        latitude: -123.0,
+        longitude: -456.0,
+      })));
+
+    expect!(parse_lat_long("-99,100")).to(
+      be_eq(Some(LatLong {
+        latitude: -99.0,
+        longitude: 100.0,
+      })));
   }
 
   #[test]
