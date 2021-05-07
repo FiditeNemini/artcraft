8641289f43be53cb3cc46844b081f45da7e8bd6f
Brandon Thomas <echelon@gmail.com>
2021-05-07 02:20:37 -0400

    add other proto type

--- src/proto_utils.rs
+++ src/proto_utils.rs
@@ -11,11 +11,13 @@ use prost::Message;
 // TODO: add some type safety w/ generics and type bounds
 
 /// Sources of events
+#[derive(Debug, Clone)]
 pub enum InboundEventSource {
   Twitch(IngestionTwitchMetadata),
 }
 
 /// Events
+#[derive(Debug, Clone)]
 pub enum InboundEvent {
   TwitchMessage(IngestionTwitchMessage),
 }
@@ -31,15 +33,38 @@ impl InboundEventSource {
           .map_err(|_| anyhow!("error decoding source proto"))
       },
       None => Err(anyhow!("invalid source type")),
-      Some(_) => Err(anyhow!("type is not yet handled")),
+      Some(_) => Err(anyhow!("source type is not yet handled")),
     }
   }
 
   fn parse_type_proto(event_payload: &PubsubEventPayloadV1)
     -> Option<IngestionSourceType>
   {
-    event_payload.ingestion_payload_type
+    event_payload.ingestion_source_type
       .and_then(|num| IngestionSourceType::from_i32(num))
+  }
+}
+
+impl InboundEvent {
+  pub fn parse_from_payload(event_payload: &PubsubEventPayloadV1)
+    -> AnyhowResult<Self>
+  {
+    match Self::parse_type_proto(event_payload) {
+      Some(IngestionPayloadType::TwitchMessage) => {
+        IngestionTwitchMessage::decode(event_payload.ingestion_payload_data())
+          .map(|m| Self::TwitchMessage(m))
+          .map_err(|_| anyhow!("error decoding payload proto"))
+      },
+      None => Err(anyhow!("invalid payload type")),
+      Some(_) => Err(anyhow!("payload type is not yet handled")),
+    }
+  }
+
+  fn parse_type_proto(event_payload: &PubsubEventPayloadV1)
+    -> Option<IngestionPayloadType>
+  {
+    event_payload.ingestion_payload_type
+      .and_then(|num| IngestionPayloadType::from_i32(num))
   }
 }
 
