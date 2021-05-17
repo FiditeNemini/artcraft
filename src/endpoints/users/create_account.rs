e93a390a078ba6d56feab18b25e1e7a74e2f6bed
Brandon Thomas <echelon@gmail.com>
2021-05-16 21:44:07 -0400

    different shape

--- src/endpoints/users/create_account.rs
+++ src/endpoints/users/create_account.rs
@@ -1,15 +1,17 @@
-use actix_web::{Responder, web, HttpResponse};
+use actix_web::{Responder, web, HttpResponse, error};
 use derive_more::{Display, Error};
 use actix_web::dev::HttpResponseBuilder;
 use actix_web::http::StatusCode;
 use actix_http::http::header;
 use actix_http::Error;
+use actix_web::error::ResponseError;
+use crate::endpoints::users::create_account::CreateAccountError::BadInput;
 
 #[derive(Deserialize)]
 pub struct CreateAccountRequest {
   pub username: String,
   pub password: String,
-  pub password_verification: String,
+  pub password_confirmation: String,
 }
 
 #[derive(Serialize)]
@@ -19,7 +21,7 @@ pub struct CreateAccountResponse {
 }
 
 #[derive(Debug, Display, Error)]
-enum CreateAccountResponseError {
+pub enum CreateAccountResponseError {
   #[display(fmt = "internal error")]
   InternalError,
 
@@ -30,7 +32,28 @@ enum CreateAccountResponseError {
   Timeout,
 }
 
-impl actix_web::error::ResponseError for CreateAccountResponseError {
+
+#[derive(Debug, Display)]
+pub enum CreateAccountError {
+  BadInput(String),
+}
+
+impl ResponseError for CreateAccountError {
+  fn status_code(&self) -> StatusCode {
+    match *self {
+      CreateAccountError::BadInput(_) => StatusCode::BAD_REQUEST,
+    }
+  }
+
+  fn error_response(&self) -> HttpResponse {
+    HttpResponseBuilder::new(self.status_code())
+      .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+      .body("bad")
+  }
+}
+
+
+impl ResponseError for CreateAccountResponseError {
   fn status_code(&self) -> StatusCode {
     match *self {
       CreateAccountResponseError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
@@ -46,8 +69,13 @@ impl actix_web::error::ResponseError for CreateAccountResponseError {
   }
 }
 
-//request: web::Json<CreateAccountRequest>
-pub async fn create_account_handler() -> Result<HttpResponse, Error> {
+
+pub async fn create_account_handler(request: web::Json<CreateAccountRequest>)
+  -> Result<HttpResponse, CreateAccountError>
+{
+  if request.username.len() < 3 {
+    return Err(CreateAccountError::BadInput("username is too short".to_string()));
+  }
 
   /*let record_id = sqlx::query!(
         r#"
@@ -65,19 +93,24 @@ VALUES ( ?, ?, ?, ? )
 
   Ok(record_id)*/
 
-  /*if true {
-    return Err(CreateAccountResponse {
-      success: false,
-    });
-  }*/
 
   /*Ok(web::Json())*/
 
+  if true {
+    //return Err(error::ErrorBadRequest(CreateAccountResponse { success: false } ));
+    //return Err(CreateAccountResponseError::UsernameExists("foo".to_string()));
+  }
+
+  // .map_err(error::ErrorInternalServerError)?;
+  // error::ErrorBadRequest(msg)
   let response = CreateAccountResponse {
     success: true,
   };
 
+  let body = serde_json::to_string(&response)
+    .map_err(|e| BadInput("".to_string()))?;
+
   Ok(HttpResponse::Ok()
     .content_type("application/json")
-    .body("foo"))
+    .body(body))
 }
