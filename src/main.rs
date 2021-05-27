88305f5d10f14e9de0022a94b4444a91953450a3
Brandon Thomas <echelon@gmail.com>
2021-05-27 13:53:01 -0400

    functions to check resreved usernames

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate serde_derive;
 
+pub const RESERVED_USERNAMES : &'static str = include_str!("../db/reserved_usernames.txt");
+pub const RESERVED_SUBSTRINGS : &'static str = include_str!("../db/reserved_usernames_including.txt");
+
 pub mod buckets;
 pub mod common_queries;
 pub mod database_helpers;
--- src/validations/mod.rs
+++ src/validations/mod.rs
@@ -1,3 +1,4 @@
 pub mod model_uploads;
 pub mod passwords;
 pub mod username;
+pub mod username_reservations;
