bc3fee54d6e414ed92dbea01c9a38013bec2f639
Brandon Thomas <echelon@gmail.com>
2021-06-06 17:36:43 -0400

    actual permissions

--- seed/sql/system_roles.sql
+++ seed/sql/system_roles.sql
@@ -2,16 +2,37 @@
 
 INSERT INTO user_roles
 SET 
-  slug='user',
-  name='User';
+  slug = 'user',
+  name = 'User',
+  can_ban_users = FALSE,
+  can_edit_other_users_data = FALSE,
+  can_approve_w2l_templates = FALSE,
+  can_upload_tts_models = TRUE,
+  can_upload_w2l_templates = TRUE,
+  can_use_tts = TRUE,
+  can_use_w2l = TRUE;
 
 INSERT INTO user_roles
 SET 
-  slug='moderator',
-  name='Moderator';
+  slug = 'mod',
+  name = 'Moderator',
+  can_ban_users = TRUE,
+  can_edit_other_users_data = TRUE,
+  can_approve_w2l_templates = TRUE,
+  can_upload_tts_models = TRUE,
+  can_upload_w2l_templates = TRUE,
+  can_use_tts = TRUE,
+  can_use_w2l = TRUE;
 
 INSERT INTO user_roles
 SET 
-  slug='admin',
-  name='Admin';
+  slug = 'admin',
+  name = 'Admin',
+  can_ban_users = TRUE,
+  can_edit_other_users_data = TRUE,
+  can_approve_w2l_templates = TRUE,
+  can_upload_tts_models = TRUE,
+  can_upload_w2l_templates = TRUE,
+  can_use_tts = TRUE,
+  can_use_w2l = TRUE;
 
