9587d2a5485576c3846315d42556faa01da0e960
Brandon Thomas <echelon@gmail.com>
2021-06-10 03:00:23 -0400

    major tts/w2l schema changes (won't compile / broken code)

--- migrations/2021-05-15-071601_assets/up.sql
+++ migrations/2021-05-15-071601_assets/up.sql
@@ -21,23 +21,23 @@ CREATE TABLE tts_models (
   -- NB: DO NOT CHANGE ORDER; APPEND ONLY!
   -- How text should be handled.
   text_preprocessing_algorithm ENUM(
-      'basic-preprocess',
-      'arpabet-preprocess'
-  ) NOT NULL DEFAULT 'basic-preprocess',
+      'basic',
+      'arpabet'
+  ) NOT NULL DEFAULT 'basic',
 
   -- Optional Pointer to a newer version of the voice
   -- If there's a newer version, we can disable this one.
-  maybe_updated_model_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_updated_model_token VARCHAR(32) DEFAULT NULL,
 
   -- We can set as a combination of ['username' + 'voice-name']
   -- There can be public aliases for voices, eg. a voice's default model.
   -- A user can change this.
   -- As such, these should not be foreign keys.
-  updatable_slug VARCHAR(64) NOT NULL,
+  -- updatable_slug VARCHAR(64) NOT NULL,
 
   -- Can be linked to a well-known voice
-  maybe_subject_token VARCHAR(32) DEFAULT NULL,
-  maybe_actor_subject_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_subject_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_actor_subject_token VARCHAR(32) DEFAULT NULL,
 
   -- ========== UNSTRUCTURED METADATA ==========
 
@@ -45,19 +45,34 @@ CREATE TABLE tts_models (
   -- If maybe_subject_token (etc.) is set, then it's authoritative instead.
   title VARCHAR(255) NOT NULL,
 
-  -- If the voice is "happy" or a singer "a-capella", etc.
-  -- This is used with "maybe_subject_token" etc.
-  maybe_voice_characteristic VARCHAR(255) DEFAULT NULL,
-
-  -- Eg. ("poor quality", "trained longer", etc.)
-  -- This is used with "maybe_subject_token" etc.
-  maybe_extra_model_disambiguation VARCHAR(255) DEFAULT NULL,
-
   -- The description of the model in markdown.
   description_markdown TEXT NOT NULL,
 
   -- Generated HTML (not user-editable).
   description_rendered_html TEXT NOT NULL,
+
+  -- eg. Bill Clinton, Batman, or Goku.
+  voice_name VARCHAR(255) DEFAULT NULL,
+
+  -- Optional; eg. Sean Schemmel
+  actor_name VARCHAR(255) DEFAULT NULL,
+
+  -- Optional; eg. if the voice is "happy" or a singer "a-capella", etc.
+  characteristic VARCHAR(255) DEFAULT NULL,
+
+  -- Optional; eg. if you train the model more than once.
+  version_string VARCHAR(255) DEFAULT NULL,
+
+  -- ========== IMMUTABLE PROVENANCE AND METADATA ==========
+
+  -- Where the file was originally downloaded (if it was downloaded)
+  original_download_url VARCHAR(512) DEFAULT NULL,
+
+  -- The filename that was used at upload time (if available)
+  original_filename VARCHAR(255) DEFAULT NULL,
+
+  -- File characteristics
+  file_size_bytes INT(10) NOT NULL DEFAULT 0,
 
   -- ========== CREATOR DETAILS ==========
 
@@ -78,14 +93,7 @@ CREATE TABLE tts_models (
     'private'
    ) NOT NULL DEFAULT 'public',
 
-  -- Where the file was originally downloaded (if it was downloaded)
-  original_download_url VARCHAR(512) DEFAULT NULL,
-
-  -- The filename that was used at upload time (if available)
-  original_filename VARCHAR(255) DEFAULT NULL,
-
-  -- File characteristics
-  file_size_bytes INT(10) NOT NULL DEFAULT 0,
+  -- ========== BUCKET STORAGE ==========
 
   -- The pytorch model
   -- For now, this will be a hash of the file contents.
@@ -94,15 +102,24 @@ CREATE TABLE tts_models (
   -- The "full url" version of the path
   private_bucket_object_name VARCHAR(255) NOT NULL,
 
+  -- ========== RATING AND RANKING ==========
+
   -- Calculated average, on a scale of 0-100
   -- Null with zero ratings.
-  calculated_average_score INT(3) DEFAULT NULL,
+  -- calculated_average_score INT(3) DEFAULT NULL,
 
   -- Other metrics
-  calculated_total_ratings_submitted_count INT(10) NOT NULL DEFAULT 0,
-  calculated_total_uses_count BIGINT(10) NOT NULL DEFAULT 0,
+  -- calculated_total_ratings_submitted_count INT(10) NOT NULL DEFAULT 0,
+  -- calculated_total_uses_count BIGINT(10) NOT NULL DEFAULT 0,
 
   -- ========== MODERATION DETAILS ==========
+
+  -- Mods have to approve of tts models for them to show up in a public index.
+  -- Models can be used by the author (or other parties that know the URL) before
+  -- they're approved, but unapproved models won't show up in public indices.
+  -- This is independent of other visibility controls (is_mod_disabled, deleted_at,
+  -- and creator_set_visibility)
+  is_mod_public_listing_approved BOOLEAN DEFAULT NULL,
 
   -- In this case, a moderator disables it.
   is_mod_disabled BOOLEAN NOT NULL DEFAULT FALSE,
@@ -112,6 +129,7 @@ CREATE TABLE tts_models (
 
   -- If a moderator has comments.
   maybe_mod_comments VARCHAR(255) DEFAULT NULL,
+
   -- The last moderator that made changes.
   maybe_mod_user_token VARCHAR(32) DEFAULT NULL,
 
@@ -143,6 +161,7 @@ CREATE TABLE tts_models (
   KEY index_creator_ip_address_last_update (creator_ip_address_last_update),
   KEY index_creator_set_visibility (creator_set_visibility),
   KEY index_private_bucket_hash (private_bucket_hash),
+  KEY index_is_mod_public_listing_approved (is_mod_public_listing_approved),
   KEY index_is_mod_disabled (is_mod_disabled)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
@@ -164,17 +183,17 @@ CREATE TABLE w2l_templates (
 
   -- Optional Pointer to a newer version of the template
   -- If there's a newer version, we can disable this one.
-  maybe_updated_template_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_updated_template_token VARCHAR(32) DEFAULT NULL,
 
   -- A combination of ['username' + 'template-name']
   -- There can be public aliases for voices, eg. a voice's default model.
   -- A user can change this.
   -- As such, these should not be foreign keys.
-  updatable_slug VARCHAR(64) NOT NULL,
+  -- updatable_slug VARCHAR(64) NOT NULL,
 
   -- Can be linked to a well-known voice
-  maybe_subject_token VARCHAR(32) DEFAULT NULL,
-  maybe_actor_subject_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_subject_token VARCHAR(32) DEFAULT NULL,
+  -- maybe_actor_subject_token VARCHAR(32) DEFAULT NULL,
 
   -- ========== UNSTRUCTURED METADATA ==========
 
@@ -186,6 +205,23 @@ CREATE TABLE w2l_templates (
 
   -- Generated HTML (not user-editable).
   description_rendered_html TEXT NOT NULL,
+
+  -- ========== IMMUTABLE PROVENANCE AND METADATA ==========
+
+  -- Where the file was originally downloaded (if it was downloaded)
+  original_download_url VARCHAR(512) DEFAULT NULL,
+
+  -- The filename that was used at upload time (if available)
+  original_filename CHAR(255) DEFAULT NULL,
+
+  -- Dimensions and other characteristics
+  file_size_bytes INT(10) NOT NULL DEFAULT 0,
+  mime_type VARCHAR(32) NOT NULL DEFAULT '',
+  frame_width INT(10) NOT NULL DEFAULT 0,
+  frame_height INT(10) NOT NULL DEFAULT 0,
+  frame_count INT(10) NOT NULL DEFAULT 0,
+  fps FLOAT NOT NULL DEFAULT 0.0,
+  duration_millis INT(10) NOT NULL DEFAULT 0,
 
   -- ========== CREATOR DETAILS ==========
 
@@ -206,20 +242,7 @@ CREATE TABLE w2l_templates (
     'private'
   ) NOT NULL DEFAULT 'public',
 
-  -- Where the file was originally downloaded (if it was downloaded)
-  original_download_url VARCHAR(512) DEFAULT NULL,
-
-  -- The filename that was used at upload time (if available)
-  original_filename CHAR(255) DEFAULT NULL,
-
-  -- Dimensions and other characteristics
-  file_size_bytes INT(10) NOT NULL DEFAULT 0,
-  mime_type VARCHAR(32) NOT NULL DEFAULT '',
-  frame_width INT(10) NOT NULL DEFAULT 0,
-  frame_height INT(10) NOT NULL DEFAULT 0,
-  frame_count INT(10) NOT NULL DEFAULT 0,
-  fps FLOAT NOT NULL DEFAULT 0.0,
-  duration_millis INT(10) NOT NULL DEFAULT 0,
+  -- ========== BUCKET STORAGE ==========
 
   -- The hash of the original source image/video
   private_bucket_hash CHAR(64) NOT NULL,
@@ -233,13 +256,15 @@ CREATE TABLE w2l_templates (
   maybe_public_bucket_preview_image_object_name VARCHAR(255) DEFAULT NULL,
   maybe_public_bucket_preview_video_object_name VARCHAR(255) DEFAULT NULL,
 
+  -- ========== RATING AND RANKING ==========
+
   -- Calculated average, on a scale of 0-100
   -- Null with zero ratings.
-  calculated_average_score INT(3) DEFAULT NULL,
+  -- calculated_average_score INT(3) DEFAULT NULL,
 
   -- Other metrics
-  calculated_total_ratings_submitted_count INT(10) NOT NULL DEFAULT 0,
-  calculated_total_uses_count BIGINT(10) NOT NULL DEFAULT 0,
+  -- calculated_total_ratings_submitted_count INT(10) NOT NULL DEFAULT 0,
+  -- calculated_total_uses_count BIGINT(10) NOT NULL DEFAULT 0,
 
   -- ========== MODERATION DETAILS ==========
 
@@ -293,6 +318,7 @@ CREATE TABLE w2l_templates (
   KEY index_creator_ip_address_last_update (creator_ip_address_last_update),
   KEY index_creator_set_visibility (creator_set_visibility),
   KEY index_private_bucket_hash (private_bucket_hash),
+  KEY index_is_mod_public_listing_approved (is_mod_public_listing_approved),
   KEY index_is_mod_disabled (is_mod_disabled)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
