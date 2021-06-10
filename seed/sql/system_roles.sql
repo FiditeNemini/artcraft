72a0ca01d986255d8fca0c37e3ec04c2d58fc22f
Brandon Thomas <echelon@gmail.com>
2021-06-09 22:41:22 -0400

    seed with new permissions

--- seed/sql/system_roles.sql
+++ seed/sql/system_roles.sql
@@ -7,6 +7,9 @@ SET
   -- Usage
   can_use_tts = TRUE,
   can_use_w2l = TRUE,
+  can_delete_own_tts_results = TRUE,
+  can_delete_own_w2l_results = TRUE,
+  can_delete_own_account = TRUE,
   -- Contribution
   can_upload_tts_models = TRUE,
   can_upload_w2l_templates = TRUE,
@@ -18,8 +21,11 @@ SET
   can_edit_other_users_tts_models = FALSE,
   can_edit_other_users_w2l_templates = FALSE,
   can_delete_other_users_tts_models = FALSE,
+  can_delete_other_users_tts_results = FALSE,
   can_delete_other_users_w2l_templates = FALSE,
-  can_ban_users = FALSE;
+  can_delete_other_users_w2l_results = FALSE,
+  can_ban_users = FALSE,
+  can_delete_users = FALSE;
 
 INSERT INTO user_roles
 SET 
@@ -28,6 +34,9 @@ SET
   -- Usage
   can_use_tts = TRUE,
   can_use_w2l = TRUE,
+  can_delete_own_tts_results = TRUE,
+  can_delete_own_w2l_results = TRUE,
+  can_delete_own_account = TRUE,
   -- Contribution
   can_upload_tts_models = TRUE,
   can_upload_w2l_templates = TRUE,
@@ -39,8 +48,11 @@ SET
   can_edit_other_users_tts_models = TRUE,
   can_edit_other_users_w2l_templates = TRUE,
   can_delete_other_users_tts_models = TRUE,
+  can_delete_other_users_tts_results = TRUE,
   can_delete_other_users_w2l_templates = TRUE,
-  can_ban_users = TRUE;
+  can_delete_other_users_w2l_results = TRUE,
+  can_ban_users = TRUE,
+  can_delete_users = TRUE;
 
 INSERT INTO user_roles
 SET 
@@ -49,6 +61,9 @@ SET
   -- Usage
   can_use_tts = TRUE,
   can_use_w2l = TRUE,
+  can_delete_own_tts_results = TRUE,
+  can_delete_own_w2l_results = TRUE,
+  can_delete_own_account = TRUE,
   -- Contribution
   can_upload_tts_models = TRUE,
   can_upload_w2l_templates = TRUE,
@@ -60,6 +75,9 @@ SET
   can_edit_other_users_tts_models = TRUE,
   can_edit_other_users_w2l_templates = TRUE,
   can_delete_other_users_tts_models = TRUE,
+  can_delete_other_users_tts_results = TRUE,
   can_delete_other_users_w2l_templates = TRUE,
-  can_ban_users = TRUE;
+  can_delete_other_users_w2l_results = TRUE,
+  can_ban_users = TRUE,
+  can_delete_users = TRUE;
 
