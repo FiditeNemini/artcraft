716f8d55eaee5349e5e4ff3ee0d1f030758a339a
Brandon Thomas <echelon@gmail.com>
2021-06-26 14:01:23 -0400

    fix layout, compile warning

--- vocodes/src/v2/upload/UploadChoiceFc.tsx
+++ vocodes/src/v2/upload/UploadChoiceFc.tsx
@@ -1,62 +1,58 @@
 import React from 'react';
 import { SessionWrapper } from '../../session/SessionWrapper';
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Props {
   sessionWrapper: SessionWrapper,
 }
 
 function UploadChoiceFc(props: Props) {
-  let history = useHistory();
-
-  //if (!props.sessionWrapper.isLoggedIn()) {
-  //  history.push('/signup');
-  //}
-
   return (
     <div>
-      <h1 className="title is-1"> Upload to Vocodes ! </h1>
-      <h1 className="subtitle is-3"> You make vo.codes <strong>better</strong> by uploading</h1>
-
-      <p>
-        You'll get credited for everything you upload. You'll also get queue priority, 
-        be eligible to win prizes, and help us become a Hollywood-killing deepfake 
-        tooling, streaming, and filmmaking powerhouse.
-      </p>
+      <div>
+        <h1 className="title is-1"> Upload to Vocodes ! </h1>
+        <h1 className="subtitle is-3"> You make vo.codes <strong>better</strong> by uploading</h1>
+      </div>
 
       <br />
 
-      <p>
-        Choose an upload type:
-      </p>
-      <br />
+      <div className="content is-medium">
+        <p>
+          You'll get credited for everything you upload. You'll also get queue priority, 
+          be eligible to win prizes, and help us become a Hollywood-killing deepfake 
+          tooling, streaming, and filmmaking powerhouse.
+        </p>
 
-      <Link
-        to="/upload/tts"
-        className="button is-link is-large is-fullwidth is-outlined"
-        onClick={() => {}}
-        >Upload voice (TTS model)</Link>
+        <p>
+          Choose an upload type:
+        </p>
 
-      <br />
+        <br />
 
-      <Link
-        to="/upload/w2l_video"
-        className="button is-link is-large is-fullwidth is-outlined"
-        onClick={() => {}}
-        >Upload lipsync video (w2l)</Link>
+        <Link
+          to="/upload/tts"
+          className="button is-link is-large is-fullwidth is-outlined"
+          >Upload voice (TTS model)</Link>
 
-      <br />
+        <br />
 
-      <Link
-        to="/upload/w2l_photo"
-        className="button is-info is-large is-fullwidth is-outlined"
-        onClick={() => {}}
-        >Upload lipsync photo (w2l)</Link>
+        <Link
+          to="/upload/w2l_video"
+          className="button is-link is-large is-fullwidth is-outlined"
+          >Upload lipsync video (w2l)</Link>
 
-      <br />
+        <br />
 
-      <p> Want to contribute code? We're hiring! </p>
+        <Link
+          to="/upload/w2l_photo"
+          className="button is-info is-large is-fullwidth is-outlined"
+          >Upload lipsync photo (w2l)</Link>
 
+        <br />
+
+        <p> Want to contribute code? We're hiring! </p>
+
+      </div>
     </div>
   )
 }
