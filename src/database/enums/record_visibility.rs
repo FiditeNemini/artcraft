baffa751d630dfa2c3dab0c882b95616f1ebb5f0
Brandon Thomas <echelon@gmail.com>
2021-07-11 16:19:33 -0400

    visibility enums

--- src/database/enums/record_visibility.rs
+++ src/database/enums/record_visibility.rs
@@ -1,15 +1,14 @@
 //! These are columns where users can control the visibility of their data.
 
-//#[sqlx(rename_all = "lowercase", rename = "preferred_tts_result_visibility")]
-
+/// To use this in a query, the query must have type annotations.
+/// See: https://www.gitmemory.com/issue/launchbadge/sqlx/1241/847154375
+/// eg. preferred_tts_result_visibility as `preferred_tts_result_visibility: crate::database::enums::record_visibility::RecordVisibility`
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize, sqlx::Type)]
+#[serde(rename_all = "lowercase")]
 #[sqlx(rename_all = "lowercase")]
 pub enum RecordVisibility {
-  #[sqlx(rename = "public")]
   Public,
-  #[sqlx(rename = "hidden")]
   Hidden,
-  #[sqlx(rename = "private")]
   Private,
 }
 
