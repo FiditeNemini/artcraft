c529756ab7532bb720995bdb9b7a02156ef0c0af
Brandon Thomas <echelon@gmail.com>
2021-09-10 01:33:58 -0400

    rename migration directory

--- migrations/2021-08-25-010101_alter_tables_add_vocoder_type/up.sql
+++ migrations/2021-08-25-010101_alter_tables_add_vocoder_type/up.sql
@@ -2,6 +2,10 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
 
+-- TODO/FIXME: I originally created this with the wrong directory month.
+--  I'll have to repair the migrations table.
+--  version: 20210925010101 | run_on: 2021-08-25 03:01:15 (development)
+
 ALTER TABLE tts_models
 ADD COLUMN maybe_default_pretrained_vocoder VARCHAR(64) DEFAULT NULL
 AFTER text_preprocessing_algorithm;
