de802846652d951379a851568889020a436f8144
Brandon Thomas <echelon@gmail.com>
2021-09-28 01:09:14 -0400

    util to get http hostname header (turns out I don't need it yet)

---
+++ src/http_server/web_utils/get_host_header.rs
@@ -0,0 +1,18 @@
+use actix_http::http::HeaderMap;
+use actix_http::http::HeaderName;
+use actix_web::HttpRequest;
+use actix_web::http::HeaderValue;
+use crate::util::anyhow_result::AnyhowResult;
+
+// NB: This is extracted because CLion is having incredible difficulty with these types.
+pub fn get_host_header(http_request: &HttpRequest) -> AnyhowResult<Option<String>> {
+  let host_header_name  = HeaderName::from_static("host");
+  let header_map : &HeaderMap = http_request.headers();
+  let maybe_hostname = header_map.get(host_header_name);
+
+  let hostname = maybe_hostname.map(|s| s.to_str())
+      .map(|inner| inner.map(|s| s.to_string()))
+      .transpose()?;
+
+  Ok(hostname)
+}
