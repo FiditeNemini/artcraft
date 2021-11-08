7256630d87cf6f29e7e464565cc172dd4e31da0f
Brandon Thomas <echelon@gmail.com>
2021-11-08 05:02:42 -0500

    http common package versions that work with both apps

--- app/http_server_common/src/lib.rs
+++ app/http_server_common/src/lib.rs
@@ -2,6 +2,8 @@
 //! Common HTTP Server components
 //!
 
+#[macro_use] extern crate serde_derive;
+
 pub mod cors;
 pub mod endpoints;
 pub mod response;
--- app/http_server_common/src/response/serialize_as_json_error.rs
+++ app/http_server_common/src/response/serialize_as_json_error.rs
@@ -1,8 +1,8 @@
+use actix_http::ResponseError;
 use actix_http::http::{StatusCode, header};
 use actix_web::HttpResponse;
-use serde::Serialize;
-use actix_http::ResponseError;
 use actix_web::dev::HttpResponseBuilder;
+use serde::Serialize;
 
 /// Turn error responses into JSON HTTP responses
 pub fn serialize_as_json_error<T>(
