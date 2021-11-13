5ce4bd3b37881c897fe90a13da9bcbaa065d0062
Brandon Thomas <echelon@gmail.com>
2021-11-12 23:35:43 -0500

    Cleanup IP filter

--- app/storyteller_web/src/http_server/middleware/ip_filter_middleware.rs
+++ app/storyteller_web/src/http_server/middleware/ip_filter_middleware.rs
@@ -1,75 +1,38 @@
-use crate::http_server::web_utils::ip_address::get_service_request_ip;
-use crate::threads::ip_banlist_set::IpBanlistSet;
+// There are two steps in middleware processing.
+// 1. Middleware initialization, middleware factory gets called with
+//    next service in chain as parameter.
+// 2. Middleware's call method gets called with normal request.
 
-//use actix_web::error::ErrorForbidden;
-//use actix_web::{HttpResponse, ResponseError};
-//use anyhow::anyhow;
-//use futures::Future;
-//use futures::future::{ok, Ready, LocalBoxFuture, err, Either};
-//use log::info;
-//use log::warn;
-//use std::pin::Pin;
-//use std::task::{Context, Poll};
-//use actix_web::body::{AnyBody, MessageBody};
-//use actix_http::{http, StatusCode, header, Response};
-//use actix_http::body::{Body, ResponseBody};
-//use actix_web::web::{BytesMut, Buf, BufMut};
-//use std::fmt;
-//use std::{io::Write as _};
-//use futures::future::FutureExt;
-
-use std::task::{Context, Poll};
-
+use actix_http::StatusCode;
+use actix_http::http::{header, HeaderMap, HeaderValue};
+use actix_http::{error, body::Body, Response};
 use actix_web::dev::{Service, Transform};
 use actix_web::dev::{ServiceRequest, ServiceResponse};
-use actix_web::{Error, HttpResponse};
-use futures_util::future::{err, ok, Either, Ready};
-
-use derive_more::{Display, Error as ErrorE};
-use actix_web::{ResponseError, HttpMessage, HttpRequest, HttpResponseBuilder};
-use actix_http::StatusCode;
-use actix_http::{error, body::Body, Response};
 use actix_web::web::{BytesMut, Buf, BufMut};
-use actix_http::http::{header, HeaderMap, HeaderValue};
+use actix_web::{Error, HttpResponse};
+use actix_web::{ResponseError, HttpMessage, HttpRequest, HttpResponseBuilder};
+use crate::http_server::web_utils::ip_address::get_service_request_ip;
+use crate::threads::ip_banlist_set::IpBanlistSet;
+use derive_more::{Display, Error as ErrorE};
+use futures_util::future::{err, ok, Either, Ready};
 use std::io::Write;
+use std::task::{Context, Poll};
 
 #[derive(Debug, Display, ErrorE)]
-pub struct BannedError {
-}
+pub struct BannedError {}
 
 impl ResponseError for BannedError {
   fn status_code(&self) -> StatusCode {
     StatusCode::UNAUTHORIZED
   }
+
   fn error_response(&self) -> HttpResponse<Body> {
-    // let mut res = Response::new(self.status_code());
-    // res.headers_mut().insert(
-    //     header::CONTENT_TYPE,
-    //     header::HeaderValue::from_static("application/json"),
-    // );
-    // res.set_body(Body::from(self.to_string())).into()
-    /*let mut res = Response::new(self.status_code());
-    let mut buf = BytesMut::new().writer();
-    let _ = write!(buf, "{}", self);
-
-    res.headers_mut().insert(
-      header::CONTENT_TYPE,
-      header::HeaderValue::from_static("application/json"),
-    );
-    res.set_body(Body::from(buf.into_inner())).into()*/
-
     HttpResponseBuilder::new(self.status_code())
-      .set_header(header::CONTENT_TYPE, "application/json")
-      .body("TODO")
+      .append_header((header::CONTENT_TYPE, "application/json"))
+      .body("{}")
   }
 }
 
-
-
-// There are two steps in middleware processing.
-// 1. Middleware initialization, middleware factory gets called with
-//    next service in chain as parameter.
-// 2. Middleware's call method gets called with normal request.
 #[derive(Clone)] // NB: Clone is safe because IpBanlist is clone-safe (internal Arc)
 pub struct IpFilter {
   ip_banlist: IpBanlistSet,
@@ -82,10 +45,6 @@ impl IpFilter {
     }
   }
 }
-
-// Middleware factory is `Transform` trait from actix-service crate
-// `S` - type of the next service
-// `B` - type of response's body
 
 impl<S> Transform<S, ServiceRequest> for IpFilter
   where
@@ -118,7 +77,7 @@ impl<S> Service<ServiceRequest> for IpFilterMiddleware<S>
   type Error = Error;
   type Future = Either<S::Future, Ready<Result<Self::Response, Self::Error>>>;
 
-  //actix_service::forward_ready!(service);
+  // alternatively(?), actix_service::forward_ready!(service);
   fn poll_ready(&self, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
     self.service.poll_ready(cx)
   }
@@ -127,14 +86,11 @@ impl<S> Service<ServiceRequest> for IpFilterMiddleware<S>
     let ip_address = get_service_request_ip(&req);
 
     // NB: Fail open.
-    let is_banned = self.ip_banlist.is_banned(&ip_address).unwrap_or(false);
+    let is_banned = self.ip_banlist
+        .is_banned(&ip_address)
+        .unwrap_or(false);
 
     if is_banned {
-      let res = req.into_response(
-        HttpResponse::Unauthorized()
-            .finish());
-      //return Either::Right(err(Error::from(error)));
-      //return Err(Error::from(UserError::ValidationError { field: "token invalid".to_string() }));
       return Either::Right(err(Error::from(BannedError {})))
     }
 
