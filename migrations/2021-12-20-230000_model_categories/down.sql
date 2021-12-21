4cb208bad742adf311d646e23464f4686b3b35dc
Brandon Thomas <echelon@gmail.com>
2021-12-20 19:36:35 -0500

    Model category tables

---
+++ migrations/2021-12-20-230000_model_categories/down.sql
@@ -0,0 +1,6 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+DROP TABLE model_categories;
+DROP TABLE model_category_assignments;
---
+++ migrations/2021-12-20-230000_model_categories/up.sql
@@ -0,0 +1,116 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+CREATE TABLE model_categories (
+  -- Not used for anything except replication.
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+
+  -- Visible "primary key"
+  token VARCHAR(32) NOT NULL,
+
+  -- ========== CATEGORY TOPOLOGY ==========
+
+  -- Optional foreign key link to super category.
+  -- If absent, this is a top-level category or top-level super category.
+  super_category_token VARCHAR(32) DEFAULT NULL,
+
+  -- If this category can be applied directly to models.
+  can_have_models BOOLEAN NOT NULL DEFAULT false,
+
+  -- If this category can have subcategories.
+  can_have_subcategories BOOLEAN NOT NULL DEFAULT false,
+
+  -- ========== CATEGORY NAME ==========
+
+  -- The name of the category, which might be complicated.
+  name VARCHAR(255) NOT NULL,
+
+  -- The name displayed in the dropdown (if it should be different), otherwise NULL.
+  -- eg. "Gender" (name) and "By Gender" (dropdown name)
+  dropdown_name VARCHAR(20) DEFAULT NULL,
+
+  -- ========== CREATOR DETAILS ==========
+
+  -- The person that created the category.
+  creator_user_token VARCHAR(32) NOT NULL,
+
+  -- For abuse tracking.
+  -- Wide enough for IPv4/6
+  creator_ip_address_creation VARCHAR(40) NOT NULL,
+  -- Technically we don't allow editing, but just in case.
+  creator_ip_address_last_update VARCHAR(40) NOT NULL,
+
+  -- ========== MODERATION DETAILS ==========
+
+  -- Mods have to approve of categories. They remain hidden and unusable until approved.
+  is_mod_approved BOOLEAN DEFAULT NULL,
+
+  -- If a moderator has comments.
+  maybe_mod_comments VARCHAR(255) DEFAULT NULL,
+
+  -- The last moderator that made changes.
+  maybe_mod_user_token VARCHAR(32) DEFAULT NULL,
+
+  -- ========== VECTOR CLOCK ==========
+
+  -- Incremented with every update.
+  version INT NOT NULL DEFAULT 0,
+
+  -- ========== TIMESTAMPS ==========
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+  -- If the category is deleted and shouldn't be displayed.
+  -- Only mods can delete categories.
+  deleted_at TIMESTAMP NULL,
+
+  -- INDICES --
+  PRIMARY KEY (id),
+  UNIQUE KEY (token),
+  KEY fk_super_category_token (super_category_token),
+  KEY index_can_have_models (can_have_models),
+  KEY index_can_have_subcategories (can_have_subcategories),
+  KEY index_is_mod_approved (is_mod_approved)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Join table assigning models to zero or more categories.
+CREATE TABLE model_category_assignments (
+  -- Not used for anything except replication.
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+
+  model_token VARCHAR(32) NOT NULL,
+  category_token VARCHAR(32) NOT NULL,
+
+  -- ========== USER DETAILS ==========
+
+  -- The person that created the assignment.
+  -- This can be the model owner or a moderator.
+  category_addition_user_token VARCHAR(32) NOT NULL,
+
+  -- The person that removed the assignment.
+  -- This can be the model owner or a moderator.
+  category_removal_user_token VARCHAR(32) NOT NULL,
+
+  -- For abuse tracking.
+  -- THIS CAN BE A MODERATOR FOR SIMPLICITY.
+  -- Wide enough for IPv4/6
+  ip_address_creation VARCHAR(40) NOT NULL,
+  ip_address_last_update VARCHAR(40) NOT NULL,
+
+  -- ========== TIMESTAMPS ==========
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP NULL,
+
+  -- INDICES --
+  PRIMARY KEY (id),
+  UNIQUE KEY (model_token, category_token),
+  KEY fk_model_token (model_token),
+  KEY fk_category_token (category_token),
+  KEY index_deleted_at (deleted_at)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
