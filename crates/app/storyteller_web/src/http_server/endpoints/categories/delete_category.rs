eb11bb5118b22836ea865f998b8c2ab4e4184fba
Brandon Thomas <echelon@gmail.com>
2021-12-22 01:02:11 -0500

    Delete and undelete categories

---
+++ crates/app/storyteller_web/src/http_server/endpoints/categories/delete_category.rs
@@ -0,0 +1,156 @@
+use actix_http::Error;
+use actix_http::http::header;
+use actix_web::cookie::Cookie;
+use actix_web::HttpResponseBuilder;
+use actix_web::error::ResponseError;
+use actix_web::http::StatusCode;
+use actix_web::web::{Path, Json};
+use actix_web::{Responder, web, HttpResponse, error, HttpRequest};
+use crate::database::enums::record_visibility::RecordVisibility;
+use crate::database::queries::query_w2l_template::select_w2l_template_by_token;
+use crate::http_server::web_utils::ip_address::get_request_ip;
+use crate::http_server::web_utils::response_error_helpers::to_simple_json_error;
+use crate::http_server::web_utils::response_success_helpers::simple_json_success;
+use crate::server_state::ServerState;
+use crate::util::email_to_gravatar::email_to_gravatar;
+use crate::util::markdown_to_html::markdown_to_html;
+use crate::util::random_crockford_token::random_crockford_token;
+use crate::util::random_prefix_crockford_token::random_prefix_crockford_token;
+use crate::validations::check_for_slurs::contains_slurs;
+use log::{info, warn, log};
+use regex::Regex;
+use sqlx::MySqlPool;
+use sqlx::error::DatabaseError;
+use sqlx::error::Error::Database;
+use sqlx::mysql::MySqlDatabaseError;
+use std::sync::Arc;
+use std::fmt;
+use http_server_common::response::serialize_as_json_error::serialize_as_json_error;
+use crate::database::helpers::tokens::Tokens;
+
+// =============== Request ===============
+
+/// For the URL PathInfo
+#[derive(Deserialize)]
+pub struct DeleteCategoryPathInfo {
+  token: String,
+}
+
+#[derive(Deserialize)]
+pub struct DeleteCategoryRequest {
+  set_delete: bool,
+}
+
+// =============== Success Response ===============
+
+#[derive(Serialize)]
+pub struct DeleteCategoryResponse {
+  pub success: bool,
+}
+
+// =============== Error Response ===============
+
+#[derive(Debug, Serialize)]
+pub enum DeleteCategoryError {
+  NotFound,
+  NotAuthorized,
+  ServerError,
+}
+
+impl ResponseError for DeleteCategoryError {
+  fn status_code(&self) -> StatusCode {
+    match *self {
+      DeleteCategoryError::NotFound => StatusCode::NOT_FOUND,
+      DeleteCategoryError::NotAuthorized => StatusCode::UNAUTHORIZED,
+      DeleteCategoryError::ServerError => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+  }
+
+  fn error_response(&self) -> HttpResponse {
+    serialize_as_json_error(self)
+  }
+}
+
+// NB: Not using DeriveMore since Clion doesn't understand it.
+impl fmt::Display for DeleteCategoryError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{:?}", self)
+  }
+}
+
+// =============== Handler ===============
+
+pub async fn delete_category_handler(
+  http_request: HttpRequest,
+  path: Path<DeleteCategoryPathInfo>,
+  request: web::Json<DeleteCategoryRequest>,
+  server_state: web::Data<Arc<ServerState>>) -> Result<HttpResponse, DeleteCategoryError>
+{
+  let maybe_user_session = server_state
+      .session_checker
+      .maybe_get_user_session(&http_request, &server_state.mysql_pool)
+      .await
+      .map_err(|e| {
+        warn!("Session checker error: {:?}", e);
+        DeleteCategoryError::ServerError
+      })?;
+
+  let user_session = match maybe_user_session {
+    Some(session) => session,
+    None => {
+      warn!("not logged in");
+      return Err(DeleteCategoryError::NotAuthorized);
+    }
+  };
+
+  // TODO: We don't have a permission for categories, so we use this as a proxy.
+  if !user_session.can_ban_users {
+    warn!("no permission to delete categories");
+    return Err(DeleteCategoryError::NotAuthorized);
+  }
+
+  let query_builder = if request.set_delete {
+    sqlx::query!(r#"
+      UPDATE model_categories
+      SET
+        deleted_at = CURRENT_TIMESTAMP
+      WHERE
+        token = ?
+      LIMIT 1
+    "#,
+      path.token)
+
+  } else {
+    sqlx::query!(r#"
+      UPDATE model_categories
+      SET
+        deleted_at = NULL
+      WHERE
+        token = ?
+      LIMIT 1
+    "#,
+      path.token)
+  };
+
+  // NB: We're soft deleting so we don't delete the associations.
+  let query_result = query_builder.execute(&server_state.mysql_pool).await;
+
+  match query_result {
+    Ok(_) => {},
+    Err(err) => {
+      warn!("Delete/undelete category edit DB error: {:?}", err);
+      return Err(DeleteCategoryError::ServerError);
+    }
+  };
+
+  let response = DeleteCategoryResponse {
+    success: true,
+  };
+
+  let body = serde_json::to_string(&response)
+      .map_err(|e| DeleteCategoryError::ServerError)?;
+
+  Ok(HttpResponse::Ok()
+      .content_type("application/json")
+      .body(body))
+}
--- debug/curl/category_tests.py
+++ debug/curl/category_tests.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import json
 import re
 import requests
 import uuid
 
 login_url = 'http://localhost:12345/login'
 category_create_url = 'http://localhost:12345/category/create'
+category_delete_url_format = 'http://localhost:12345/category/{}/delete'
 
 payload = {
   'username_or_email': 'echelon',
@@ -43,5 +45,40 @@ payload = {
 r = requests.post(category_create_url, cookies=cookies, json=payload)
 
 print('Status: {}'.format(r.status_code))
+
+d = json.loads(r.content)
+category_token = d['token']
+
+print(f'created category token: #{category_token}')
+
+
+print("===== (Soft) Delete Category =====")
+
+category_delete_url = category_delete_url_format.format(category_token)
+
+cookies = { 'session': session_cookie }
+
+payload = {
+  'set_delete': True,
+}
+
+r = requests.post(category_delete_url, cookies=cookies, json=payload)
+
+print('Status: {}'.format(r.status_code))
 print(r.content)
+
+
+print("===== Undelete Category =====")
+
+cookies = { 'session': session_cookie }
+
+payload = {
+  'set_delete': False,
+}
+
+r = requests.post(category_delete_url, cookies=cookies, json=payload)
+
+print('Status: {}'.format(r.status_code))
+print(r.content)
+
 
