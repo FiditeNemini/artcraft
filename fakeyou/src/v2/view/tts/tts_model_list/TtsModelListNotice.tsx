6fa26879fb999cb7c4a603b0e02b6e2d0d9a8e12
Brandon Thomas <echelon@gmail.com>
2021-12-30 03:25:18 -0500

    Simplify FakeYou notice

--- fakeyou/src/v2/view/tts/tts_model_list/TtsModelListNotice.tsx
+++ fakeyou/src/v2/view/tts/tts_model_list/TtsModelListNotice.tsx
@@ -8,9 +8,6 @@ function TtsModelListNotice(props: Props) {
     <article className="message is-link">
     <div className="message-body">
       <em>Vocodes</em> is now <strong><em>FakeYou</em></strong>!
-      Why the change? Not many know what vocoders are, so the old name was kind of silly. 
-      We've also got lots of ambitious plans for the future, including a full set of virtual 
-      production and deepfake tools, so stay tuned.
     </div>
     </article>
   )  
