85a21efaf648f13b08dc423a1f38f94d1730a85a
Brandon Thomas <echelon@gmail.com>
2022-01-03 02:27:24 -0500

    Fix sqlx issue preventing builds
    Move to Rust workspace broke how OFFLINE mode db schema/query cache is
    read and stored.

--- codegen_database.sh
+++ codegen_database.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Necessary since we're now in a more complex Rust workspace
+pushd crates/app/storyteller_web
+
 SQLX_OFFLINE=true cargo sqlx prepare -- --bin storyteller-web
+
+popd
+
+mv crates/app/storyteller_web/sqlx-data.json . 
+
