5c3afb0e68fde7e5bc938a356d7c88f86ffced5c
Brandon Thomas <echelon@gmail.com>
2022-01-06 03:09:07 -0500

    note about migrations in prod

--- migrations/2021-08-25-010101_alter_tables_add_vocoder_type/up.sql
+++ migrations/2021-08-25-010101_alter_tables_add_vocoder_type/up.sql
@@ -6,6 +6,7 @@
 --  I'll have to repair the migrations table.
 --  version: 20210925010101 | run_on: 2021-08-25 03:01:15 (development)
 --  UPDATE __diesel_schema_migrations SET version=20210825010101 WHERE version=20210925010101 LIMIT 1;
+-- NB: Fixed in production on 2022-01-06
 
 ALTER TABLE tts_models
 ADD COLUMN maybe_default_pretrained_vocoder VARCHAR(64) DEFAULT NULL
