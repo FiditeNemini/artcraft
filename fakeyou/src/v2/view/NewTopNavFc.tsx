7ef3d002bab993222f09826cd40b8a5c50b30b33
Brandon Thomas <echelon@gmail.com>
2022-01-14 01:55:26 -0500

    looking good

--- fakeyou/src/v2/view/NewTopNavFc.tsx
+++ fakeyou/src/v2/view/NewTopNavFc.tsx
@@ -76,6 +76,7 @@ function NewTopNavFc(props: Props) {
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="field is-grouped">
+              {/*
               <p className="control">
                 <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
                   <span className="icon">
@@ -86,13 +87,17 @@ function NewTopNavFc(props: Props) {
                   </span>
                 </a>
               </p>
+              */}
+
               <p className="control">
+                {/*
                 <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip">
                   <span className="icon">
                     <i className="fas fa-download"></i>
                   </span>
                   <span>Download</span>
                 </a>
+                */}
 
                 <MigrationTopNavSession
                   sessionWrapper={props.sessionWrapper}
