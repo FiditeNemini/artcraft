313950f7b3a5b9ef6b862cbfb6150beab31968f6
Brandon Thomas <echelon@gmail.com>
2022-01-31 03:25:23 -0500

    remove cheers from text

--- crates/lib/twitch_common/src/cheers.rs
+++ crates/lib/twitch_common/src/cheers.rs
@@ -1,5 +1,32 @@
 use once_cell::sync::Lazy;
 use std::collections::HashSet;
+use regex::Regex;
+
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
+
+
+pub fn remove_cheers(text: &str) -> String {
+  return TWITCH_CHEER_REGEX.replace_all(text, "").to_string()
+}
+
+static TWITCH_CHEER_REGEX : Lazy<Regex> = Lazy::new(|| {
+  let mut regex_pieces  = Vec::new();
+
+  for cheer in TWITCH_CHEERS.iter() {
+    regex_pieces.push(format!("({}\\d+)", cheer));
+  }
+
+  let regex = regex_pieces.join("|");
+  let regex = format!("({})", regex);
+
+  Regex::new(&regex).unwrap()
+});
+
 
 /// These cheer emotes always combine with a number
 /// This list seems pretty comprehensive
@@ -45,3 +72,15 @@ static TWITCH_CHEERS : Lazy<HashSet<String>> = Lazy::new(|| {
   }
   s
 });
+
+#[cfg(test)]
+mod tests {
+  use crate::cheers::remove_cheers;
+
+  // TODO: REMOVE SPACES
+  #[test]
+  fn test_remove_cheers() {
+    let r = remove_cheers("testing Kappa1 SeemsGood100 removal");
+    assert_eq!(&r, "testing   removal"); // TODO: REMOVE SPACES
+  }
+}
