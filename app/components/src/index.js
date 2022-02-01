dbbf2566c8593f699b99faef5d1f8e46107259d0
Brandon Thomas <echelon@gmail.com>
2022-01-31 23:12:31 -0500

    WIP monorepo
    Following
    https://dev.to/ynwd/how-to-create-react-monorepo-with-npm-workspace-webpack-and-create-react-app-2dhn

---
+++ app/components/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from "react"
+
+const Header = ({ text = "" }) => {
+    return (
+        <h1>Shared header library {text}</h1>
+    )
+}
+
+export {
+    Header
+}
+
