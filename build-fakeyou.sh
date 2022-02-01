ddc17184e3fd6cf50654e7c9c745f6dc3093e315
Brandon Thomas <echelon@gmail.com>
2022-02-01 02:39:13 -0500

    ignore engines opt

--- build-fakeyou.sh
+++ build-fakeyou.sh
@@ -15,7 +15,8 @@ set -euxo pipefail
 #echo "Copying redirects configuration to Netlify build dir..."
 #cp _redirects fakeyou/build
 
-yarn build-fakeyou --silent --ignore-optional
+# --ignore-engines: https://stackoverflow.com/a/59615348
+yarn build-fakeyou --silent --ignore-optional --ignore-engines
 
 mkdir fakeyou
 mv packages/fakeyou/build/ fakeyou/build/
