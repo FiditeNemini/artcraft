d74c2f8cc9fff5e4470cae728167099646f856dc
Brandon Thomas <echelon@gmail.com>
2022-02-01 03:24:08 -0500

    cleanup build scripts

--- build-fakeyou.sh
+++ build-fakeyou.sh
@@ -2,19 +2,6 @@
 
 set -euxo pipefail
 
-## NB: Temporary for migration
-#ln -s fakeyou/ fakeyou
-#
-#pushd fakeyou/
-#
-#npm install
-#npm run-script build
-#
-#popd
-#
-#echo "Copying redirects configuration to Netlify build dir..."
-#cp _redirects fakeyou/build
-
 # --ignore-engines: https://stackoverflow.com/a/59615348
 yarn build-fakeyou --verbose --ignore-optional --ignore-engines
 
--- build-storyteller.sh
+++ build-storyteller.sh
@@ -2,23 +2,12 @@
 
 set -euxo pipefail
 
-## NB: Temporary for migration
-#ln -s fakeyou/ fakeyou
-#
-#pushd fakeyou/
-#
-#npm install
-#npm run-script build
-#
-#popd
-#
-#echo "Copying redirects configuration to Netlify build dir..."
-#cp _redirects fakeyou/build
-
-yarn build-storyteller
+# --ignore-engines: https://stackoverflow.com/a/59615348
+yarn build-storyteller --verbose --ignore-optional --ignore-engines
 
 mkdir storyteller
 mv packages/storyteller/build/ storyteller/build/
 
 echo "Copying redirects configuration to Netlify build dir..."
 cp _redirects storyteller/build/
+
