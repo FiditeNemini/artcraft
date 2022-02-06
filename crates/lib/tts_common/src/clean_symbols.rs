75461a49dff5793ee84e6ece01571e9036affb93
Brandon Thomas <echelon@gmail.com>
2022-02-05 22:40:04 -0500

    new small crate to get rid of smart quotes, etc

---
+++ crates/lib/tts_common/src/clean_symbols.rs
@@ -0,0 +1,39 @@
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use log::info;
+use unicode_segmentation::UnicodeSegmentation;
+
+static REPLACEMENTS : Lazy<HashMap<String, String>> = Lazy::new(|| {
+  let mut map = HashMap::new();
+  map.insert("’".to_string(), "'".to_string());
+  map.insert("‘".to_string(), "'".to_string());
+  map.insert("“".to_string(), "\"".to_string());
+  map.insert("”".to_string(), "\"".to_string());
+  map
+});
+
+pub fn clean_symbols(input_text: &str) -> String {
+  let segmented= UnicodeSegmentation::graphemes(input_text, true)
+      .map(|segment| {
+        if let Some(replace) = REPLACEMENTS.get(segment) {
+          return replace.as_str();
+        }
+        return segment;
+      })
+      .collect::<Vec<&str>>();
+
+  segmented.join("")
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::clean_symbols::clean_symbols;
+
+  #[test]
+  fn filters_smart_quotes() {
+    assert_eq!(clean_symbols("That’s ok"), "That's ok".to_string());
+    assert_eq!(clean_symbols("That’s it’s new home"), "That's it's new home".to_string());
+    assert_eq!(clean_symbols("‘foo’"), "'foo'".to_string());
+    assert_eq!(clean_symbols("“as ‘shown’ here.”"), "\"as 'shown' here.\"".to_string());
+  }
+}
