cdc04b66d7a54e376a128f5f6b202644ad2c73b5
Brandon Thomas <echelon@gmail.com>
2022-02-18 19:13:52 -0500

    get twitch event rule for user

--- crates/lib/database_queries/src/twitch_event_rules/mod.rs
+++ crates/lib/database_queries/src/twitch_event_rules/mod.rs
@@ -1,4 +1,5 @@
 pub mod delete_twitch_event_rule;
+pub mod get_twitch_event_rule_for_user;
 pub mod insert_twitch_event_rule_builder;
+pub mod list_twitch_event_rules_for_user;
 pub mod update_twitch_event_rule_builder;
-mod list_twitch_event_rules;
