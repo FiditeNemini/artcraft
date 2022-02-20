f1a9def628d3109e5428d1c1a10e7dbe0ffc9251
Brandon Thomas <echelon@gmail.com>
2022-02-19 22:19:51 -0500

    event response and predicate

---
+++ crates/service/storyteller_web/src/complex_models/event_match_predicate.rs
@@ -0,0 +1,41 @@
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum EventMatchPredicate {
+  /// A predicate for an exactly matched cheermote name, including the bit value,
+  /// eg "Cheer1". Case insensitive.
+  BitsCheermoteNameExactMatch {
+    cheermote_name: String,
+  },
+  /// A predicate for a cheermote name without value, but with a minimum bits
+  /// threshold. eg "Cheer", minimum=150 bits. Case insensitive.
+  BitsCheermoteNameSpendThreshold {
+    cheermote_name_prefix: String,
+    minimum_bits_spent: u32,
+  },
+  BitsSpendThreshold {
+    minimum_bits_spent: u32,
+  },
+  ChannelPointsRewardNameExactMatch {
+    reward_name: String,
+  },
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::complex_models::event_match_predicate::EventMatchPredicate;
+
+  #[test]
+  fn bits_cheermote_name_exact_match() {
+    let rust_value = EventMatchPredicate::BitsCheermoteNameExactMatch {
+      cheermote_name: "Cheer1".to_string()
+    };
+    let json = "{\"bits_cheermote_name_exact_match\":{\"cheermote_name\":\"Cheer1\"}}";
+
+    let converted_to_json= serde_json::to_string(&rust_value).unwrap();
+    assert_eq!(&converted_to_json, json);
+
+    let converted_from_json : EventMatchPredicate = serde_json::from_str(json).unwrap();
+    assert_eq!(&converted_from_json, &rust_value);
+  }
+}
---
+++ crates/service/storyteller_web/src/complex_models/event_responses.rs
@@ -0,0 +1,32 @@
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum EventResponse {
+  /// Respond with a single TTS voice.
+  TtsSingleVoice {
+    tts_model_token: String,
+  },
+  /// Respond with a random TTS voice.
+  TtsRandomVoice {
+    tts_model_tokens: Vec<String>,
+  },
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::complex_models::event_responses::EventResponse;
+
+  #[test]
+  fn tts_single_voice() {
+    let rust_value = EventResponse::TtsSingleVoice {
+      tts_model_token: "foo".to_string()
+    };
+    let json = "{\"tts_single_voice\":{\"tts_model_token\":\"foo\"}}";
+
+    let converted_to_json= serde_json::to_string(&rust_value).unwrap();
+    assert_eq!(&converted_to_json, json);
+
+    let converted_from_json : EventResponse = serde_json::from_str(json).unwrap();
+    assert_eq!(&converted_from_json, &rust_value);
+  }
+}
