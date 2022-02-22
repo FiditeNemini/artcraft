d8d2d2845779a8e6b6d198d434f24966afd10ec3
Brandon Thomas <echelon@gmail.com>
2022-02-22 02:33:37 -0500

    something of a UI

--- packages/storyteller/src/pages/tts_configs/TtsConfigsIndexPage.tsx
+++ packages/storyteller/src/pages/tts_configs/TtsConfigsIndexPage.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { SessionWrapper } from '@storyteller/components/src/session/SessionWrapper';
 import { ListTwitchEventRules, ListTwitchEventRulesIsError, ListTwitchEventRulesIsOk, TwitchEventRule } from '@storyteller/components/src/api/storyteller/twitch_event_rules/ListTwitchEventRules';
+import { TwitchEventRuleElement } from './TwitchEventRuleElement';
 
 interface Props {
   sessionWrapper: SessionWrapper,
@@ -28,9 +29,55 @@ function TtsConfigsIndexPage(props: Props) {
   }
 
   return (
-    <div>
-      <h1> TTS Setup </h1>
-    </div>
+    <>
+      <div className="section">
+        <h1 className="title"> TTS Setup </h1>
+        <h2 className="subtitle"> Configure how your stream TTS works </h2>
+      </div>
+      <div className="content">
+        <h1 className="title"> Bits / Cheers </h1>
+        <h2 className="subtitle is-4">
+          (These settings are best for small and medium-sized channels.)
+        </h2>
+        <p>
+          You can create rules for matching cheers. When someone cheers, these rules will be tested in order. 
+          The first match wins, and the action the rule specifies will be taken.
+        </p>
+        {twitchEventRules.map(rule => {
+          return <TwitchEventRuleElement rule={rule} />
+        })}
+      </div>
+      <div className="content">
+        <h1 className="title"> Channel Points / Rewards </h1>
+        <h2 className="subtitle is-4">
+          (These settings are best for small channels.)
+        </h2>
+        <p>
+          You can create rules for matching channel point reward redemptions. When a reward is redeemed, 
+          it will be tested against these rules in order. The first match wins, and the action the rule 
+          specifies will be taken.
+        </p>
+        {twitchEventRules.map(rule => {
+          return <TwitchEventRuleElement rule={rule} />
+        })}
+      </div>
+      <div className="content">
+        <h1 className="title"> Donation Services (Streamlabs, StreamElements, etc.) </h1>
+        <h2 className="subtitle is-4">
+          (These settings are great for large channels, but work for channels of all sizes.)
+        </h2>
+        <p>Coming soon&hellip;</p>
+      </div>
+      <div className="content">
+        <h1 className="title"> Text <code>/slash</code> Commands</h1>
+        <h2 className="subtitle is-4">
+          (These settings are great for very small channels.)
+        </h2>
+        <p>Coming soon&hellip;</p>
+      </div>
+      <br />
+      <br />
+    </>
   )
 }
 
---
+++ packages/storyteller/src/pages/tts_configs/TwitchEventRuleElement.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { TwitchEventRule } from '@storyteller/components/src/api/storyteller/twitch_event_rules/ListTwitchEventRules';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowUp, faRulerVertical } from '@fortawesome/free-solid-svg-icons';
+
+interface Props {
+  rule: TwitchEventRule,
+}
+
+function TwitchEventRuleElement(props: Props) {
+
+  let title = "Not Set"
+  if (props.rule.event_match_predicate.bits_cheermote_name_exact_match !== undefined) {
+    title = "Cheermote Name Matches"
+  } else if (props.rule.event_match_predicate.bits_cheermote_prefix_spend_threshold !== undefined) {
+    title = "Cheermote Prefix Matches and Spend Threshold"
+  } else if (props.rule.event_match_predicate.bits_spend_threshold !== undefined) {
+    title = "Spend Threshold"
+  }
+
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <p className="title is-5">
+            {title}
+          </p>
+          <p className="subtitle is-5">
+            Subtitle
+          </p>
+        </div>
+        <footer className="card-footer">
+          <p className="card-footer-item">
+            <span className="icon">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </span>
+          </p>
+          <p className="card-footer-item">
+            <span className="icon">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </span>
+          </p>
+          <p className="card-footer-item">
+            <span>
+              Edit
+            </span>
+          </p>
+          <p className="card-footer-item">
+            <span>
+              Delete
+            </span>
+          </p>
+        </footer>
+      </div>
+      <br />
+    </div>
+  )
+}
+
+export { TwitchEventRuleElement }
