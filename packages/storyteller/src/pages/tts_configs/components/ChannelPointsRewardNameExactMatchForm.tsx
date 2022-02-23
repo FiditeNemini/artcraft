1555a696390ac531579f48277c043ef7622598c1
Brandon Thomas <echelon@gmail.com>
2022-02-23 18:44:44 -0500

    fixed bug

--- packages/storyteller/src/pages/tts_configs/components/ChannelPointsRewardNameExactMatchForm.tsx
+++ packages/storyteller/src/pages/tts_configs/components/ChannelPointsRewardNameExactMatchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface ChannelPointsRewardNameExactMatchProps {
   rewardName: string,
@@ -6,7 +6,15 @@ interface ChannelPointsRewardNameExactMatchProps {
 };
 
 function ChannelPointsRewardNameExactMatchForm(props: ChannelPointsRewardNameExactMatchProps) {
-  const [rewardName, setRewardName] = useState<string>(props.rewardName);
+  const [rewardName, setRewardName] = useState<string>('');
+
+  // NB: useState is not always setting from props correctly (after several re-renders)
+  // The following answers suggests using useEffect:
+  //  https://stackoverflow.com/a/54866051 (less clear by also using useState(), but good comments)
+  //  https://stackoverflow.com/a/62982753
+  useEffect(() => {
+    setRewardName(props.rewardName);
+  }, [props.rewardName]);
 
   const updateRewardName = (ev: React.FormEvent<HTMLInputElement>) : boolean => {
     const value = (ev.target as HTMLInputElement).value;
@@ -14,7 +22,6 @@ function ChannelPointsRewardNameExactMatchForm(props: ChannelPointsRewardNameExa
     props.updateRewardName(value);
     return true;
   }
-  console.log(rewardName, props.rewardName)
 
   return (
     <>
