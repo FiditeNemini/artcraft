c241312358d698838208c770d0806ea9a3a4df93
Brandon Thomas <echelon@gmail.com>
2022-03-18 08:45:50 -0400

    wip

--- packages/storyteller-home/src/pages/index/IndexPage.tsx
+++ packages/storyteller-home/src/pages/index/IndexPage.tsx
@@ -72,6 +72,43 @@ function IndexPage () {
         </h2>
       </section>
 
+      <section className="hero is-small">
+        <div className="hero-body">
+          <div className="columns is-vcentered">
+
+            <div className="column">
+              <p className="title is-3">
+                2) <em>Storyteller TTS for Twitch</em>
+              </p>
+              <p className="subtitle is-5">
+                Twitch Streamers and Creators can engage and monetize
+              </p>
+              <p>
+                It's tough to build an audience on Twitch. It's even tougher to earn an
+                income. We've built the most engaging donation system for Twitch to date,
+                letting audience members pay to use Deep Fake voices and emotes in their
+                favorite streamers' live broadcasts. 
+              </p>
+            </div>
+
+            <div className="column is-one-third">
+              <div className="mascot">
+                <img src="/mascot/kitsune_pose6_black_2000.webp" alt="FakeYou's mascot!" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section is-small">
+        <h1 className="title is-4">Get started with your channel!</h1>
+        <h2 className="subtitle is-6">
+          There's nothing to install. It's the easiest, most engaging, most fun system for Twitch yet.
+          And it earns <em>you</em> money.
+        </h2>
+      </section>
+
+
 
     </div>
   );
