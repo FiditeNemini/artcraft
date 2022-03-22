07923bb34f22adfd8fcaf75a6aea86df91ec3d4d
Brandon Thomas <echelon@gmail.com>
2022-03-21 23:55:59 -0400

    twitch event logs schema wip

--- _migrations/2022-03-14-035745_twitch_event_match_logs/up.sql
+++ _migrations/2022-03-14-035745_twitch_event_match_logs/up.sql
@@ -1,1 +1,20 @@
--- Your SQL goes here
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+-- Log any event matching (bits, channel points, etc.)
+CREATE TABLE twitch_event_match_logs(
+  -- Not used for anything except replication.
+  id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+  -- Type of event, eg.
+  event_type VARCHAR(32) NOT NULL,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL,
+
+  -- INDICES --
+  PRIMARY KEY (id)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
