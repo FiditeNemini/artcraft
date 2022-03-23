4d54041c02c95f14db91605251cf2a33f9994dfd
Brandon Thomas <echelon@gmail.com>
2022-03-23 00:38:59 -0400

    fix build error

--- packages/fakeyou/src/v2/view/tts/tts_model_list/TtsModelListFc.tsx
+++ packages/fakeyou/src/v2/view/tts/tts_model_list/TtsModelListFc.tsx
@@ -284,7 +284,7 @@ function TtsModelListFc(props: Props) {
 
       {vocodesNotice}
 
-      {/* twitchTtsNotice */}
+      {twitchTtsNotice}
 
       <form onSubmit={handleFormSubmit} className="main-form">
 
