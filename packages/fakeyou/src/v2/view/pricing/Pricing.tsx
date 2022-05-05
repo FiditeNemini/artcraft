11b12a1e724c6719f8f8cbcb9fa1ca5dab540a01
Angelica Chung <angelica.ramos.91@gmail.com>
2022-05-05 16:53:06 -0400

    nowhere near  done, but a safe place to check in until tomorrow

--- packages/fakeyou/src/v2/view/pricing/Pricing.tsx
+++ packages/fakeyou/src/v2/view/pricing/Pricing.tsx
@@ -13,7 +13,9 @@ interface Props {
 }
 
 function PricingPage(props: Props) {
-  const PRICE_LIST = FAKEYOU_PRICES
+  const FREE = FAKEYOU_PRICES.free
+  const BASIC = FAKEYOU_PRICES.basic
+  const PRO = FAKEYOU_PRICES.pro
 
   console.log(PRICE_LIST)
 
@@ -43,22 +45,82 @@ function PricingPage(props: Props) {
           </thead>
 
           <tbody>
+            {/* Price */}
             <tr>
               <td className="has-text-weight-bold">Price</td>
-              <td>${PRICE_LIST.free.price}/month</td>
-              <td>${PRICE_LIST.basic.price}/month</td>
-              <td>${PRICE_LIST.pro.price}/month</td>
+              <td>${FREE.price}/month</td>
+              <td>${BASIC.price}/month</td>
+              <td>${PRO.price}/month</td>
             </tr>
 
+            {/* Extended Audio */}
             <tr>
               <td>Extended Audio</td>
               <td>
-                <FontAwesomeIcon icon={PRICE_LIST.free.features.extendedAudio ? TRUE : FALSE} className={`fa-3x ${PRICE_LIST.free.features.extendedAudio ? "has-text-success" : "has-text-danger"}`}
+                <FontAwesomeIcon icon={FREE.features.extendedAudio ? TRUE : FALSE} className={`fa-3x ${FREE.features.extendedAudio ? "has-text-success" : "has-text-danger"}`}
                 />
               </td>
-              <td>{PRICE_LIST.basic.features.extendedAudio ? "Yes" : "No"}</td>
-              <td>{PRICE_LIST.pro.features.extendedAudio ? "Yes" : "No"}</td>
+              <td>
+                <FontAwesomeIcon icon={BASIC.features.extendedAudio ? TRUE : FALSE} className={`fa-3x ${BASIC.features.extendedAudio ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={PRO.features.extendedAudio ? TRUE : FALSE} className={`fa-3x ${PRO.features.extendedAudio ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
             </tr>
+
+            {/* mp3 */}
+            <tr>
+              <td>mp3 Format</td>
+              <td>
+                <FontAwesomeIcon icon={FREE.features.mp3 ? TRUE : FALSE} className={`fa-3x ${FREE.features.mp3 ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={BASIC.features.mp3 ? TRUE : FALSE} className={`fa-3x ${BASIC.features.mp3 ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={PRO.features.mp3 ? TRUE : FALSE} className={`fa-3x ${PRO.features.mp3 ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+            </tr>
+
+            {/* Priority Processing */}
+            <tr>
+              <td>Priority Processing</td>
+              <td>
+                <FontAwesomeIcon icon={FREE.features.priorityProcessing ? TRUE : FALSE} className={`fa-3x ${FREE.features.priorityProcessing ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={BASIC.features.priorityProcessing ? TRUE : FALSE} className={`fa-3x ${BASIC.features.priorityProcessing ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={PRO.features.priorityProcessing ? TRUE : FALSE} className={`fa-3x ${PRO.features.priorityProcessing ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+            </tr>
+
+            {/* Commercial Voices */}
+            <tr>
+              <td>Commercial Voices</td>
+              <td>
+                <FontAwesomeIcon icon={FREE.features.commercialVoices ? TRUE : FALSE} className={`fa-3x ${FREE.features.commercialVoices ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={BASIC.features.commercialVoices ? TRUE : FALSE} className={`fa-3x ${BASIC.features.commercialVoices ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+              <td>
+                <FontAwesomeIcon icon={PRO.features.commercialVoices ? TRUE : FALSE} className={`fa-3x ${PRO.features.commercialVoices ? "has-text-success" : "has-text-danger"}`}
+                />
+              </td>
+            </tr>
+
           </tbody>
         </table>
       </div>
