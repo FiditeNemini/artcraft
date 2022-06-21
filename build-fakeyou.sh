12514dbcd55208684c1771bb559e5495edf46745
Brandon Thomas <echelon@gmail.com>
2022-06-20 23:16:32 -0400

    This seems to be the correct way to not cache the build SHA

--- build-fakeyou.sh
+++ build-fakeyou.sh
@@ -4,6 +4,8 @@
 set -euxo pipefail
 
 # Add the GIT SHA to the build
+# This must be done before everything else, or it will get cached with the build.
+# (This might be making the builds less performant?)
 # COMMIT_REF is defined by Netlify to be the commit SHA
 # We want a short 8 character reference.
 echo "Labelling build with short SHA..."
