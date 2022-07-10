ab549ab2842e6a09ef615b970348af62612e36f3
Brandon Thomas <echelon@gmail.com>
2022-07-10 00:48:29 -0400

    query improvement, fixes

--- analytics/most_popular_tts_models_time_period.sql
+++ analytics/most_popular_tts_models_time_period.sql
@@ -6,19 +6,25 @@
 select m.token, m.title, m.ietf_language_tag, r.use_count from (
     select model_token, count(*) as use_count
     from tts_results
-    where created_at < now() - interval 30 day
+    where created_at > ( CURDATE() - INTERVAL 30 DAY )
     group by model_token
+    order by use_count desc
+    limit 100
 ) as r
     join tts_models as m
     on m.token = r.model_token;
 
--- All models by use count, last 5 days
-select m.token, m.title, m.ietf_language_tag, r.use_count from (
+-- Most models by use count, last n days
+-- Limited due to tmux scrollback.
+select m.token, m.title, m.ietf_language_tag, u.username, r.use_count from (
     select model_token, count(*) as use_count
     from tts_results
-    where created_at < now() - interval 5 day
+    where created_at > ( CURDATE() - INTERVAL 5 DAY )
     group by model_token
-    order by use_count desc limit 100
 ) as r
     join tts_models as m
-    on m.token = r.model_token;
+    on m.token = r.model_token
+    join users as u
+    on u.token = m.creator_user_token
+    order by r.use_count desc
+    limit 500;
