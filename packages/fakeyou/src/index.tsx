6ebf4b1e2d5e984883d4e6ac7f4cf90b4dbda5b4
Brandon Thomas <echelon@gmail.com>
2022-07-11 00:50:39 -0400

    Attempt to fix styles with additional class nesting

--- packages/fakeyou/src/index.tsx
+++ packages/fakeyou/src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import AppTranslated from './AppTranslated';
+import { USE_REFRESH } from './Refresh';
 
 const IS_IOS : boolean = /iPad|iPhone|iPod/.test(navigator.platform || "");
 
 const enableSpectrograms = !IS_IOS;
 
 const flashVocodesNotice = new URLSearchParams(window.location.search).has('vocodes');
+
+const designSystemClass = USE_REFRESH ? 'fakeyou-refresh' : 'fakeyou-old';
+
+document.getElementsByTagName('html')[0].classList.add(designSystemClass);
 
 ReactDOM.render(
   <React.StrictMode>
