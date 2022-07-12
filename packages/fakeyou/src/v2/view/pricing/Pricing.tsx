b5262be3a0b51f9fdebabf2a9a11f40ea5e8b645
BFlat <bombayapirat@gmail.com>
2022-07-12 20:01:52 +0100

    pricing page update

--- packages/fakeyou/src/v2/view/pricing/Pricing.tsx
+++ packages/fakeyou/src/v2/view/pricing/Pricing.tsx
@@ -1,97 +1,230 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoneyBillWave, faCheckCircle as TRUE, faTimesCircle as FALSE } from '@fortawesome/free-solid-svg-icons';
-import { SessionWrapper } from '@storyteller/components/src/session/SessionWrapper';
-import { FAKEYOU_PRICES as FYP, STORYTELLER_PRICES as STP } from '@storyteller/fakeyou/src/data/PriceTiers'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import {
+  faMoneyBillWave,
+  faCheckCircle as TRUE,
+  faTimesCircle as FALSE,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { SessionWrapper } from "@storyteller/components/src/session/SessionWrapper";
+import {
+  FAKEYOU_PRICES as FYP,
+  STORYTELLER_PRICES as STP,
+} from "@storyteller/fakeyou/src/data/PriceTiers";
 
 interface Props {
-  sessionWrapper: SessionWrapper,
-  querySessionCallback: () => void,
+  sessionWrapper: SessionWrapper;
+  querySessionCallback: () => void;
 }
 
 function PricingPage(props: Props) {
   function capitalize(word: string) {
-    return word.charAt(0).toUpperCase() + word.slice(1)
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
-  const [FY_TIERS, ST_TIERS] = [
-    Object.keys(FYP),
-    Object.keys(STP)
-  ]
+  const [FY_TIERS, ST_TIERS] = [Object.keys(FYP), Object.keys(STP)];
 
-  const [FY_FEATURES, ST_FEATURES] = [Object.keys(FYP.free.features), Object.keys(STP.free.features)]
+  const [FY_FEATURES, ST_FEATURES] = [
+    Object.keys(FYP.free.features),
+    Object.keys(STP.free.features),
+  ];
 
-  let current_tiers: string[], current_features: string[], current_list: any
+  let current_tiers: string[], current_features: string[], current_list: any;
 
-  const site = window.location.hostname
+  const site = window.location.hostname;
 
   switch (site) {
-    case 'fakeyou':
-      [current_tiers, current_features, current_list] = [FY_TIERS, FY_FEATURES, FYP];
-      break
-    case 'storyteller':
-      [current_tiers, current_features, current_list] = [ST_TIERS, ST_FEATURES, STP];
-      break
+    case "fakeyou":
+      [current_tiers, current_features, current_list] = [
+        FY_TIERS,
+        FY_FEATURES,
+        FYP,
+      ];
+      break;
+    case "storyteller":
+      [current_tiers, current_features, current_list] = [
+        ST_TIERS,
+        ST_FEATURES,
+        STP,
+      ];
+      break;
     default:
-      [current_tiers, current_features, current_list] = [FY_TIERS, FY_FEATURES, FYP];
-      break
+      [current_tiers, current_features, current_list] = [
+        FY_TIERS,
+        FY_FEATURES,
+        FYP,
+      ];
+      break;
   }
-
 
   return (
     <div>
-      <h1 className="title is-1">
-        Pricing
-      </h1>
+      <div className="container py-5 text-center">
+        <h1 className="display-5 fw-bold">FakeYou Pricing</h1>
+      </div>
 
-      <div className="table-container">
-        <table className="table is-striped is-narrow is-hoverable is-fullwidth">
-          <thead>
-            <tr>
-              <th className="is-flex is-align-items-center is-justify-content-center">
-                <FontAwesomeIcon icon={faMoneyBillWave} className="fa-3x" />
-              </th>
+      <div className="container mt-3">
+        <div className="row gy-4">
+          <div className="col-12 col-lg-4">
+            <div className="rounded panel p-4 h-100">
+              <h2 className="text-center my-2">Free</h2>
+              <p className="mb-4 text-center">Great for hobbyists</p>
+              <Link to="/" className="btn btn-secondary w-100 fs-5">
+                Create free TTS
+              </Link>
+              <h2 className="display-5 fw-bold text-center my-5">
+                ${current_list.free.price}
+                <span className="fs-5 opacity-75 fw-normal"> /month</span>
+              </h2>
+              <ul className="pricing-list d-flex flex-column gap-3">
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Unlimited TTS Generation
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Up to 10 seconds audio
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-lg-4">
+            <div className="rounded panel p-4 panel-border h-100">
+              <h2 className="text-center my-2">Basic</h2>
+              <p className="mb-4 text-center">Great for hobbyists</p>
+              <Link to="/" className="btn btn-primary w-100 fs-5">
+                Buy Basic
+              </Link>
+              <h2 className="display-5 fw-bold text-center my-5">
+                ${current_list.basic.price}
+                <span className="fs-5 opacity-75 fw-normal"> /month</span>
+              </h2>
+              <ul className="pricing-list d-flex flex-column gap-3">
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Unlimited TTS Generation
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Up to 30 seconds audio
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Priority processing
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-lg-4">
+            <div className="rounded panel p-4 h-100">
+              <h2 className="text-center my-2">Pro</h2>
+              <p className="mb-4 text-center">Great for hobbyists</p>
+              <Link to="/" className="btn btn-primary w-100 fs-5">
+                Buy Pro
+              </Link>
+              <h2 className="display-5 fw-bold text-center my-5">
+                ${current_list.pro.price}
+                <span className="fs-5 opacity-75 fw-normal"> /month</span>
+              </h2>
+              <ul className="pricing-list d-flex flex-column gap-3">
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Unlimited TTS Generation
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Up to 1 minute audio
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Priority processing
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  Generate mp3 file
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCheck} className="text-red me-3" />
+                  For Commercial Use
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
-              {
-                current_tiers.map(e => { return <th key={e} className="has-text-weight-bold is-size-1-desktop is-size-4-tablet is-size-6-mobile">{capitalize(e)}</th> })
-              }
-            </tr>
-          </thead>
+      <div className="container-panel mt-5 py-5">
+        <div className="panel p-4">
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="">Feature List</th>
 
-          <tbody>
-            <tr>
-              <td className="has-text-weight-bold">Price</td>
-              <td>${current_list.free.price}/month</td>
-              <td>${current_list.basic.price}/month</td>
-              <td>${current_list.pro.price}/month</td>
-            </tr>
+                {current_tiers.map((e) => {
+                  return (
+                    <th key={e} className="">
+                      {capitalize(e)}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
 
-            {
-              current_features.map((e, i) => {
+            <tbody>
+              <tr>
+                <td className="has-text-weight-bold">Price</td>
+                <td>${current_list.free.price}/month</td>
+                <td>${current_list.basic.price}/month</td>
+                <td>${current_list.pro.price}/month</td>
+              </tr>
+
+              {current_features.map((e, i) => {
                 return (
                   <tr key={i}>
-                    <td >{e}</td>
+                    <td>{e}</td>
 
                     <td>
-                      <FontAwesomeIcon icon={current_list.free.features[e] ? TRUE : FALSE} className={`fa-3x ${current_list.free.features[e] ? "has-text-success" : "has-text-danger"}`} />
+                      <FontAwesomeIcon
+                        icon={current_list.free.features[e] ? TRUE : FALSE}
+                        className={`fa-2x ${
+                          current_list.free.features[e]
+                            ? "has-text-success"
+                            : "has-text-danger"
+                        }`}
+                      />
                     </td>
 
                     <td>
-                      <FontAwesomeIcon icon={current_list.basic.features[e] ? TRUE : FALSE} className={`fa-3x ${current_list.basic.features[e] ? "has-text-success" : "has-text-danger"}`} />
+                      <FontAwesomeIcon
+                        icon={current_list.basic.features[e] ? TRUE : FALSE}
+                        className={`fa-2x ${
+                          current_list.basic.features[e]
+                            ? "has-text-success"
+                            : "has-text-danger"
+                        }`}
+                      />
                     </td>
 
                     <td>
-                      <FontAwesomeIcon icon={current_list.pro.features[e] ? TRUE : FALSE} className={`fa-3x ${current_list.pro.features[e] ? "has-text-success" : "has-text-danger"}`} />
+                      <FontAwesomeIcon
+                        icon={current_list.pro.features[e] ? TRUE : FALSE}
+                        className={`fa-2x ${
+                          current_list.pro.features[e]
+                            ? "has-text-success"
+                            : "has-text-danger"
+                        }`}
+                      />
                     </td>
                   </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export { PricingPage };
