dc410c5aae2b72cba2885848850f04791e07d842
Brandon Thomas <echelon@gmail.com>
2022-07-27 06:19:37 -0400

    Add spanish_v2 as "Old English Arpabet" text pipeline

--- packages/components/src/constants/TextPipeline.ts
+++ packages/components/src/constants/TextPipeline.ts
@@ -3,9 +3,9 @@
 const TEXT_PIPELINE_NAMES : Map<string, string> = new Map([
   //["legacy_vocodes", "Legacy Vocodes"],
   ["legacy_fakeyou", "Legacy FakeYou (grapheme-focused)"],
+  ["spanish_v2", "English v0 (Old Arpabet)"], // NB: Tricksy.
   ["english_v1", "English v1 (Arpabet)"],
   //["spanish_v1", "Spanish v1"],
-  //["spanish_v2", "Spanish v2"],
 ]);
 
 export { TEXT_PIPELINE_NAMES }
