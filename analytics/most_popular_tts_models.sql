7c97e81830145f2414b56e8a49c18327ac5cbb25
Brandon Thomas <echelon@gmail.com>
2022-07-27 03:11:21 -0400

    new model analytics query

--- analytics/most_popular_tts_models.sql
+++ analytics/most_popular_tts_models.sql
@@ -43,3 +43,21 @@ select m.token, m.title, m.ietf_language_tag, u.username, r.use_count from (
     on u.token = m.creator_user_token
 order by r.use_count desc
     limit 500;
+
+--
+-- Top 100 models by use count, last 5 days, not spanish, etc.
+-- Limited due to tmux scrollback.
+--
+select m.token, m.title, m.ietf_language_tag, u.username, r.use_count from (
+    select model_token, count(*) as use_count
+    from tts_results
+    where created_at > ( CURDATE() - INTERVAL 5 DAY )
+    group by model_token
+) as r
+    join tts_models as m
+    on m.token = r.model_token
+    join users as u
+    on u.token = m.creator_user_token
+    where m.ietf_language_tag NOT IN ('es', 'es-419', 'es-ES', 'es-MX', 'pt-BR')
+order by r.use_count desc
+    limit 100;
