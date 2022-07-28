fdf8a92cd8312b6ee0d631ab42d777cc5b1ed4b6
Brandon Thomas <echelon@gmail.com>
2022-07-28 02:26:12 -0400

    analytics queries

--- analytics/most_popular_tts_models.sql
+++ analytics/most_popular_tts_models.sql
@@ -48,7 +48,16 @@ order by r.use_count desc
 -- Top 100 models by use count, last 5 days, not spanish, etc.
 -- Limited due to tmux scrollback.
 --
-select m.token, m.title, m.ietf_language_tag, u.username, r.use_count from (
+select
+    m.token,
+    m.title,
+    m.ietf_language_tag,
+    u.username,
+    r.use_count,
+    m.user_deleted_at,
+    m.mod_deleted_at,
+    m.created_at
+from (
     select model_token, count(*) as use_count
     from tts_results
     where created_at > ( CURDATE() - INTERVAL 5 DAY )
@@ -59,5 +68,33 @@ select m.token, m.title, m.ietf_language_tag, u.username, r.use_count from (
     join users as u
     on u.token = m.creator_user_token
     where m.ietf_language_tag NOT IN ('es', 'es-419', 'es-ES', 'es-MX', 'pt-BR')
+order by r.use_count desc
+    limit 100;
+
+--
+-- Use count over 5 day window, single language, single user.
+--
+select
+    m.token,
+    m.title,
+    m.ietf_language_tag,
+    u.username,
+    r.use_count,
+    m.user_deleted_at,
+    m.mod_deleted_at,
+    m.created_at
+from (
+    select model_token, count(*) as use_count
+    from tts_results
+    where created_at > ( CURDATE() - INTERVAL 5 DAY )
+    group by model_token
+) as r
+    join tts_models as m
+    on m.token = r.model_token
+    join users as u
+    on u.token = m.creator_user_token
+    where
+        m.ietf_language_tag NOT IN ('es', 'es-419', 'es-ES', 'es-MX', 'pt-BR')
+        AND u.username = 'vegito1089'
 order by r.use_count desc
     limit 100;
