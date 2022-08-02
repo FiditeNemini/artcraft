cb95840982ef22002d8f76b4757c1ba103c9a4d4
Angelica Chung <angelica.ramos.91@gmail.com>
2022-08-02 11:01:30 -0400

    particles

--- packages/fakeyou/src/Particles.tsx
+++ packages/fakeyou/src/Particles.tsx
@@ -6,14 +6,32 @@ import particlesOptions from "./particles.json";
 import { ISourceOptions } from "tsparticles-engine";
 
 function ParticlesBG() {
+  let options = particlesOptions
+
+  function checkDownloadSpeed() {
+    const startTime = (new Date()).getTime();
+    let endTime: any;
+
+    window.onload = function () {
+      endTime = (new Date()).getTime()
+
+      if ((endTime - startTime) < 500) options.particles.number.value = 70
+    }
+
+    window.onerror = function (err, msg) {
+      console.log('error checking speed')
+    }
+  }
+
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
+    checkDownloadSpeed();
   }, []);
 
   return (
     <div id="particles-container">
       <Particles
-        options={particlesOptions as ISourceOptions}
+        options={options as ISourceOptions}
         init={particlesInit}
       />
     </div>
