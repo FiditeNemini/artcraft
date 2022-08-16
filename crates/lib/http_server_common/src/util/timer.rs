4bd0e09e7dea844b6ec59577967c2d6e54ba6a6d
Brandon Thomas <echelon@gmail.com>
2022-08-16 01:18:47 -0400

    Move user profile query to query lib

--- crates/lib/http_server_common/src/util/timer.rs
+++ crates/lib/http_server_common/src/util/timer.rs
@@ -1,8 +1,41 @@
 use std::future::Future;
+use std::time::{Instant, Duration};
 
-pub async fn time_section<F, Fut, T>(callback: F) -> T
-  where F: Fn() -> Fut,
-        Fut: Future<Output = T>,
-{
-  callback().await
+pub struct MultiBenchmarkingTimer {
+  recorded_sections: Vec<SectionTime>,
+}
+
+#[derive(Clone, Serialize)]
+pub struct SectionTime {
+  pub section_name: String,
+  pub duration: Duration,
+}
+
+impl MultiBenchmarkingTimer {
+
+  pub fn new() -> Self {
+    Self {
+      recorded_sections: Vec::new(),
+    }
+  }
+
+  pub async fn time_async_section<F, Fut, T>(
+    &mut self,
+    section_name: &str,
+    callback: F
+  ) -> T
+    where F: Fn() -> Fut,
+          Fut: Future<Output = T>,
+  {
+    let before = Instant::now();
+    let result = callback().await;
+    let after = Instant::now();
+
+    self.recorded_sections.push(SectionTime {
+      section_name: section_name.to_string(),
+      duration: after.duration_since(before),
+    });
+
+    result
+  }
 }
--- crates/service/storyteller_web/src/http_server/endpoints/users/get_profile.rs
+++ crates/service/storyteller_web/src/http_server/endpoints/users/get_profile.rs
@@ -10,13 +10,14 @@ use anyhow::anyhow;
 use chrono::{DateTime, Utc};
 use crate::http_server::web_utils::response_error_helpers::to_simple_json_error;
 use crate::server_state::ServerState;
-use database_queries::column_types::record_visibility::RecordVisibility;
-use database_queries::helpers::boolean_converters::i8_to_bool;
+use database_queries::queries::users::get_user_profile_by_username::{get_user_profile_by_username, UserProfileResult};
 use database_queries::queries::users::user_badges::list_user_badges::UserBadgeForList;
 use database_queries::queries::users::user_badges::list_user_badges::list_user_badges;
+use http_server_common::util::timer::MultiBenchmarkingTimer;
 use log::{info, warn, log};
 use md5::{Md5, Digest};
 use regex::Regex;
+use reusable_types::entity_visibility::EntityVisibility;
 use sqlx::MySqlPool;
 use sqlx::error::DatabaseError;
 use sqlx::error::Error::Database;
@@ -24,7 +25,6 @@ use sqlx::mysql::MySqlDatabaseError;
 use std::fmt;
 use std::sync::Arc;
 use time::Instant;
-use http_server_common::util::timer::time_section;
 
 // TODO: This is duplicated in query_user_profile
 // TODO: This handler has embedded queries.
@@ -39,8 +39,8 @@ pub struct UserProfileRecordForResponse {
   pub profile_rendered_html: String,
   pub user_role_slug: String,
   pub disable_gravatar: bool,
-  pub preferred_tts_result_visibility: RecordVisibility,
-  pub preferred_w2l_result_visibility: RecordVisibility,
+  pub preferred_tts_result_visibility: EntityVisibility,
+  pub preferred_w2l_result_visibility: EntityVisibility,
   pub discord_username: Option<String>,
   pub twitch_username: Option<String>,
   pub twitter_username: Option<String>,
@@ -64,31 +64,6 @@ pub struct UserProfileModeratorFields {
 pub struct ProfileSuccessResponse {
   pub success: bool,
   pub user: Option<UserProfileRecordForResponse>,
-}
-
-#[derive(Serialize)]
-pub struct RawUserProfileRecord {
-  pub user_token: String,
-  pub username: String,
-  pub email_gravatar_hash: String,
-  pub display_name: String,
-  pub profile_markdown: String,
-  pub profile_rendered_html: String,
-  pub user_role_slug: String,
-  pub disable_gravatar: i8,
-  pub preferred_tts_result_visibility: RecordVisibility,
-  pub preferred_w2l_result_visibility: RecordVisibility,
-  pub discord_username: Option<String>,
-  pub twitch_username: Option<String>,
-  pub twitter_username: Option<String>,
-  pub patreon_username: Option<String>,
-  pub github_username: Option<String>,
-  pub cashapp_username: Option<String>,
-  pub website_url: Option<String>,
-  pub is_banned: i8,
-  pub maybe_mod_comments: Option<String>,
-  pub maybe_mod_user_token: Option<String>,
-  pub created_at: DateTime<Utc>,
 }
 
 #[derive(Debug)]
@@ -138,21 +113,20 @@ pub async fn get_profile_handler(
   path: Path<GetProfilePathInfo>,
   server_state: web::Data<Arc<ServerState>>) -> Result<HttpResponse, ProfileError>
 {
-  let time_at_request_start = Instant::now();
+  let mut benchmark = MultiBenchmarkingTimer::new();
 
-  let maybe_user_session = time_section(|| async {
-    server_state
-        .session_checker
-        .maybe_get_user_session(&http_request, &server_state.mysql_pool)
-        .await
-        .map_err(|e| {
-          warn!("Session checker error: {:?}", e);
-          ProfileError::ServerError
-        })
-    }
-  ).await?;
-
-  let time_after_session_check = Instant::now();
+  let maybe_user_session =
+      benchmark.time_async_section("session checker", || async {
+        server_state
+            .session_checker
+            .maybe_get_user_session(&http_request, &server_state.mysql_pool)
+            .await
+            .map_err(|e| {
+              warn!("Session checker error: {:?}", e);
+              ProfileError::ServerError
+            })
+        }
+      ).await?;
 
   let mut is_mod = false;
 
@@ -160,65 +134,25 @@ pub async fn get_profile_handler(
     is_mod = user_session.can_ban_users;
   }
 
-  let time_before_profile_query = Instant::now();
+  let maybe_user_profile =
+      get_user_profile_by_username(&path.username, &server_state.mysql_pool).await;
 
-  // NB: Lookup failure is Err(RowNotFound).
-  // NB: Since this is publicly exposed, we don't query sensitive data.
-  let maybe_profile_record = sqlx::query_as!(
-      RawUserProfileRecord,
-        r#"
-SELECT
-    users.token as user_token,
-    username,
-    display_name,
-    email_gravatar_hash,
-    profile_markdown,
-    profile_rendered_html,
-    user_role_slug,
-    disable_gravatar,
-    preferred_tts_result_visibility as `preferred_tts_result_visibility: database_queries::column_types::record_visibility::RecordVisibility`,
-    preferred_w2l_result_visibility as `preferred_w2l_result_visibility: database_queries::column_types::record_visibility::RecordVisibility`,
-    discord_username,
-    twitch_username,
-    twitter_username,
-    patreon_username,
-    github_username,
-    cashapp_username,
-    website_url,
-    is_banned,
-    maybe_mod_comments,
-    maybe_mod_user_token,
-    created_at
-FROM users
-WHERE
-    users.username = ?
-    AND users.user_deleted_at IS NULL
-    AND users.mod_deleted_at IS NULL
-        "#,
-        &path.username,
-    )
-    .fetch_one(&server_state.mysql_pool)
-    .await; // TODO: This will return error if it doesn't exist
-
-  let time_after_profile_query = Instant::now();
-
-  let profile_record : RawUserProfileRecord = match maybe_profile_record {
-    Ok(profile_record) => profile_record,
+  let mut user_profile = match maybe_user_profile {
+    Ok(Some(user_profile)) => user_profile,
+    Ok(None) => {
+      warn!("Invalid user");
+      return Err(ProfileError::NotFound);
+    },
     Err(err) => {
-      match err {
-        sqlx::Error::RowNotFound => {
-          warn!("Invalid user");
-          return Err(ProfileError::NotFound);
-        },
-        _ => {
-          warn!("User profile query error: {:?}", err);
-          return Err(ProfileError::ServerError);
-        }
-      }
+      warn!("User profile query error: {:?}", err);
+      return Err(ProfileError::ServerError);
     }
   };
 
-  let is_banned = i8_to_bool(profile_record.is_banned);
+  let is_banned = user_profile.maybe_moderator_fields
+      .as_ref()
+      .map(|mod_fields| mod_fields.is_banned)
+      .unwrap_or(false);
 
   if is_banned && !is_mod {
     // Can't see banned users.
@@ -227,7 +161,7 @@ WHERE
 
   let time_before_badges_list = Instant::now();
 
-  let badges = list_user_badges(&server_state.mysql_pool, &profile_record.user_token)
+  let badges = list_user_badges(&server_state.mysql_pool, &user_profile.user_token)
       .await
       .unwrap_or_else(|err| {
         warn!("Error querying badges: {:?}", err);
@@ -236,38 +170,38 @@ WHERE
 
   let time_after_badges_list = Instant::now();
 
-  let maybe_mod_fields = if is_mod {
-    Some(UserProfileModeratorFields {
-      is_banned,
-      maybe_mod_comments: profile_record.maybe_mod_comments.clone(),
-      maybe_mod_user_token: profile_record.maybe_mod_user_token.clone(),
-    })
-  } else {
-    None
-  };
-
-  let profile_for_response = UserProfileRecordForResponse {
-    user_token: profile_record.user_token,
-    username: profile_record.username,
-    display_name: profile_record.display_name,
-    email_gravatar_hash: profile_record.email_gravatar_hash,
-    profile_markdown: profile_record.profile_markdown,
-    profile_rendered_html: profile_record.profile_rendered_html,
-    user_role_slug: profile_record.user_role_slug,
-    disable_gravatar: i8_to_bool(profile_record.disable_gravatar),
-    preferred_tts_result_visibility: profile_record.preferred_tts_result_visibility,
-    preferred_w2l_result_visibility: profile_record.preferred_w2l_result_visibility,
-    discord_username: profile_record.discord_username,
-    twitch_username: profile_record.twitch_username,
-    twitter_username: profile_record.twitter_username,
-    patreon_username: profile_record.patreon_username,
-    github_username: profile_record.github_username,
-    cashapp_username: profile_record.cashapp_username,
-    website_url: profile_record.website_url,
-    created_at: profile_record.created_at,
-    maybe_moderator_fields: maybe_mod_fields,
+  let mut profile_for_response = UserProfileRecordForResponse {
+    user_token: user_profile.user_token,
+    username: user_profile.username,
+    display_name: user_profile.display_name,
+    email_gravatar_hash: user_profile.email_gravatar_hash,
+    profile_markdown: user_profile.profile_markdown,
+    profile_rendered_html: user_profile.profile_rendered_html,
+    user_role_slug: user_profile.user_role_slug,
+    disable_gravatar: user_profile.disable_gravatar,
+    preferred_tts_result_visibility: user_profile.preferred_tts_result_visibility,
+    preferred_w2l_result_visibility: user_profile.preferred_w2l_result_visibility,
+    discord_username: user_profile.discord_username,
+    twitch_username: user_profile.twitch_username,
+    twitter_username: user_profile.twitter_username,
+    patreon_username: user_profile.patreon_username,
+    github_username: user_profile.github_username,
+    cashapp_username: user_profile.cashapp_username,
+    website_url: user_profile.website_url,
+    created_at: user_profile.created_at,
+    maybe_moderator_fields: user_profile.maybe_moderator_fields.map(|mod_fields| {
+      UserProfileModeratorFields {
+        is_banned: mod_fields.is_banned,
+        maybe_mod_comments: mod_fields.maybe_mod_comments,
+        maybe_mod_user_token: mod_fields.maybe_mod_user_token,
+      }
+    }),
     badges,
   };
+
+  if !is_mod {
+    profile_for_response.maybe_moderator_fields = None;
+  }
 
   let response = ProfileSuccessResponse {
     success: true,
