e86612af4b82c5173d1dbca06372cc04698c5b30
Brandon Thomas <echelon@gmail.com>
2022-08-16 21:05:57 -0400

    session_info endpoint uses single pooled connection

--- crates/service/storyteller_web/src/http_server/web_utils/session_checker.rs
+++ crates/service/storyteller_web/src/http_server/web_utils/session_checker.rs
@@ -170,7 +170,7 @@ AND deleted_at IS NULL
     }
   }
 
-  //#[deprecated = "Use the PoolConnection method"]
+  #[deprecated = "Use the PoolConnection method"]
   pub async fn maybe_get_user_session(
     &self,
     request: &HttpRequest,
