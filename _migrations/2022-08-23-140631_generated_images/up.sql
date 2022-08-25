2b52488bfa01fdcb5d0cc22960c4426793a69284
Brandon Thomas <echelon@gmail.com>
2022-08-25 04:01:50 -0400

    generated images tables

--- _migrations/2022-08-23-140631_generated_images/up.sql
+++ _migrations/2022-08-23-140631_generated_images/up.sql
@@ -1,3 +1,10 @@
 -- noinspection SqlDialectInspectionForFile
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
+
+
+CREATE TABLE generated_image_sequence ()
+
+CREATE TABLE generated_images ()
+
+CREATE TABLE generated_image_request_logs()
