57e3dd05c794126eb8055669a5c9165af8cd72e4
Brandon Thomas <echelon@gmail.com>
2022-08-26 00:26:44 -0400

    generated image data

--- _migrations/2022-08-23-140631_generated_images/down.sql
+++ _migrations/2022-08-23-140631_generated_images/down.sql
@@ -1,3 +1,7 @@
 -- noinspection SqlDialectInspectionForFile
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
+
+DROP TABLE generated_images;
+DROP TABLE generated_image_batches;
+DROP TABLE generated_image_request_logs;
