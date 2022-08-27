1a7485b9982b7e8b4f95eba8afae88fe3e3f86c3
Angelica Chung <angelica.ramos.91@gmail.com>
2022-08-27 12:59:09 -0400

    title added to toggle

--- packages/dashboard.power.stream/src/layout/TopNav.tsx
+++ packages/dashboard.power.stream/src/layout/TopNav.tsx
@@ -29,6 +29,7 @@ function TopNav(props: Props) {
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasNavbar2"
             aria-controls="offcanvasNavbar2"
+            title="toggle navbar"
           >
             <span className="sr-only">Navbar toggle</span>
             <span className="navbar-toggler-icon"></span>
