784b576024b716871fcbeca32fa8657ebdffda50
Brandon Thomas <echelon@gmail.com>
2022-09-04 00:44:43 -0400

    Add scripts to publish to production
    Eventually a central deploy system that doesn't push from developer
    machines should be the standard mechanism. This will work for now.

---
+++ publish_production_storyteller.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+MASTER_BRANCH='master'
+PRODUCTION_BRANCH='production-storyteller'
+
+git stash
+git checkout $MASTER_BRANCH
+git pull
+
+if git show-ref --verify --quiet "refs/heads/$PRODUCTION_BRANCH"; then
+    echo "Deleting branch ${PRODUCTION_BRANCH}"
+    git branch -D $PRODUCTION_BRANCH
+fi
+
+git checkout -b $PRODUCTION_BRANCH
+git push --set-upstream origin $PRODUCTION_BRANCH
+
+git checkout $MASTER_BRANCH
+
