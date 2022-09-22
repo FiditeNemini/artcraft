8f807a77190d8574686c24a042e70c3cbe657c71
Brandon Thomas <echelon@gmail.com>
2022-09-22 05:36:16 -0400

    wip

--- crates/service/storyteller_web/src/subscriptions/subscription_plan.rs
+++ crates/service/storyteller_web/src/subscriptions/subscription_plan.rs
@@ -11,23 +11,37 @@
 
 #[derive(Clone)]
 pub struct SubscriptionPlan {
-  /// Slug is the unique identifier for the subscription
+  /// This is the website or platform the subscription exists on.
+  /// These should be distinct namespaces.
   /// It must be human-readability friendly, consist of only lowercase alphanumerics,
   /// underscores, and hyphens, and be 32 characters or fewer.
-  pub slug: String,
+  pub category_key: String,
+
+  /// This is the unique identifier for the subscription (must be unique across namespaces!)
+  /// It must be human-readability friendly, consist of only lowercase alphanumerics,
+  /// underscores, and hyphens, and be 32 characters or fewer.
+  pub product_key: String,
+
+  /// The stripe product ID for this plan
+  pub stripe_product_id: String,
+  pub stripe_test_product_id: String,
+
+  /// The stripe price ID for this plan
+  pub stripe_price_ids: Vec<String>,
+  pub stripe_test_price_ids: Vec<String>,
 }
 
 impl SubscriptionPlan {
 
-  pub fn base_tts_priority_level(&self) -> u32 {
+  pub fn has_feature_foo(&self) -> bool {
+    false
+  }
+
+  pub fn priority_level_tts(&self) -> u32 {
     0
   }
 
-  pub fn base_api_tts_priority_level(&self) -> Option<u32> {
+  pub fn priority_level_tts_api(&self) -> Option<u32> {
     Some(0)
-  }
-
-  pub fn has_feature_foo(&self) -> bool {
-    false
   }
 }
