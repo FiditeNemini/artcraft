2c60ee456af09cb9856cc86a8c0fc03ad56059e5
BFlat <bombayapirat@gmail.com>
2022-11-01 12:03:51 +0700

    added wizard to the randomized image stack

--- packages/fakeyou/src/v2/view/tts/tts_model_list/TtsModelListPage.tsx
+++ packages/fakeyou/src/v2/view/tts/tts_model_list/TtsModelListPage.tsx
@@ -114,7 +114,9 @@ function TtsModelListPage(props: Props) {
       // "mascot/halloween_1.webp",
       // "mascot/halloween_2.webp",
       // "mascot/halloween_3.webp",
+      
       "mascot/kitsune_pose2.webp",
+      "mascot/kitsune_wizard.webp",
     ];
 
     return images[Math.floor(Math.random() * images.length)];
