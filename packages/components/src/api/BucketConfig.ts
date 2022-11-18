dda87d85b0a5324e6ce54f34564676548f8d5a55
Brandon Thomas <echelon@gmail.com>
2022-11-18 01:09:15 -0500

    Fix localhost/development bucket urls

--- packages/components/src/api/BucketConfig.ts
+++ packages/components/src/api/BucketConfig.ts
@@ -4,14 +4,10 @@ class BucketConfig {
   isLocalDev: boolean;
 
   constructor() {
-    if (document.location.host.includes("localhost") ||
-        document.location.host.includes("jungle.horse")) {
-      this.isLocalDev = true;
-    } else {
-      this.isLocalDev = false;
-    }
-    // TODO TEMP
-    //this.isLocalDev = false;
+    this.isLocalDev = (
+      document.location.host.includes("localhost") ||
+      document.location.host.includes("jungle.horse") ||
+      document.location.host.startsWith("dev."));
   }
 
   // TODO: Prevent callers with undefined/null paths
