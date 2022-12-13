89e721e3bcac93c974f0c3e612a9a3b066c4fd57
Brandon Thomas <echelon@gmail.com>
2022-12-13 00:20:30 -0500

    derive serde traits

--- crates/lib/tokens/src/users/user.rs
+++ crates/lib/tokens/src/users/user.rs
@@ -1,12 +1,11 @@
-use serde::{Deserializer, Serializer};
+use serde::Serialize;
+use serde::Deserialize;
 use std::fmt::{Debug, Display, Formatter};
 
 // https://docs.rs/sqlx/latest/sqlx/trait.Type.html
-#[derive(Clone, PartialEq, Eq, sqlx::Type, Debug)]
+#[derive(Clone, PartialEq, Eq, sqlx::Type, Debug, Serialize, Deserialize)]
 #[sqlx(transparent)]
 pub struct UserToken(pub String);
-
-// TODO: Macros should implement all of the following:
 
 impl Display for UserToken {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -14,18 +13,23 @@ impl Display for UserToken {
   }
 }
 
-impl serde::Serialize for UserToken {
-  fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-    serializer.serialize_str(&self.0)
-  }
-}
-
-impl<'de> serde::Deserialize<'de> for UserToken {
-  fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
-    let s = String::deserialize(d)?;
-    Ok(UserToken(s))
-  }
-}
+// For reference, here's what the serde implementation might be if manually written.
+// This may be useful for designing composite types in the future:
+//
+//   use serde::{Deserializer, Serializer};
+//
+//   impl serde::Serialize for UserToken {
+//     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+//       serializer.serialize_str(&self.0)
+//     }
+//   }
+//
+//   impl<'de> serde::Deserialize<'de> for UserToken {
+//     fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+//       let s = String::deserialize(d)?;
+//       Ok(UserToken(s))
+//     }
+//   }
 
 #[cfg(test)]
 mod tests {
