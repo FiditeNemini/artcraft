732828dca01da7649f027e77864c5cdab8c94449
Brandon Thomas <echelon@gmail.com>
2022-12-16 00:47:41 -0500

    fix build script

--- build-storyteller.io.sh
+++ build-storyteller.io.sh
@@ -15,11 +15,11 @@ find . -type f -exec sed -i "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} +
 # find . -type f -exec sed -i '' -e "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} + 
 
 # --ignore-engines: https://stackoverflow.com/a/59615348
-yarn build-storyteller-home --verbose --ignore-optional --ignore-engines
+yarn build-storyteller --verbose --ignore-optional --ignore-engines
 
-mkdir storyteller-home
-mv packages/storyteller-home/build/ storyteller-home/build/
+mkdir storyteller
+mv packages/storyteller.io/build/ storyteller.io/build/
 
 echo "Copying redirects configuration to Netlify build dir..."
-cp _redirects storyteller-home/build/
+cp _redirects storyteller.io/build/
 
