8591f74ff205e8c2ad1e04305fd2bed29b08d545
Brandon Thomas <echelon@gmail.com>
2022-12-19 17:41:59 -0500

    Look at "started" w2l jobs too

--- analytics/w2l_template_upload_jobs.sql
+++ analytics/w2l_template_upload_jobs.sql
@@ -6,7 +6,7 @@
 select count(*) from w2l_template_upload_jobs where status='pending';
 
 -- See pending download URLs:
-select download_url from w2l_template_upload_jobs where status='pending';
+select download_url from w2l_template_upload_jobs where status IN ('pending', 'started', 'attempt_failed');
 
 -- See old jobs that might be stuck as "started"
 select count(*)
