9fbafd7fcdaf839f5e5e76e236263e410b590ec4
Brandon Thomas <echelon@gmail.com>
2023-01-14 06:39:28 -0500

    sqlx 0.4.x -> 0.6.x

--- crates/_types/enums/src/macros.rs
+++ crates/_types/enums/src/macros.rs
@@ -51,12 +51,18 @@ macro_rules! impl_enum_display_and_debug_using_to_str {
 /// so our encoder/decoder need to set the rules.
 ///
 /// Solution adapted from https://github.com/launchbadge/sqlx/discussions/1502
+/// The 0.6.2 series solution adapted from https://docs.rs/sqlx-core/0.6.2/src/sqlx_core/mysql/types/uuid.rs.html#38-66
+///
 macro_rules! impl_enum_sqlx_coders {
   ($t:ident) => {
 
     impl sqlx::Type<sqlx_core::mysql::MySql> for $t {
       fn type_info() -> sqlx_core::mysql::MySqlTypeInfo {
-        String::type_info()
+        // // 0.4.x series:
+        // String::type_info()
+
+        // NB: https://docs.rs/sqlx-core/0.6.2/src/sqlx_core/mysql/types/uuid.rs.html#38-66 serves as an example
+        <str as sqlx::Type<sqlx_core::mysql::MySql>>::type_info()
       }
     }
 
@@ -65,9 +71,15 @@ macro_rules! impl_enum_sqlx_coders {
         &self,
         buf: &mut <sqlx_core::mysql::MySql as sqlx_core::database::HasArguments<'q>>::ArgumentBuffer
       ) -> sqlx_core::encode::IsNull {
-        // NB: In the absence of `#[derive(sqlx::Type)]` and `#sqlx(rename_all="lowercase")]`,
-        // this controls the casing of the variants when sent to MySQL.
-        self.to_str().encode_by_ref(buf)
+        // // 0.4.x series:
+        // // NB: In the absence of `#[derive(sqlx::Type)]` and `#sqlx(rename_all="lowercase")]`,
+        // // this controls the casing of the variants when sent to MySQL.
+        // self.to_str().encode_by_ref(buf)
+
+        // NB: https://docs.rs/sqlx-core/0.6.2/src/sqlx_core/mysql/types/uuid.rs.html#38-66 and
+        //  https://docs.rs/sqlx-core/0.6.2/src/sqlx_core/mysql/types/str.rs.html#75-78 serves as examples
+        let value = self.to_str();
+        <&str as sqlx::Encode<sqlx_core::mysql::MySql>>::encode(&*value, buf)
       }
     }
 
@@ -75,8 +87,15 @@ macro_rules! impl_enum_sqlx_coders {
       fn decode(
         value: sqlx_core::mysql::MySqlValueRef<'r>,
       ) -> Result<Self, sqlx_core::error::BoxDynError> {
-        let string = String::decode(value)?;
-        let value = $t::from_str(&string)?;
+        // // 0.4.x series:
+        // let string = String::decode(value)?;
+        // let value = $t::from_str(&string)?;
+        // Ok(value)
+
+        // NB: https://docs.rs/sqlx-core/0.6.2/src/sqlx_core/mysql/types/uuid.rs.html#38-66 serves as an example
+        // delegate to the &str type to decode from MySQL
+        let text = <&str as sqlx::Decode<sqlx_core::mysql::MySql>>::decode(value)?;
+        let value = $t::from_str(&text)?;
         Ok(value)
       }
     }
