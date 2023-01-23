f93738b69938b700030b037a20e0706d56086ede
Brandon Thomas <echelon@gmail.com>
2023-01-23 05:21:23 -0500

    Add Italian signup page

--- packages/fakeyou.com/src/_i18n/locales/it/index.ts
+++ packages/fakeyou.com/src/_i18n/locales/it/index.ts
@@ -2,12 +2,14 @@
 // 67 million speakers (worldwide #?)
 // Twitch #10 (41k concurrent viewers, 1.2k concurrent viewers)
 
+import * as account from './account';
 import * as common from './common';
 import * as nav from './nav';
 import * as notices from './notices';
 import * as tts from './tts';
 
 export {
+    account,
     common,
     nav,
     notices,
