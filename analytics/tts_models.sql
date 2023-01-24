dbfd80531d2134bcd82a4676b23660add209d29b
Brandon Thomas <echelon@gmail.com>
2023-01-23 23:21:44 -0500

    analytics query

--- analytics/tts_models.sql
+++ analytics/tts_models.sql
@@ -63,6 +63,35 @@ order by r.use_count desc
     limit 500;
 
 --
+-- Top 20 ***NEW*** models to share in #announcements
+-- Can be scoped by creator.
+--
+select
+    m.token,
+    m.text_pipeline_type,
+    m.ietf_language_tag,
+    m.title,
+    u.username,
+    r.use_count,
+    m.user_deleted_at,
+    m.mod_deleted_at,
+    m.created_at
+from (
+         select model_token, count(*) as use_count
+         from tts_results
+         where created_at > ( CURDATE() - INTERVAL 5 DAY )
+         group by model_token
+     ) as r
+         join tts_models as m
+              on m.token = r.model_token
+         join users as u
+              on u.token = m.creator_user_token
+where m.created_at > ( CURDATE() - INTERVAL 31 DAY )
+  AND u.username IN ( 'vegito1089', 'justinjohn0306')
+order by r.use_count desc
+    limit 500;
+
+--
 -- Top 500 models by use count, last 5 days, not spanish, etc.
 -- Limited due to tmux scrollback.
 --
