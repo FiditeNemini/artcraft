cb9a2aa7dd337297e6842b4c23edb0155a4c289e
BFlat <bombayapirat@gmail.com>
2023-01-28 08:45:48 +0700

    rating btn bugfix

--- packages/fakeyou.com/src/v2/view/_common/ratings/RatingButtons.tsx
+++ packages/fakeyou.com/src/v2/view/_common/ratings/RatingButtons.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { t } from "i18next";
-import { GetUserRating, GetUserRatingIsOk } from "@storyteller/components/src/api/user_ratings/GetUserRating";
-import { SetUserRating, SetUserRatingIsOk } from "@storyteller/components/src/api/user_ratings/SetUserRating";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faThumbsDown,
-  faThumbsUp,
-} from "@fortawesome/free-solid-svg-icons";
+  GetUserRating,
+  GetUserRatingIsOk,
+} from "@storyteller/components/src/api/user_ratings/GetUserRating";
+import {
+  SetUserRating,
+  SetUserRatingIsOk,
+} from "@storyteller/components/src/api/user_ratings/SetUserRating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 
@@ -17,14 +20,16 @@ interface Props {
 
 /**
  * This is a reusable component that can be put on several different pages.
- * 
+ *
  * It requires the entity type ("tts_model", "tts_result", "w2l_template", "w2l_result", etc.)
  * and the entity token, and it will be able to fetch a user's previous vote and change it.
- * 
+ *
  * This button component manages all of its own state and API calls.
  */
 function RatingButtons(props: Props) {
-  const [userRatingValue, setUserRatingValue] = useState<string|undefined>(undefined);
+  const [userRatingValue, setUserRatingValue] = useState<string | undefined>(
+    undefined
+  );
 
   const loadRating = useCallback(async () => {
     const request = {
@@ -36,11 +41,7 @@ function RatingButtons(props: Props) {
       let ratingValue = rating.maybe_rating_value || undefined;
       setUserRatingValue(ratingValue);
     }
-  }, [
-    setUserRatingValue, 
-    props.entity_type, 
-    props.entity_token,
-  ]);
+  }, [setUserRatingValue, props.entity_type, props.entity_token]);
 
   const toggleUpvote = async () => {
     let nextValue = userRatingValue === "positive" ? "neutral" : "positive";
@@ -90,10 +91,7 @@ function RatingButtons(props: Props) {
         theme="fakeyou"
         arrow={false}
       >
-        <button 
-          className={upClasses} 
-          onClick={toggleUpvote}
-        >
+        <button className={upClasses} onClick={toggleUpvote} type="button">
           <FontAwesomeIcon icon={faThumbsUp} />
         </button>
       </Tippy>
@@ -107,15 +105,12 @@ function RatingButtons(props: Props) {
         theme="fakeyou"
         arrow={false}
       >
-        <button 
-          className={downClasses} 
-          onClick={toggleDownvote}
-        >
+        <button className={downClasses} onClick={toggleDownvote} type="button">
           <FontAwesomeIcon icon={faThumbsDown} />
         </button>
       </Tippy>
     </div>
-  )
+  );
 }
 
 export { RatingButtons };
