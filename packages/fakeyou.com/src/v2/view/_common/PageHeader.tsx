04b7261c97691349fda15be29a870a1139401617
BFlat <bombayapirat@gmail.com>
2023-02-05 22:56:52 +0700

    w2l page

--- packages/fakeyou.com/src/v2/view/_common/PageHeader.tsx
+++ packages/fakeyou.com/src/v2/view/_common/PageHeader.tsx
@@ -10,18 +10,22 @@ interface Props {
 
 function PageHeader(props: Props) {
   return (
-    <div className="container hero-section pt-4 pt-lg-5 pb-5 mb-3">
-      <h1 className="fw-bold text-center text-md-start">
-        {props.titleIcon}
-        {props.title}
-      </h1>
-      <hr />
-      <p className="text-center text-md-start">{props.subText}</p>
-      {props.showButtons && (
-        <div className="d-flex flex-column flex-md-row gap-3 justify-content-center justify-content-md-start mt-4">
-          {props.actionButtons}
+    <div className="container-panel hero-section pt-4 pt-lg-5 pb-5">
+      <div className="panel">
+        <div className="p-3 py-4 p-md-4">
+          <h1 className="fw-bold text-center text-md-start">
+            {props.titleIcon}
+            {props.title}
+          </h1>
+          <hr />
+          <p className="text-center text-md-start">{props.subText}</p>
+          {props.showButtons && (
+            <div className="d-flex flex-column flex-md-row gap-3 justify-content-center justify-content-md-start mt-4">
+              {props.actionButtons}
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
