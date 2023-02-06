035faa0561c964d7eb168957189b1b6adac9fa88
Brandon Thomas <echelon@gmail.com>
2023-02-06 17:56:32 -0500

    update launch script

--- launch_chatbot.bat
+++ launch_chatbot.bat
@@ -2,9 +2,11 @@ REM Comments on windows use "REM". Weird-ass platform.
 
 REM Make sure migrations are up to date
 pushd crates\programs\aichatbot-sidecar
+
 diesel migration run
 popd
 
 REM Start the sidecar
-DATABASE_URL=sqlite://runtime_data/database.db cargo run --bin aichatbot-sidecar
+set DATABASE_URL=sqlite://runtime_data/database.db 
+cargo run --bin aichatbot-sidecar
 
