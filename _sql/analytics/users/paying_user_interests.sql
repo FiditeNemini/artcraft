41e03feb854be3337e1d483e17df680cb01e4154
Brandon Thomas <echelon@gmail.com>
2023-02-23 02:15:04 -0500

    more analytics queries

--- _sql/analytics/users/paying_user_interests.sql
+++ _sql/analytics/users/paying_user_interests.sql
@@ -35,6 +35,46 @@ FROM (
         x.use_count > 10;
 
 
+-- The first voice a paying customer uses
+-- TODO: List *all* of the voices a paying customer uses before they subscribe, not just the first.
+-- select first row of partially aggregate query: https://stackoverflow.com/a/73157541
+SELECT *
+FROM (
+         SELECT
+             t.token,
+             t.title,
+             count(*) as use_count,
+             t.text_pipeline_type,
+             t.ietf_language_tag,
+             t.created_at,
+             t.user_deleted_at,
+             t.mod_deleted_at
+         FROM tts_models AS t
+                  JOIN
+              (
+                  SELECT
+                      model_token
+                  FROM (
+                           SELECT
+                               model_token,
+                               maybe_creator_user_token,
+                               row_number() over(partition by maybe_creator_user_token order by id asc) as rownum
+                           FROM
+                               tts_inference_jobs
+                           WHERE maybe_creator_user_token IN
+                                 (
+                                     select distinct user_token
+                                     from user_subscriptions
+                                 )
+                       ) AS x
+                  WHERE rownum = 1
+                  ORDER BY model_token desc
+              ) AS y
+              ON t.token = y.model_token
+         GROUP BY t.token
+     ) AS z
+ORDER BY use_count desc
+
 -- The last voice a paying customer uses
 -- select first row of partially aggregate query: https://stackoverflow.com/a/73157541
 SELECT *
