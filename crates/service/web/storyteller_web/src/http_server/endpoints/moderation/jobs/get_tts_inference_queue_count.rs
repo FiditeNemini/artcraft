2d20430f55cb8016f7e2a4992a6910a128a7bf9b
Brandon Thomas <echelon@gmail.com>
2023-03-06 19:56:26 -0500

    port tts inference stats from storyteller web

--- crates/service/web/storyteller_web/src/http_server/endpoints/moderation/jobs/get_tts_inference_queue_count.rs
+++ crates/service/web/storyteller_web/src/http_server/endpoints/moderation/jobs/get_tts_inference_queue_count.rs
@@ -1,7 +1,7 @@
 use actix_http::Error;
 use actix_http::http::header;
-use actix_web::cookie::Cookie;
 use actix_web::HttpResponseBuilder;
+use actix_web::cookie::Cookie;
 use actix_web::error::ResponseError;
 use actix_web::http::StatusCode;
 use actix_web::web::Path;
@@ -10,6 +10,7 @@ use chrono::{DateTime, Utc};
 use crate::http_server::web_utils::response_success_helpers::simple_json_success;
 use crate::http_server::web_utils::serialize_as_json_error::serialize_as_json_error;
 use crate::server_state::ServerState;
+use database_queries::queries::tts::tts_inference_jobs::get_pending_tts_inference_job_detailed_stats::{get_pending_tts_inference_job_detailed_stats, PendingCountResult};
 use log::{info, warn, log};
 use regex::Regex;
 use sqlx::error::DatabaseError;
@@ -91,80 +92,22 @@ pub async fn get_tts_inference_queue_count_handler(
     return Err(GetTtsInferenceQueueCountError::Unauthorized);
   }
 
-  // NB(old?): Lookup failure is Err(RowNotFound).
-  // NB(2022-03-05): The "seconds_since_first" result might return null if no pending, so IFNULL
-  //  means we won't fail the full query.
-  let maybe_result = sqlx::query_as!(
-      PendingCountResult,
-        r#"
-SELECT
-  IFNULL((
-    SELECT
-      NOW() - t1.created_at AS seconds_since_first
-    FROM tts_inference_jobs AS t1
-    WHERE t1.status = "pending"
-    ORDER BY t1.id ASC
-    LIMIT 1
-  ), 0) as seconds_since_first,
-  sub2.pending_count,
-  sub3.pending_priority_nonzero_count,
-  sub4.pending_priority_gt_one_count,
-  sub5.attempt_failed_count
-FROM
-  (
-    SELECT
-      count(t2.id) as pending_count
-    FROM tts_inference_jobs AS t2
-    WHERE t2.status = "pending"
-  ) as sub2,
-  (
-    SELECT
-      count(t3.id) as pending_priority_nonzero_count
-    FROM tts_inference_jobs AS t3
-    WHERE t3.status = "pending"
-    AND t3.priority_level > 0
-  ) as sub3,
-  (
-    SELECT
-      count(t4.id) as pending_priority_gt_one_count
-    FROM tts_inference_jobs AS t4
-    WHERE t4.status = "pending"
-    AND t4.priority_level > 1
-  ) as sub4,
-  (
-    SELECT
-      count(t5.id) as attempt_failed_count
-    FROM tts_inference_jobs AS t5
-    WHERE t5.status = "attempt_failed"
-  ) as sub5
-        "#,
-    )
-      .fetch_one(&server_state.mysql_pool)
-      .await;
-
-  let result : PendingCountResult = match maybe_result {
-    Ok(result) => result,
-    Err(err) => {
-      match err {
-        sqlx::Error::RowNotFound => {
-          // NB: Not Found for null results means nothing is pending in the queue
-          PendingCountResult {
-            seconds_since_first: 0,
-            pending_count: 0,
-            pending_priority_nonzero_count: 0,
-            pending_priority_gt_one_count: 0,
-            attempt_failed_count: 0,
-          }
-        },
-        _ => {
-          warn!("get tts pending count error: {:?}", err);
-          return Err(GetTtsInferenceQueueCountError::ServerError)
+  let result = get_pending_tts_inference_job_detailed_stats(&server_state.mysql_pool)
+      .await
+      .map_err(|err| {
+        warn!("get tts pending count error: {:?}", err);
+        GetTtsInferenceQueueCountError::ServerError
+      })?
+      .unwrap_or(
+        // NB: Not Found for null results means nothing is pending in the queue (not an error!)
+        PendingCountResult {
+          seconds_since_first: 0,
+          pending_count: 0,
+          pending_priority_nonzero_count: 0,
+          pending_priority_gt_one_count: 0,
+          attempt_failed_count: 0,
         }
-      }
-    },
-  };
-
-  println!("Foo: {:?}",  &result);
+      );
 
   let response = GetTtsInferenceQueueCountResponse {
     success: true,
@@ -181,13 +124,4 @@ FROM
   Ok(HttpResponse::Ok()
       .content_type("application/json")
       .body(body))
-}
-
-#[derive(Serialize, Debug)]
-pub struct PendingCountResult {
-  pub seconds_since_first: i64,
-  pub pending_count: i64,
-  pub pending_priority_nonzero_count: i64,
-  pub pending_priority_gt_one_count: i64,
-  pub attempt_failed_count: i64,
 }
