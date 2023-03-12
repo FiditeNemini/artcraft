817b2c01df153ab28bbcc4fdfbb8c489862cf414
Brandon Thomas <echelon@gmail.com>
2023-03-11 22:53:01 -0500

    new redis ttl cache crate (untested)

---
+++ crates/lib/caching/redis_caching/src/lib.rs
@@ -0,0 +1,20 @@
+//! redis_caching
+//!
+//! Easy-to use Redis caching utility.
+//!
+
+// Never allow these
+#![forbid(private_in_public)]
+#![forbid(unused_must_use)] // NB: It's unsafe to not close/check some things
+
+// Okay to toggle
+#![forbid(unreachable_patterns)]
+#![forbid(unused_imports)]
+#![forbid(unused_mut)]
+#![forbid(unused_variables)]
+
+// Always allow
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
+pub mod redis_ttl_cache;
