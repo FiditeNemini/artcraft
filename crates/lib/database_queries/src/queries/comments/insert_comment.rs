df2444ddc0c5f2979b43a792a1be7b6b4ba57f85
Brandon Thomas <echelon@gmail.com>
2023-03-16 11:09:26 -0400

    insert comment

---
+++ crates/lib/database_queries/src/queries/comments/insert_comment.rs
@@ -0,0 +1,83 @@
+use anyhow::anyhow;
+use chrono::Utc;
+use enums::common::visibility::Visibility;
+use errors::AnyhowResult;
+use sqlx::MySqlPool;
+use std::path::Path;
+use enums::by_table::comments::comment_entity_type::CommentEntityType;
+use tokens::tokens::comments::CommentToken;
+use tokens::tokens::tts_models::TtsModelToken;
+use tokens::tokens::w2l_templates::W2lTemplateToken;
+use tokens::users::user::UserToken;
+use tokens::voice_conversion::model::VoiceConversionModelToken;
+
+pub enum CommentEntityToken {
+  User(UserToken),
+  TtsModel(TtsModelToken),
+  TtsResult(String), // TODO: Strong type
+  W2lTemplate(W2lTemplateToken),
+  W2lResult(String), // TODO: Strong type
+}
+
+pub struct Args<'a> {
+  pub entity_token: &'a CommentEntityToken,
+
+  pub uuid_idempotency_token: &'a str,
+
+  pub user_token: Option<&'a UserToken>,
+
+  pub comment_markdown: &'a str,
+  pub comment_rendered_html: &'a str,
+
+  pub creator_ip_address: &'a str,
+
+  pub mysql_pool: &'a MySqlPool,
+}
+
+
+pub async fn insert_comment(
+  args: Args<'_>,
+) -> AnyhowResult<CommentToken> {
+
+  let comment_token = CommentToken::generate();
+
+  let (entity_type, entity_token) = match args.entity_token {
+    CommentEntityToken::User(user_token) => (CommentEntityType::User, user_token.as_str()),
+    CommentEntityToken::TtsModel(tts_model_token) => (CommentEntityType::TtsModel, tts_model_token.as_str()),
+    CommentEntityToken::TtsResult(tts_result_token) => (CommentEntityType::TtsResult, tts_result_token.as_str()),
+    CommentEntityToken::W2lTemplate(w2l_template_token) => (CommentEntityType::W2lTemplate, w2l_template_token.as_str()),
+    CommentEntityToken::W2lResult(w2l_result_token) => (CommentEntityType::W2lResult, w2l_result_token.as_str()),
+  };
+
+  let query_result = sqlx::query!(
+        r#"
+INSERT INTO comments
+SET
+  token = ?,
+  uuid_idempotency_token = ?,
+  user_token = ?,
+  entity_type = ?,
+  entity_token = ?,
+  comment_markdown = ?,
+  comment_rendered_html = ?,
+  creator_ip_address = ?
+        "#,
+      &comment_token,
+      args.uuid_idempotency_token,
+      args.user_token,
+      entity_type,
+      entity_token,
+      args.comment_markdown,
+      args.comment_rendered_html,
+      args.creator_ip_address,
+    )
+      .execute(args.mysql_pool)
+      .await;
+
+  let _record_id = match query_result {
+    Ok(res) => res.last_insert_id(),
+    Err(err) => return Err(anyhow!("Mysql error: {:?}", err)),
+  };
+
+  Ok(comment_token)
+}
