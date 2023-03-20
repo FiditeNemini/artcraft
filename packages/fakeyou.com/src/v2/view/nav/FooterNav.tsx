add4578f7854620baf65b95eff983b2b7860a67c
Brandon Thomas <echelon@gmail.com>
2023-03-20 06:08:17 -0400

    follow intent

--- packages/fakeyou.com/src/v2/view/nav/FooterNav.tsx
+++ packages/fakeyou.com/src/v2/view/nav/FooterNav.tsx
@@ -185,7 +185,7 @@ function FooterNav(props: Props) {
           <div className="d-flex flex-column flex-lg-row pt-2 align-items-center gap-0 gap-lg-4">
             <span className="flex-grow-1">
               © 2023 FakeYou, built by{" "}
-              <a href="https://twitter.com/echelon">echelon</a> in Atlanta.
+              <a href="https://twitter.com/intent/follow?screen_name=echelon">echelon</a> in Atlanta.
             </span>
             <div className="d-flex flex-column flex-lg-row align-items-center mt-4 mt-lg-0">
               {moderationLink}
