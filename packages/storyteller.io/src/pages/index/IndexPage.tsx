cbbbb3492e15f1fa4d3136ea605174b12563cb5b
BFlat <bombayapirat@gmail.com>
2023-03-21 17:31:33 +0700

    our team

--- packages/storyteller.io/src/pages/index/IndexPage.tsx
+++ packages/storyteller.io/src/pages/index/IndexPage.tsx
@@ -213,7 +213,6 @@ function IndexPage() {
           </Tippy>
         </div>
       </div>
-
       <div id="social" className="bg-light section-2">
         <div>
           <Marquee gradient={false} speed={100}>
@@ -273,7 +272,6 @@ function IndexPage() {
           </div>
         </div>
       </div>
-
       <div className="bg-dark section-2">
         <div className="container">
           <div className="row gx-5 gy-5">
@@ -319,7 +317,6 @@ function IndexPage() {
           </div>
         </div>
       </div>
-
       <div
         id="film"
         className="bg-light section"
@@ -416,7 +413,6 @@ function IndexPage() {
           </div>
         </div>
       </div>
-
       <div className="bg-dark section-2">
         <div className="container">
           <div className="row gx-4 gy-4 gx-lg-5 gy-lg-5 mb-5">
@@ -492,7 +488,6 @@ function IndexPage() {
           </div>
         </div>
       </div>
-
       <div className="bg-hero-2">
         <div className="d-flex justify-content-center">
           {/* <img
@@ -602,7 +597,6 @@ function IndexPage() {
           <div className="shape-4"></div>
         </div>
       </div>
-
       <div
         id="music"
         className="bg-light section"
@@ -674,7 +668,6 @@ function IndexPage() {
         </div>
       </div>
 
-      {/* NB: We're not quite ready to show our deck yet. But we're going to be filling this out silently.
       <div id="team" className="bg-light section-2">
         <div>
           <Marquee gradient={false} speed={120}>
@@ -691,30 +684,67 @@ function IndexPage() {
           </Marquee>
         </div>
 
-        <div className="container py-5 text-center d-flex flex-column align-items-center mt-3">
-          <h1 className="fw-bold display-5 about-title mt-5">
-            We’re building the future production cloud studio
-          </h1>
-          <p className="fw-normal opacity-75 mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis
-            vehicula odio. Nulla non sem scelerisque, hendrerit libero sagittis,
-            rhoncus orci. Etiam in molestie augue. Donec lobortis tincidunt
-            massa, a posuere libero sagittis eget. Vivamus sagittis mauris non
-            tristique ornare. Pellentesque rhoncus ipsum magna, nec lacinia odio
-            sagittis et. Nunc aliquet cursus dui fermentum molestie.
-          </p>
+        <div className="container py-5 mt-5">
+          <div className="row g-4 g-md-5 mb-5 align-items-center">
+            <div className="col-12 col-md-6">
+              <div>
+                <h1 className="fw-bold display-5 about-title">
+                  We’re building the future production cloud studio
+                </h1>
+                <hr className="p-1 mt-4 text-red opacity-100 w-25" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <p className="fw-normal opacity-75">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div className="container mt-4">
-          <div className="row g-4 g-md-5">
+        <div className="container">
+          <h2 className="mb-4">The Team</h2>
+
+          <div className="row g-4 g-md-5 mb-5">
+            <div className="col-6 col-md-3 mb-0">
+              <img
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
+                alt=""
+              />
+            </div>
+            <div className="col-12 col-md-9 text-start d-flex flex-column justify-content-center">
+              <p className="fw-semibold opacity-100 mb-0 fs-5">
+                Brandon Thomas
+              </p>
+              <p className="team-role-text">Founder and CEO</p>
+              {/* <hr className="mt-0 my-4" /> */}
+              <p className="fw-normal opacity-75 mt-1">
+                Brandon worked 8 years as a distributed systems engineer at
+                Square. He’s spent the last decade making indie films and being
+                plugged into the Atlanta art scene. In college he built a laser
+                projector and programmed it to play video games on the side of
+                skyscrapers. Today he’s working on disrupting Hollywood and the
+                music industry by bringing down cost structures such that anyone
+                can make film from home.
+              </p>
+            </div>
+          </div>
+
+          <div className="row g-4 g-md-5 pb-4">
             <div className="col-6 col-md-3">
               <img
                 src="/images/team/placeholder-pfp.jpg"
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Brandon</p>
-              <p className="team-role-text">Founder / Backend</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Ozzy</p>
+              <p className="team-role-text">Machine Learning Engineer</p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -722,17 +752,37 @@ function IndexPage() {
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Scott</p>
-              <p className="team-role-text">3D Artist</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Michael</p>
+              <p className="team-role-text">Machine Learning Engineer</p>
             </div>
             <div className="col-6 col-md-3">
               <img
-                src="/images/team/paul.webp"
-                className="img-fluid img-team mb-3"
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Paul</p>
-              <p className="team-role-text">Systems / Devops Engineer</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Z.</p>
+              <p className="team-role-text">Machine Learning Engineer</p>
+            </div>
+            <div className="col-6 col-md-3">
+              <img
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
+                alt=""
+              />
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Scott</p>
+              <p className="team-role-text">
+                Chief Creative Officer / Technical 3D Artist
+              </p>
+            </div>
+            <div className="col-6 col-md-3">
+              <img
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
+                alt=""
+              />
+              <p className="fw-semibold opacity-100 mb-0 fs-5">???</p>
+              <p className="team-role-text">Technical 3D Artist</p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -740,8 +790,19 @@ function IndexPage() {
                 className="img-fluid img-team mb-3"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Bombay</p>
-              <p className="team-role-text">Web Design / Frontend </p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Bombay</p>
+              <p className="team-role-text">UI / Frontend Engineer </p>
+            </div>
+            <div className="col-6 col-md-3">
+              <img
+                src="/images/team/paul.webp"
+                className="img-fluid img-team mb-3"
+                alt=""
+              />
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Paul</p>
+              <p className="team-role-text">
+                Backend Engineer / Rust Full Stack
+              </p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -749,8 +810,8 @@ function IndexPage() {
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Michael</p>
-              <p className="team-role-text">ML</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Jose</p>
+              <p className="team-role-text">Data Team</p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -758,8 +819,8 @@ function IndexPage() {
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Jose</p>
-              <p className="team-role-text">ML</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">Rodrigo</p>
+              <p className="team-role-text">Data Team</p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -767,8 +828,8 @@ function IndexPage() {
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Rodrigo</p>
-              <p className="team-role-text">ML</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">???</p>
+              <p className="team-role-text">???</p>
             </div>
             <div className="col-6 col-md-3">
               <img
@@ -776,14 +837,46 @@ function IndexPage() {
                 className="img-fluid img-team mb-3 opacity-50"
                 alt=""
               />
-              <p className="fw-semibold opacity-100 mb-0">Someone</p>
-              <p className="team-role-text">Role Title</p>
+              <p className="fw-semibold opacity-100 mb-0 fs-5">???</p>
+              <p className="team-role-text">???</p>
+            </div>
+            <div className="col-6 col-md-3">
+              <img
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
+                alt=""
+              />
+              <p className="fw-semibold opacity-100 mb-0 fs-5">???</p>
+              <p className="team-role-text">???</p>
+            </div>
+          </div>
+
+          <h2 className="mb-4 mt-5">Our Advisor</h2>
+
+          <div className="row g-4 g-md-5 mb-5">
+            <div className="col-6 col-md-3">
+              <img
+                src="/images/team/placeholder-pfp.jpg"
+                className="img-fluid img-team mb-3 opacity-50"
+                alt=""
+              />
+            </div>
+            <div className="col-12 col-md-9 text-start d-flex flex-column justify-content-center">
+              <p className="fw-semibold opacity-100 mb-0 fs-5">
+                Alex Wissner-Gross
+              </p>
+              <p className="team-role-text">Advisor</p>
+              {/* <hr className="mt-0 my-4" /> */}
+              <p className="fw-normal opacity-75 mt-1">
+                Alex was the last person to graduate from MIT with a triple
+                major, and he’s led a combined 1B of startup exits.{" "}
+                <br className="d-none d-md-block" />
+                We’re hoping we’re his first decacorn exit.
+              </p>
             </div>
           </div>
         </div>
       </div>
-      */}
-
       <div id="mentions" className="bg-light section-2">
         <div>
           <Marquee gradient={false} speed={120}>
@@ -964,7 +1057,6 @@ function IndexPage() {
           </Swiper>
         </div>
       </div>
-
       <div id="contact" className="bg-light section-2 pb-0">
         <div className="container text-center">
           <h4 className="opacity-75 position-relative zi-2 mb-1">Contact Us</h4>
