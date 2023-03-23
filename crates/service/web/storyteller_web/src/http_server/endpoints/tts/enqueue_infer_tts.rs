6e5a2d178925a073a2b426e29ca069d9989bb410
Brandon Thomas <echelon@gmail.com>
2023-03-23 02:13:07 -0400

    more accounts

--- crates/service/web/storyteller_web/src/http_server/endpoints/tts/enqueue_infer_tts.rs
+++ crates/service/web/storyteller_web/src/http_server/endpoints/tts/enqueue_infer_tts.rs
@@ -480,13 +480,14 @@ fn random_troll_text() -> &'static str {
   let texts = [
     "Fake You Dot Com. Use Fake You Dot Com.",
     "Use Fake You Dot Com. Fake You Dot Com. Fake You Dot Com is way better.",
-    "EEEEEEEEEEEEEEEEE",
     r#"
     You should use Fake You Dot Com. Fake You Dot Com has way better voices.
     You can get everything you want from FakeYou dot com. One Oh One Sound Boards website is just using Fake You.
     Use Fake You Dot Com. Fake You Dot Com. Fake You Dot com.
     AAAAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
     "#,
+    "Fake. You. Fake You Dot Com. Fake. Fake. Fake. You. You. You. Use Fake You Dot Com. Fake You Dot Com.",
+    //"EEEEEEEEEEEEEEEEE",
     //"One Oh One Soundboards is a shitty website. Use Fake You Dot Com instead.",
     //r#"
     //beep. beep. beep. beep. beep.
