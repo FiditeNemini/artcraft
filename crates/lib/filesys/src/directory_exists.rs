75607b3c125335d07fd1c0b881ee21cf08f0011e
Brandon Thomas <echelon@gmail.com>
2023-04-20 00:27:37 -0700

    tests, aspath

--- crates/lib/filesys/src/directory_exists.rs
+++ crates/lib/filesys/src/directory_exists.rs
@@ -1,11 +1,50 @@
-use std::path::PathBuf;
+use std::path::Path;
 
-pub fn directory_exists(path: &PathBuf) -> bool {
-  if !path.exists() {
+pub fn directory_exists<P: AsRef<Path>>(path: P) -> bool {
+  let path_ref = path.as_ref();
+  if !path_ref.exists() {
     return false;
   }
-  if !path.is_dir() {
+  if !path_ref.is_dir() {
     return false;
   }
   true
+}
+
+#[cfg(test)]
+mod tests {
+  use std::path::PathBuf;
+  use crate::directory_exists::directory_exists;
+
+  #[test]
+  fn test_directory_exists() {
+    // Common directories
+    assert_eq!(true, directory_exists("."));
+    assert_eq!(true, directory_exists("./"));
+    assert_eq!(true, directory_exists("../"));
+    assert_eq!(true, directory_exists("/"));
+
+    // As PathBuf
+    assert_eq!(true, directory_exists(PathBuf::from(".")));
+    assert_eq!(true, directory_exists(PathBuf::from("./")));
+    assert_eq!(true, directory_exists(PathBuf::from("../")));
+    assert_eq!(true, directory_exists(PathBuf::from("/")));
+
+    // Other directories
+    assert_eq!(true, directory_exists("../../../test_data/audio/flac"));
+    assert_eq!(true, directory_exists(PathBuf::from("../../../test_data/audio/flac")));
+  }
+
+  #[test]
+  fn test_file_does_not_exist() {
+    assert_eq!(false, directory_exists(""));
+    assert_eq!(false, directory_exists("   "));
+    assert_eq!(false, directory_exists("foo"));
+    assert_eq!(false, directory_exists("foo/bar/baz"));
+    assert_eq!(false, directory_exists("/foo/bar/baz"));
+
+    assert_eq!(false, directory_exists(PathBuf::from("")));
+    assert_eq!(false, directory_exists(PathBuf::from("   ")));
+    assert_eq!(false, directory_exists(PathBuf::from("foo/bar/baz")));
+  }
 }
