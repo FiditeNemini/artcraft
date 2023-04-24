523ed4be4647f854408296eab4a2accb5d92e7fe
Brandon Thomas <echelon@gmail.com>
2023-04-24 00:51:23 -0700

    simplify audio recorder

--- packages/fakeyou.com/src/v2/view/pages/vc/vc_model_list/components/RecordComponent.tsx
+++ packages/fakeyou.com/src/v2/view/pages/vc/vc_model_list/components/RecordComponent.tsx
@@ -1,52 +1,55 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useAudioRecorder } from "react-audio-voice-recorder";
 import { InputVcAudioPlayer } from "../../../../_common/InputVcAudioPlayer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/pro-solid-svg-icons";
 
-export default function RecordComponent() {
-  const [recorded, setRecorded] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [audioLink, setAudioLink] = useState<string>();
+interface RecorderProps {
+  recordingBlob: any;
+}
 
+function RecordedAudioComponent(props: RecorderProps) {
+
+  // Only generate the URL on change.
+  const audioLink = useMemo(() => {
+    if (!props.recordingBlob) {
+      return;
+    }
+    return URL.createObjectURL(props.recordingBlob);
+  }, [props.recordingBlob]);
+
+  if (!props.recordingBlob) {
+    return <></>;
+  }
+
+  return (
+    <div className="panel panel-inner rounded p-3">
+      <InputVcAudioPlayer filename={audioLink as string} />
+    </div>
+  );
+}
+
+export default function RecordComponent() {
   const { startRecording, stopRecording, recordingBlob, isRecording } =
     useAudioRecorder();
 
   const handleStopRecording = (blob: any) => {
     stopRecording();
-
-    if (recordingBlob) {
-      console.log('blob', recordingBlob);
-      const url = URL.createObjectURL(recordingBlob);
-      setAudioLink(url);
-      setRecorded(true);
-    }
-
-    setLoading(!recordingBlob);
   };
 
   return (
     <div className="d-flex flex-column gap-3" id="record-audio">
       {isRecording ? (
         <button className="btn btn-secondary" onClick={handleStopRecording}>
-          {loading ? (
-            <div className="d-flex align-items-center">
-              <div className="spinner-border spinner-border-sm" role="status">
-                <span className="visually-hidden">Saving...</span>
-              </div>
-              Saving
+          <div className="d-flex align-items-center">
+            <div
+              className="spinner-grow spinner-grow-sm text-danger me-2"
+              role="status"
+            >
+              <span className="visually-hidden">Recording...</span>
             </div>
-          ) : (
-            <div className="d-flex align-items-center">
-              <div
-                className="spinner-grow spinner-grow-sm text-danger me-2"
-                role="status"
-              >
-                <span className="visually-hidden">Recording...</span>
-              </div>
-              Stop Recording
-            </div>
-          )}
+            Stop Recording
+          </div>
         </button>
       ) : (
         <button className="btn btn-secondary" onClick={startRecording}>
@@ -55,13 +58,7 @@ export default function RecordComponent() {
         </button>
       )}
 
-      {recorded ? (
-        <div className="panel panel-inner rounded p-3">
-          <InputVcAudioPlayer filename={audioLink as string} />
-        </div>
-      ) : (
-        <></>
-      )}
+      <RecordedAudioComponent recordingBlob={recordingBlob} />
     </div>
   );
 }
