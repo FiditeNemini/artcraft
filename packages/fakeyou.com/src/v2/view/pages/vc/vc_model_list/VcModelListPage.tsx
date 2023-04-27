0afd7df0bbd27d01ab89e4c6ff58f3b741b1456a
BFlat <bombayapirat@gmail.com>
2023-04-27 15:22:11 +0700

    fixed link

--- packages/fakeyou.com/src/v2/view/pages/vc/vc_model_list/VcModelListPage.tsx
+++ packages/fakeyou.com/src/v2/view/pages/vc/vc_model_list/VcModelListPage.tsx
@@ -187,7 +187,7 @@ function VcModelListPage(props: Props) {
           <span className="fw-medium">
             We're paying you for new voice-to-voice model submissions!
           </span>
-          <a href="httpsL//discord.gg/fakeyou" className="fw-semibold ms-2">
+          <a href="https://discord.gg/fakeyou" className="fw-semibold ms-2">
             Join our Discord{" "}
             <FontAwesomeIcon icon={faArrowRight} className="ms-1" />
           </a>
