a5ce9a3cc4509629f0ee3742f7abfcd9db57ab50
Brandon Thomas <echelon@gmail.com>
2023-04-30 08:16:27 -0700

    analytics

--- _sql/analytics/generic_inference/generic_inference_jobs.sql
+++ _sql/analytics/generic_inference/generic_inference_jobs.sql
@@ -16,26 +16,31 @@ order by id desc
 limit 50;
 
 -- Same, but with usernames
-select
-    jobs.id,
-    jobs.maybe_input_source_token,
-    u.username,
-    jobs.assigned_worker,
-    jobs.assigned_cluster,
-    TRUNCATE(jobs.success_execution_millis / 1000 / 60, 2) as execution_minutes,
-    jobs.success_execution_millis as execution_millis,
-    m.original_duration_millis as duration_millis,
-    jobs.success_execution_millis / m.original_duration_millis as ratio
-from generic_inference_jobs AS jobs
-         left join users AS u on
-        u.token = jobs.maybe_creator_user_token
-         left join media_uploads AS m on
-        m.token = jobs.maybe_input_source_token
-where status != 'pending'
-and success_execution_millis IS NOT NULL
-order by id desc
-    limit 50;
-
+select *
+from (
+    select
+        jobs.id,
+        u.username,
+        jobs.assigned_worker,
+        jobs.assigned_cluster,
+        jobs.maybe_input_source_token,
+        m.media_source,
+        m.maybe_original_mime_type as mime_type,
+        TRUNCATE(jobs.success_execution_millis / 1000 / 60, 2) as execution_minutes,
+        jobs.success_execution_millis as execution_millis,
+        m.original_duration_millis as duration_millis,
+        jobs.success_execution_millis / m.original_duration_millis as ratio
+    from generic_inference_jobs AS jobs
+        left join users AS u on
+            u.token = jobs.maybe_creator_user_token
+        left join media_uploads AS m on
+            m.token = jobs.maybe_input_source_token
+    where status != 'pending'
+    and success_execution_millis IS NOT NULL
+    order by id desc
+        limit 50
+) as t
+order by execution_minutes desc;
 
 -- Get pending so-vits-svc jobs
 select count(*)
