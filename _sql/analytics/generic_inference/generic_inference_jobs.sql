6ad2468e598d62b3253e67744b350264d4e40278
Brandon Thomas <echelon@gmail.com>
2023-04-30 08:09:07 -0700

    analytics queries

--- _sql/analytics/generic_inference/generic_inference_jobs.sql
+++ _sql/analytics/generic_inference/generic_inference_jobs.sql
@@ -22,14 +22,20 @@ select
     u.username,
     jobs.assigned_worker,
     jobs.assigned_cluster,
-    jobs.success_execution_millis / 1000 / 60 as minutes
+    TRUNCATE(jobs.success_execution_millis / 1000 / 60, 2) as execution_minutes,
+    jobs.success_execution_millis as execution_millis,
+    m.original_duration_millis as duration_millis,
+    jobs.success_execution_millis / m.original_duration_millis as ratio
 from generic_inference_jobs AS jobs
-left join users AS u on
-    u.token = jobs.maybe_creator_user_token
+         left join users AS u on
+        u.token = jobs.maybe_creator_user_token
+         left join media_uploads AS m on
+        m.token = jobs.maybe_input_source_token
 where status != 'pending'
 and success_execution_millis IS NOT NULL
 order by id desc
     limit 50;
+
 
 -- Get pending so-vits-svc jobs
 select count(*)
