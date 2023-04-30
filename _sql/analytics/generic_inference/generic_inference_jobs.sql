3d63e34ca1ad0e7d1a1d55182e2e999388c5e88e
Brandon Thomas <echelon@gmail.com>
2023-04-29 23:36:02 -0700

    analytics queries

--- _sql/analytics/generic_inference/generic_inference_jobs.sql
+++ _sql/analytics/generic_inference/generic_inference_jobs.sql
@@ -14,3 +14,25 @@ where status != 'pending'
 and success_execution_millis IS NOT NULL
 order by id desc
 limit 50;
+
+-- Same, but with usernames
+select
+    jobs.id,
+    jobs.maybe_input_source_token,
+    u.username,
+    jobs.assigned_worker,
+    jobs.assigned_cluster,
+    jobs.success_execution_millis / 1000 / 60 as minutes
+from generic_inference_jobs AS jobs
+left join users AS u on
+    u.token = jobs.maybe_creator_user_token
+where status != 'pending'
+and success_execution_millis IS NOT NULL
+order by id desc
+    limit 50;
+
+-- Get pending so-vits-svc jobs
+select count(*)
+from generic_inference_jobs
+where maybe_model_type = 'so_vits_svc'
+  and status='pending';
