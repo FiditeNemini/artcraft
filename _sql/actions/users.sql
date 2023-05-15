6ec39b9c68fbf436f3cf4fc885ab9e051eee2617
Brandon Thomas <echelon@gmail.com>
2023-05-14 21:00:27 -0700

    loyalty for VC contributors

--- _sql/actions/users.sql
+++ _sql/actions/users.sql
@@ -17,4 +17,19 @@ where token IN (
         ) as t
 );
 
+-- Upgrade top VC model uploaders to a loyalty plan
+update users
+set maybe_loyalty_program_key = 'fakeyou_contributor'
+where token IN (
+    select creator_user_token from
+        (
+            select count(*) as upload_count,
+                   creator_user_token
+            from voice_conversion_models
+            group by creator_user_token
+            having upload_count >= 10
+            order by upload_count desc
+        ) as t
+);
+
 
