5ec2cde68fc770995e169932211a3667af1c220d
Brandon Thomas <echelon@gmail.com>
2023-05-22 11:15:19 -0700

    report errors better

--- _sql/analytics/generic_inference/generic_inference_jobs.sql
+++ _sql/analytics/generic_inference/generic_inference_jobs.sql
@@ -26,7 +26,8 @@ from (
              jobs.id,
              jobs.status,
              jobs.attempt_count as attempts,
-             jobs.internal_debugging_failure_reason as i_failure_reason,
+             trim(replace(replace(jobs.internal_debugging_failure_reason, "\n", ""), "\r", ""))
+                 as i_failure_reason,
              u.username,
              jobs.assigned_worker,
              jobs.assigned_cluster,
