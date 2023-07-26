e5289809fd49f7b7e650fb1f77d4c21855b44925
Brandon Thomas <echelon@gmail.com>
2023-07-26 01:13:38 -0400

    more analytics queries

--- _sql/analytics/analytics_stripe_sigma/subscriptions.sql
+++ _sql/analytics/analytics_stripe_sigma/subscriptions.sql
@@ -62,5 +62,29 @@ join (
 on s.id = user_tokens.subscription_id
 order by s.id asc
 
-
-
+-- Without emails
+select
+    s.id as subscription_id,
+    user_tokens.user_token,
+    usernames.username,
+    s.status as subscription_status,
+    s.created,
+    s.cancel_at,
+    s.canceled_at
+from subscriptions as s
+join (
+    select
+        m.subscription_id,
+        m.value as username
+    from subscriptions_metadata as m
+    where m.key = 'username'
+) as usernames
+on s.id = usernames.subscription_id
+join (
+    select m.subscription_id,
+           m.value as user_token
+    from subscriptions_metadata as m
+    where m.key = 'user_token'
+) as user_tokens
+on s.id = user_tokens.subscription_id
+order by s.id asc
---
+++ _sql/analytics/users/top_users_by_usage.sql
@@ -0,0 +1,52 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+SELECT
+    u.username as username,
+    j.token as token
+FROM users AS u
+JOIN tts_inference_jobs AS j
+ON u.token = j.maybe_creator_user_token
+  AND j.created_at > NOW() - INTERVAL 31 DAY
+
+
+SELECT
+    t.maybe_creator_user_token as user_token,
+    u.username,
+    count(*) as usage_count
+FROM tts_results as t
+JOIN users as u
+ON u.token = t.maybe_creator_user_token
+WHERE t.maybe_creator_user_token IS NOT NULL
+GROUP BY t.maybe_creator_user_token
+ORDER BY usage_count DESC
+LIMIT 50000;
+
+
+SELECT
+    v.maybe_creator_user_token as user_token,
+    u.username,
+    count(*) as usage_count
+FROM voice_conversion_results as v
+JOIN users as u
+ON u.token = v.maybe_creator_user_token
+WHERE v.maybe_creator_user_token IS NOT NULL
+GROUP BY v.maybe_creator_user_token
+ORDER BY usage_count DESC
+LIMIT 5000;
+
+
+SELECT
+    w.maybe_creator_user_token as user_token,
+    u.username,
+    count(*) as usage_count
+FROM w2l_results as w
+         JOIN users as u
+              ON u.token = w.maybe_creator_user_token
+WHERE w.maybe_creator_user_token IS NOT NULL
+GROUP BY w.maybe_creator_user_token
+ORDER BY usage_count DESC
+LIMIT 5000;
+
+
