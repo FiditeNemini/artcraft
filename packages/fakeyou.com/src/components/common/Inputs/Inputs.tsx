af9981c4e51af1f858cf6fdb5c43e6b437cb8587
BFlat <bombayapirat@gmail.com>
2023-08-10 15:54:43 +0700

    rename scss file to Inputs

--- packages/fakeyou.com/src/components/common/Inputs/Inputs.tsx
+++ packages/fakeyou.com/src/components/common/Inputs/Inputs.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import "./Input.scss";
+import "./Inputs.scss";
 import {
   default as ReactSelect,
   Props as ReactSelectProps,
