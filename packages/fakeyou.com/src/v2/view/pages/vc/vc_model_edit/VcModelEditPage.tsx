a9e1000631a6847b119046a9f191e200a140e4a7
BFlat <bombayapirat@gmail.com>
2023-08-10 13:55:44 +0700

    select field component

--- packages/fakeyou.com/src/v2/view/pages/vc/vc_model_edit/VcModelEditPage.tsx
+++ packages/fakeyou.com/src/v2/view/pages/vc/vc_model_edit/VcModelEditPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SessionWrapper } from "@storyteller/components/src/session/SessionWrapper";
 import { useParams } from "react-router-dom";
-import Input from "components/common/Input/Input";
+import { Input, TextArea, Select } from "components/common/Input/Input";
 import Panel from "components/common/Panel/Panel";
-import { faUser } from "@fortawesome/pro-solid-svg-icons";
+import { faEye, faFilePen } from "@fortawesome/pro-solid-svg-icons";
+import PageHeaderModelView from "components/layout/PageHeaderModelView/PageHeaderModelView";
 
 interface VcModelEditPageProps {
   sessionWrapper: SessionWrapper;
@@ -13,18 +14,49 @@ const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   console.log(event.target.value);
 };
 
+const savebtn = <button className="btn btn-primary">Save Changes</button>;
+
+const visibility = [
+  { value: "public", label: "Public" },
+  { value: "hidden", label: "Hidden" },
+];
+
 export default function VcModelEditPage(props: VcModelEditPageProps) {
   let { token } = useParams() as { token: string };
   return (
     <div>
+      <PageHeaderModelView
+        title="Solid Snake"
+        subText="Solid Snake"
+        view="edit"
+        titleIcon={faFilePen}
+        extras={savebtn}
+        modelType="V2V"
+      />
+
       <Panel padding>
-        <Input
-          label="Username"
-          type="text"
-          placeholder="Username"
-          icon={faUser}
-          onChange={handleChange}
-        />
+        <div className="d-flex flex-column gap-4">
+          <Input
+            label="Name"
+            type="text"
+            placeholder="Model title"
+            onChange={handleChange}
+          />
+
+          <TextArea
+            rows={3}
+            label="Description"
+            placeholder="Description"
+            onChange={() => {}}
+          />
+
+          <Select
+            icon={faEye}
+            defaultValue={visibility[0]}
+            options={visibility}
+            label="Visibility"
+          />
+        </div>
       </Panel>
     </div>
   );
