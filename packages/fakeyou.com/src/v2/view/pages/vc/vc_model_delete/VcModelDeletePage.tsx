300fd206fa28bda7924d1e86583a265afe5d6bb5
BFlat <bombayapirat@gmail.com>
2023-08-11 12:34:26 +0700

    delete page init

--- packages/fakeyou.com/src/v2/view/pages/vc/vc_model_delete/VcModelDeletePage.tsx
+++ packages/fakeyou.com/src/v2/view/pages/vc/vc_model_delete/VcModelDeletePage.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { SessionWrapper } from "@storyteller/components/src/session/SessionWrapper";
 import { useParams } from "react-router-dom";
-import { Input, TextArea, Select } from "components/common/Inputs/Inputs";
 import Panel from "components/common/Panel/Panel";
-import { faEye, faFilePen, faSave } from "@fortawesome/pro-solid-svg-icons";
+import { faSave, faTrash } from "@fortawesome/pro-solid-svg-icons";
 import PageHeaderModelView from "components/layout/PageHeaderModelView/PageHeaderModelView";
 import Button from "components/common/Button/Button";
 import PageContainer from "components/common/PageContainer/PageContainer";
 
-interface VcModelEditPageProps {
+interface VcModelDeletePageProps {
   sessionWrapper: SessionWrapper;
 }
 
@@ -22,48 +21,21 @@ const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
 
 const savebtn = <button className="btn btn-primary">Save Changes</button>;
 
-const visibility = [
-  { value: "public", label: "Public" },
-  { value: "hidden", label: "Hidden" },
-];
-
-export default function VcModelEditPage(props: VcModelEditPageProps) {
+export default function VcModelDeletePage(props: VcModelDeletePageProps) {
   let { token } = useParams() as { token: string };
   return (
     <PageContainer>
       <PageHeaderModelView
         title="Solid Snake"
         subText="Solid Snake"
-        view="edit"
-        titleIcon={faFilePen}
+        view="delete"
+        titleIcon={faTrash}
         extras={savebtn}
         modelType="V2V"
       />
 
       <Panel padding>
         <div className="d-flex flex-column gap-4">
-          <Input
-            label="Name"
-            type="text"
-            placeholder="Model title"
-            onChange={handleChange}
-          />
-
-          <TextArea
-            rows={3}
-            label="Description"
-            placeholder="Description"
-            onChange={() => {}}
-          />
-
-          <Select
-            isSearchable={false}
-            icon={faEye}
-            defaultValue={visibility[0]}
-            options={visibility}
-            label="Visibility"
-          />
-
           <Button label="Save Changes" icon={faSave} onClick={handleSave} />
         </div>
       </Panel>
