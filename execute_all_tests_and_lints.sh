a4524ee679f0b28ebd0564183c76db89885c5e03
Brandon Thomas <echelon@gmail.com>
2023-08-23 17:13:11 -0400

    execute all lints and tests

---
+++ execute_all_tests_and_lints.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo 'Run cargo check...'
+
+cargo check
+
+echo 'Run lints...'
+./execute_lints.sh
+
+echo 'Run tests...'
+./execute_tests.sh
+
+echo 'All tests and lints passed successfully!'
+
