8dca0fa6f3a1ca4a1200fe019548f8f1c5a67e2a
BFlat <bombayapirat@gmail.com>
2023-09-29 05:03:12 +0700

    face animation overflow fix

--- packages/fakeyou.com/src/v2/view/pages/landing/LandingPage.tsx
+++ packages/fakeyou.com/src/v2/view/pages/landing/LandingPage.tsx
@@ -303,13 +303,17 @@ function LandingPage(props: Props) {
             >
               <div className="d-flex px-4 pt-4 align-items-start w-100">
                 <div className="flex-grow-1">
-                  <h4 className="fw-bold text-white mb-1 d-flex align-items-center text-nowrap">
-                    <span className="badge-new mt-0 me-2">
+                  <div className="mb-1">
+                    <span className="badge-new d-inline-flex align-items-center mb-2 me-2">
                       <FontAwesomeIcon icon={faSparkles} className="me-1" />
                       {t("productNewTag")}
                     </span>
-                    {t("productFaceAnimatorTitle")}
-                  </h4>
+
+                    <h4 className="fw-bold text-white d-inline-flex align-items-center mb-0">
+                      <span>{t("productFaceAnimatorTitle")}</span>
+                    </h4>
+                  </div>
+
                   <h6 className="fw-normal opacity-75 text-white">
                     {t("productFaceAnimatorText")}
                   </h6>
