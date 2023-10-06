95888e683d101911c28dd24c01fd47ffe21058fb
Michael Chung <michael.chung@databites.ca>
2023-10-06 18:01:25 -0400

    Formatter works now, braces can get properly placed

--- crates/service/job/inference_job/src/job/job_types/tts/process_single_tts_job.rs
+++ crates/service/job/inference_job/src/job/job_types/tts/process_single_tts_job.rs
@@ -8,7 +8,7 @@ use tokens::tokens::tts_models::TtsModelToken;
 
 use crate::job::job_loop::job_success_result::JobSuccessResult;
 use crate::job::job_loop::process_single_job_error::ProcessSingleJobError;
-use crate::job::job_types::tts::{tacotron2_v2_early_fakeyou, vits};
+use crate::job::job_types::tts::{ tacotron2_v2_early_fakeyou, vits };
 use crate::job::job_types::tts::tacotron2_v2_early_fakeyou::process_job::ProcessJobArgs;
 use crate::job::job_types::tts::vits::process_job::VitsProcessJobArgs;
 use crate::job::job_types::tts::vall_e_x::process_job::VALLEXProcessJobArgs;
@@ -16,96 +16,102 @@ use crate::job_dependencies::JobDependencies;
 
 use super::vall_e_x;
 
-pub async fn process_single_tts_job(job_dependencies: &JobDependencies, job: &AvailableInferenceJob) -> Result<JobSuccessResult, ProcessSingleJobError> {
+pub async fn process_single_tts_job(
+    job_dependencies: &JobDependencies,
+    job: &AvailableInferenceJob
+) -> Result<JobSuccessResult, ProcessSingleJobError> {
+    let tts_model_token = match job.maybe_model_token.as_deref() {
+        None => {
+            return Err(ProcessSingleJobError::Other(anyhow!("no model token on job")));
+        }
+        Some(model_token) => TtsModelToken::new_from_str(model_token),
+    };
 
-  let tts_model_token = match job.maybe_model_token.as_deref() {
-    None => return Err(ProcessSingleJobError::Other(anyhow!("no model token on job"))),
-    Some(model_token) => TtsModelToken::new_from_str(model_token),
-  };
+    // TODO: Move common checks for slurs, etc. here.
 
-  // TODO: Move common checks for slurs, etc. here.
+    let raw_inference_text = job.maybe_raw_inference_text
+        .as_deref()
+        .ok_or(ProcessSingleJobError::Other(anyhow!("no inference text")))?;
 
-  let raw_inference_text = job.maybe_raw_inference_text
-      .as_deref()
-      .ok_or(ProcessSingleJobError::Other(anyhow!("no inference text")))?;
+    // TODO: Interrogate cache (which also depends on other flags)
+    let maybe_tts_model = get_tts_model_for_inference_improved(
+        &job_dependencies.mysql_pool,
+        tts_model_token.as_str()
+    ).await.map_err(|err| ProcessSingleJobError::Other(anyhow!("database error: {:?}", err)))?;
 
-  // TODO: Interrogate cache (which also depends on other flags)
-  let maybe_tts_model = get_tts_model_for_inference_improved(
-    &job_dependencies.mysql_pool, tts_model_token.as_str())
-      .await
-      .map_err(|err| ProcessSingleJobError::Other(anyhow!("database error: {:?}", err)))?;
+    // TODO: Attempt to grab job lock
 
-// TODO: Attempt to grab job lock
+    //  // ==================== ATTEMPT TO GRAB JOB LOCK ==================== //
+    //
+    //  info!("Attempting to grab lock for job: {}", job.inference_job_token);
+    //
+    //  let lock_acquired =
+    //      mark_tts_inference_job_pending_and_grab_lock(&job_args.mysql_pool, job.id)
+    //          .await
+    //          .map_err(|e| ProcessSingleJobError::Other(e))?;
+    //
+    //  if !lock_acquired {
+    //    warn!("Could not acquire job lock for: {:?}", &job.id);
+    //    let duration = start.elapsed();
+    //
+    //    since_creation_span.set_attribute("status", "failure");
+    //    since_creation_span.set_duration(duration);
+    //
+    //    job_iteration_span.set_attribute("status", "failure");
+    //    job_iteration_span.set_duration(duration);
+    //
+    //    return Ok((since_creation_span, job_iteration_span));
+    //  }
+    //
+    //  info!("Lock acquired for job: {}", job.inference_job_token);
 
-//  // ==================== ATTEMPT TO GRAB JOB LOCK ==================== //
-//
-//  info!("Attempting to grab lock for job: {}", job.inference_job_token);
-//
-//  let lock_acquired =
-//      mark_tts_inference_job_pending_and_grab_lock(&job_args.mysql_pool, job.id)
-//          .await
-//          .map_err(|e| ProcessSingleJobError::Other(e))?;
-//
-//  if !lock_acquired {
-//    warn!("Could not acquire job lock for: {:?}", &job.id);
-//    let duration = start.elapsed();
-//
-//    since_creation_span.set_attribute("status", "failure");
-//    since_creation_span.set_duration(duration);
-//
-//    job_iteration_span.set_attribute("status", "failure");
-//    job_iteration_span.set_duration(duration);
-//
-//    return Ok((since_creation_span, job_iteration_span));
-//  }
-//
-//  info!("Lock acquired for job: {}", job.inference_job_token);
+    // Look for the Zeroshot job type
+    // let job_success_result = match job.maybe_model_type {
+    //   Some(InferenceModelType::VallEX) => {
 
-  // Look for the Zeroshot job type
-  // let job_success_result = match job.maybe_model_type {
-  //   Some(InferenceModelType::VallEX) => {
-      
-  //   }
-  //   Some(model_type) => return Err(ProcessSingleJobError::Other(anyhow!("wrong model type: {:?}", model_type))),
-  //   None => return Err(ProcessSingleJobError::Other(anyhow!("no model type in record"))),
-  // };
+    //   }
+    //   Some(model_type) => return Err(ProcessSingleJobError::Other(anyhow!("wrong model type: {:?}", model_type))),
+    //   None => return Err(ProcessSingleJobError::Other(anyhow!("no model type in record"))),
+    // };
 
     if let Some(model_type) = job.maybe_model_type {
-      if model_type ==  InferenceModelType::VallEX {
-          let job_success_result = vall_e_x::process_job::process_job(VALLEXProcessJobArgs {
-            job_dependencies,
-            job,
-          }).await?;
-          Ok(job_success_result)
-      };
-      // do nothing...
-    };
+        if model_type == InferenceModelType::VallEX {
+            let job_success_result = vall_e_x::process_job::process_job(VALLEXProcessJobArgs {
+                job_dependencies,
+                job,
+            }).await?;
+            Ok(job_success_result);
+        }
+        // do nothing try regular tts else other zero shot models
+    }
 
     // lets run the regular tts
     let tts_model = match maybe_tts_model {
-      None => return Err(ProcessSingleJobError::Other(anyhow!("tts model not found: {:?}", tts_model_token))),
-      Some(model) => model,
+        None => {
+            return Err(
+                ProcessSingleJobError::Other(anyhow!("tts model not found: {:?}", tts_model_token))
+            );
+        }
+        Some(model) => model,
     };
 
     let job_success_result: JobSuccessResult = match tts_model.tts_model_type {
-      TtsModelType::Tacotron2 => {
-        tacotron2_v2_early_fakeyou::process_job::process_job(ProcessJobArgs {
-          job_dependencies,
-          job,
-          tts_model: &tts_model,
-          raw_inference_text,
-        }).await?
-      }
-      TtsModelType::Vits => {
-        vits::process_job::process_job(VitsProcessJobArgs {
-          job_dependencies,
-          job,
-          tts_model: &tts_model,
-          raw_inference_text,
-        }).await?
-      }
+        TtsModelType::Tacotron2 => {
+            tacotron2_v2_early_fakeyou::process_job::process_job(ProcessJobArgs {
+                job_dependencies,
+                job,
+                tts_model: &tts_model,
+                raw_inference_text,
+            }).await?
+        }
+        TtsModelType::Vits => {
+            vits::process_job::process_job(VitsProcessJobArgs {
+                job_dependencies,
+                job,
+                tts_model: &tts_model,
+                raw_inference_text,
+            }).await?
+        }
     };
     Ok(job_success_result)
-  } 
 }
-
