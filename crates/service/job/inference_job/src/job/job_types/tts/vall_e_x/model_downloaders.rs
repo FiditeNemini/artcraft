3845c632b24adba3ff00c57805bca79ed218a453
Michael Chung <michael.chung@databites.ca>
2023-10-10 17:40:33 -0400

    Turns out we don't support folder downloads only files.

--- crates/service/job/inference_job/src/job/job_types/tts/vall_e_x/model_downloaders.rs
+++ crates/service/job/inference_job/src/job/job_types/tts/vall_e_x/model_downloaders.rs
@@ -4,7 +4,7 @@ pub struct VallEXDownloaders {
     pub vall_e_x: VallEXModelMapping,
     pub whisper_lg: WhisperLargeModelMapping,
     pub whisper_md: WhisperMediumModelMapping,
-    pub vocos_encodec_24khz: VocosEncodecModelMapping
+    pub vocos_encodec_24khz: VocosEncodecModelMapping,
 }
 
 crate::impl_model_downloader!(
@@ -13,64 +13,57 @@ crate::impl_model_downloader!(
     "VALL_E_X_MAPPING_BUCKET_PATH",
     "/dependencies/zero_shot_tts/vall-e-x_1.0/vallex-checkpoint.pt",
     "VALL_E_X_MAPPING_FILESYSTEM_PATH",
-    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/vallex-checkpoint.pt" 
-  );
-  
-  crate::impl_model_downloader!(
+    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/vallex-checkpoint.pt"
+);
+
+crate::impl_model_downloader!(
     WhisperLargeModelMapping,
     "whisper Large Model Mapping",
     "WHISPER_LARGE_MAPPING_BUCKET_PATH",
-    "/dependencies/zero_shot_tts/vall-e-x_1.0/whisper-large",
+    "/dependencies/zero_shot_tts/vall-e-x_1.0/whisper-large/model.safetensors",
     "WHISPER_LARGE_MAPPING_FILESYSTEM_PATH",
-    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/whisper-large"
-  );
-  
-  crate::impl_model_downloader!(
+    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/whisper-large/model.safetensors"
+);
+
+crate::impl_model_downloader!(
     WhisperMediumModelMapping,
     "whisper Medium Model Mapping",
     "WHISPER_MEDIUM_TENSOR_BUCKET_PATH",
-    "/dependencies/zero_shot_tts/vall-e-x_1.0/whisper-medium",
+    "/dependencies/zero_shot_tts/vall-e-x_1.0/whisper-medium/model.safetensors",
     "WHISPER_MEDIUM_TENSOR_FILESYSTEM_PATH",
-    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/whisper-medium"
-  );
-  
-  crate::impl_model_downloader!(
+    "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/whisper-medium/model.safetensors"
+);
+
+crate::impl_model_downloader!(
     VocosEncodecModelMapping,
     "Vocos Encodec Model Mapping",
     "VOCOS_ENCODEC_TENSOR_BUCKET_PATH",
     "/dependencies/zero_shot_tts/vall-e-x_1.0/encodec_pytorch_model.bin",
     "VOCOS_ENCODEC_TENSOR_FILESYSTEM_PATH",
     "/tmp/downloads/zero_shot_tts/vall-e-x_1.0/encodec_pytorch_model.bin"
-  );
+);
 
 impl VallEXDownloaders {
     pub fn build_all_from_env() -> Self {
-      Self {
-        vall_e_x: VallEXModelMapping::from_env(),
-        whisper_lg: WhisperLargeModelMapping::from_env(),
-        whisper_md: WhisperMediumModelMapping::from_env(),
-        vocos_encodec_24khz: VocosEncodecModelMapping::from_env()
-      }
+        Self {
+            vall_e_x: VallEXModelMapping::from_env(),
+            whisper_lg: WhisperLargeModelMapping::from_env(),
+            whisper_md: WhisperMediumModelMapping::from_env(),
+            vocos_encodec_24khz: VocosEncodecModelMapping::from_env(),
+        }
     }
-  
+
     pub fn all_downloaders(&self) -> Vec<&dyn ModelDownloader> {
-      vec![
-        &self.vall_e_x,
-        &self.whisper_lg,
-        &self.whisper_md,
-        &self.vocos_encodec_24khz,
-      ]
+        vec![&self.vall_e_x, &self.whisper_lg, &self.whisper_md, &self.vocos_encodec_24khz]
     }
-  }
-  
+}
 
-
-  mod test {
+mod test {
     // use std::path::Path;
-  
+
     // use crate::job::job_types::lipsync::sad_talker::model_downloaders::{SadTalkerModelFirstMapping, SadTalkerModelSecondMapping};
     // use crate::util::model_downloader::ModelDownloader;
-  
+
     // #[test]
     // fn test_downloader_model_first_mapping() {
     //   let model = SadTalkerModelFirstMapping::default();
@@ -78,7 +71,7 @@ impl VallEXDownloaders {
     //   assert_eq!(model.get_cloud_bucket_path(), "/animation_sadtalker/model/mapping_00109-model.pth.tar");
     //   assert_eq!(model.get_filesystem_path(), Path::new("/tmp/downloads/sadtalker/mapping_00109-model.pth.tar"));
     // }
-  
+
     // #[test]
     // fn test_downloader_model_second_mapping() {
     //   let model = SadTalkerModelSecondMapping::default();
@@ -87,5 +80,4 @@ impl VallEXDownloaders {
     //   assert_eq!(model.get_filesystem_path(), Path::new("/tmp/downloads/sadtalker/mapping_00229-model.pth.tar"));
     // }
 
-  }
-  
+}
