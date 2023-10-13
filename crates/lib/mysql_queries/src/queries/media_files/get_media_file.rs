138057e105c2b60c8aeb4c157b1fff16e90c795f
Brandon Thomas <echelon@gmail.com>
2023-10-13 02:38:34 -0400

    v1 of media_file GET endpoint.
    Media files are used for inference outputs, file uploads, Zero Shot
    files, etc. We don't have any of the basic CRUD operations around them
    yet, so this is a start.
    
    This endpoint will need more metadata - moderation, file provenance,
    file details, etc. But this will do for now.

---
+++ crates/lib/mysql_queries/src/queries/media_files/get_media_file.rs
@@ -0,0 +1,196 @@
+// NB: Incrementally getting rid of build warnings...
+#![forbid(unused_imports)]
+#![forbid(unused_mut)]
+#![forbid(unused_variables)]
+
+use anyhow::anyhow;
+use chrono::{DateTime, Utc};
+use sqlx::MySqlPool;
+
+use enums::by_table::media_files::media_file_type::MediaFileType;
+use enums::common::visibility::Visibility;
+use errors::AnyhowResult;
+use tokens::tokens::media_files::MediaFileToken;
+use tokens::tokens::users::UserToken;
+
+#[derive(Serialize)]
+pub struct MediaFile {
+  pub token: MediaFileToken,
+
+  pub media_type: MediaFileType,
+
+  // TODO: Bucket hash bits.
+
+  // TODO: Other media details (file size, mime type, dimensions, duration, etc.)
+  // TODO: Provenance data (product, upload vs inference, model details and foreign keys)
+
+  pub maybe_creator_user_token: Option<UserToken>,
+  pub maybe_creator_username: Option<String>,
+  pub maybe_creator_display_name: Option<String>,
+  pub maybe_creator_gravatar_hash: Option<String>,
+
+  pub creator_set_visibility: Visibility,
+
+  pub public_bucket_directory_hash: String,
+  pub maybe_public_bucket_prefix: Option<String>,
+  pub maybe_public_bucket_extension: Option<String>,
+
+  pub created_at: DateTime<Utc>,
+  pub updated_at: DateTime<Utc>,
+
+  // pub maybe_moderator_fields: Option<MediaFileModeratorFields>,
+}
+
+// /// "Moderator-only fields" that we wouldn't want to expose to ordinary users.
+// /// It's the web endpoint controller's responsibility to clear these for non-mods.
+// #[derive(Serialize)]
+// pub struct MediaFileModeratorFields {
+//   pub model_creator_is_banned: bool,
+//   pub result_creator_is_banned_if_user: bool,
+//   pub result_creator_ip_address: String,
+//   pub result_creator_deleted_at: Option<DateTime<Utc>>,
+//   pub mod_deleted_at: Option<DateTime<Utc>>,
+//   pub maybe_mod_user_token: Option<String>,
+// }
+
+#[derive(Serialize)]
+pub struct MediaFileRaw {
+  pub token: MediaFileToken,
+
+  pub media_type: MediaFileType,
+
+  // TODO: Bucket hash bits.
+
+  pub maybe_creator_user_token: Option<UserToken>,
+  pub maybe_creator_username: Option<String>,
+  pub maybe_creator_display_name: Option<String>,
+  pub maybe_creator_gravatar_hash: Option<String>,
+
+  pub creator_set_visibility: Visibility,
+
+  pub public_bucket_directory_hash: String,
+  pub maybe_public_bucket_prefix: Option<String>,
+  pub maybe_public_bucket_extension: Option<String>,
+
+  //pub model_is_mod_approved: bool, // converted
+  //pub maybe_mod_user_token: Option<String>,
+
+  pub created_at: DateTime<Utc>,
+  pub updated_at: DateTime<Utc>,
+}
+
+pub async fn get_media_file(
+  media_file_token: &MediaFileToken,
+  can_see_deleted: bool,
+  mysql_pool: &MySqlPool
+) -> AnyhowResult<Option<MediaFile>> {
+
+  let record = if can_see_deleted {
+    select_including_deleted(media_file_token, mysql_pool).await
+  } else {
+    select_without_deleted(media_file_token, mysql_pool).await
+  };
+
+  let record = match record {
+    Ok(record) => record,
+    Err(ref err) => {
+      return match err {
+        sqlx::Error::RowNotFound => Ok(None),
+        _ => Err(anyhow!("database error: {:?}", err)),
+      }
+    }
+  };
+
+  Ok(Some(MediaFile {
+    token: record.token,
+    media_type: record.media_type,
+    maybe_creator_user_token: record.maybe_creator_user_token,
+    maybe_creator_username: record.maybe_creator_username,
+    maybe_creator_display_name: record.maybe_creator_display_name,
+    maybe_creator_gravatar_hash: record.maybe_creator_gravatar_hash,
+    creator_set_visibility: record.creator_set_visibility,
+    public_bucket_directory_hash: record.public_bucket_directory_hash,
+    maybe_public_bucket_prefix: record.maybe_public_bucket_prefix,
+    maybe_public_bucket_extension: record.maybe_public_bucket_extension,
+    created_at: record.created_at,
+    updated_at: record.updated_at,
+  }))
+}
+
+async fn select_including_deleted(
+  media_file_token: &MediaFileToken,
+  mysql_pool: &MySqlPool
+) -> Result<MediaFileRaw, sqlx::Error> {
+  sqlx::query_as!(
+      MediaFileRaw,
+        r#"
+SELECT
+    m.token as `token: tokens::tokens::media_files::MediaFileToken`,
+
+    m.media_type as `media_type: enums::by_table::media_files::media_file_type::MediaFileType`,
+
+    users.token as `maybe_creator_user_token: tokens::tokens::users::UserToken`,
+    users.username as maybe_creator_username,
+    users.display_name as maybe_creator_display_name,
+    users.email_gravatar_hash as maybe_creator_gravatar_hash,
+
+    m.creator_set_visibility as `creator_set_visibility: enums::common::visibility::Visibility`,
+
+    m.public_bucket_directory_hash,
+    m.maybe_public_bucket_prefix,
+    m.maybe_public_bucket_extension,
+
+    m.created_at,
+    m.updated_at
+
+FROM media_files AS m
+LEFT OUTER JOIN users
+    ON m.maybe_creator_user_token = users.token
+WHERE
+    m.token = ?
+        "#,
+      media_file_token
+    )
+    .fetch_one(mysql_pool)
+    .await
+}
+
+async fn select_without_deleted(
+  media_file_token: &MediaFileToken,
+  mysql_pool: &MySqlPool
+) -> Result<MediaFileRaw, sqlx::Error> {
+  sqlx::query_as!(
+      MediaFileRaw,
+        r#"
+SELECT
+    m.token as `token: tokens::tokens::media_files::MediaFileToken`,
+
+    m.media_type as `media_type: enums::by_table::media_files::media_file_type::MediaFileType`,
+
+    users.token as `maybe_creator_user_token: tokens::tokens::users::UserToken`,
+    users.username as maybe_creator_username,
+    users.display_name as maybe_creator_display_name,
+    users.email_gravatar_hash as maybe_creator_gravatar_hash,
+
+    m.creator_set_visibility as `creator_set_visibility: enums::common::visibility::Visibility`,
+
+    m.public_bucket_directory_hash,
+    m.maybe_public_bucket_prefix,
+    m.maybe_public_bucket_extension,
+
+    m.created_at,
+    m.updated_at
+
+FROM media_files AS m
+LEFT OUTER JOIN users
+    ON m.maybe_creator_user_token = users.token
+WHERE
+    m.token = ?
+    AND m.user_deleted_at IS NULL
+    AND m.mod_deleted_at IS NULL
+        "#,
+      media_file_token
+    )
+    .fetch_one(mysql_pool)
+    .await
+}
---
+++ crates/service/web/storyteller_web/src/http_server/endpoints/media_files/get_media_file.rs
@@ -0,0 +1,186 @@
+use std::fmt;
+use std::sync::Arc;
+
+use actix_web::{HttpMessage, HttpRequest, HttpResponse, web};
+use actix_web::error::ResponseError;
+use actix_web::http::StatusCode;
+use actix_web::web::Path;
+use chrono::{DateTime, Utc};
+use log::warn;
+
+use buckets::public::media_files::original_file::MediaFileBucketPath;
+use enums::by_table::media_files::media_file_type::MediaFileType;
+use enums::common::visibility::Visibility;
+use mysql_queries::queries::media_files::get_media_file::get_media_file;
+use tokens::tokens::media_files::MediaFileToken;
+
+use crate::http_server::common_responses::user_details_lite::UserDetailsLight;
+use crate::http_server::web_utils::response_error_helpers::to_simple_json_error;
+use crate::server_state::ServerState;
+
+/// For the URL PathInfo
+#[derive(Deserialize)]
+pub struct GetMediaFilePathInfo {
+  token: MediaFileToken,
+}
+
+#[derive(Serialize)]
+pub struct GetMediaFileSuccessResponse {
+  pub success: bool,
+  pub media_file: MediaFile,
+}
+
+#[derive(Serialize)]
+pub struct MediaFile {
+  pub token: MediaFileToken,
+
+  /// Type of media will dictate which fields are populated and what
+  /// the frontend should display (eg. video player vs audio player).
+  pub media_type: MediaFileType,
+
+  /// URL to the media file
+  pub public_bucket_path: String,
+
+  //// Provenance Data
+  //pub origin_product: MediaFileOriginProductCategory,
+  //pub origin_category: MediaFileOriginCategory,
+  //pub maybe_origin_model_type: Option<MediaFileOriginModelType>,
+  //pub maybe_origin_model_token: Option<String>,
+  //pub maybe_origin_filename: Option<String>,
+
+  // User
+  pub maybe_creator_user: Option<UserDetailsLight>,
+
+  pub creator_set_visibility: Visibility,
+
+  pub created_at: DateTime<Utc>,
+  pub updated_at: DateTime<Utc>,
+
+  //pub maybe_moderator_fields: Option<ModeratorFields>,
+}
+
+//#[derive(Serialize)]
+//pub struct ModeratorFields {
+//  pub creator_ip_address: String,
+//  pub creator_is_banned_if_user: bool,
+//  pub maybe_creator_deleted_at: Option<DateTime<Utc>>,
+//  pub maybe_mod_deleted_at: Option<DateTime<Utc>>,
+//}
+
+#[derive(Debug)]
+pub enum GetMediaFileError {
+  ServerError,
+  NotFound,
+}
+
+impl ResponseError for GetMediaFileError {
+  fn status_code(&self) -> StatusCode {
+    match *self {
+      GetMediaFileError::ServerError=> StatusCode::INTERNAL_SERVER_ERROR,
+      GetMediaFileError::NotFound=> StatusCode::NOT_FOUND,
+    }
+  }
+
+  fn error_response(&self) -> HttpResponse {
+    let error_reason = match self {
+      GetMediaFileError::ServerError => "server error".to_string(),
+      GetMediaFileError::NotFound => "not found".to_string(),
+    };
+
+    to_simple_json_error(&error_reason, self.status_code())
+  }
+}
+
+// NB: Not using derive_more::Display since Clion doesn't understand it.
+impl fmt::Display for GetMediaFileError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{:?}", self)
+  }
+}
+
+pub async fn get_media_file_handler(
+  http_request: HttpRequest,
+  path: Path<GetMediaFilePathInfo>,
+  server_state: web::Data<Arc<ServerState>>) -> Result<HttpResponse, GetMediaFileError>
+{
+  let maybe_user_session = server_state
+      .session_checker
+      .maybe_get_user_session(&http_request, &server_state.mysql_pool)
+      .await
+      .map_err(|e| {
+        warn!("Session checker error: {:?}", e);
+        GetMediaFileError::ServerError
+      })?;
+
+  let mut show_deleted_results = false;
+  let mut is_moderator = false;
+
+  if let Some(user_session) = maybe_user_session {
+    // NB: Moderators can see deleted results.
+    // Original creators cannot see them (unless they're moderators!)
+    show_deleted_results = user_session.can_delete_other_users_tts_results;
+    // Moderators get to see all the fields.
+    is_moderator = user_session.can_delete_other_users_tts_results
+        || user_session.can_edit_other_users_tts_models;
+  }
+
+  let result = get_media_file(
+    &path.token,
+    show_deleted_results,
+    &server_state.mysql_pool
+  ).await;
+
+  let result = match result {
+    Err(e) => {
+      warn!("query error: {:?}", e);
+      return Err(GetMediaFileError::ServerError);
+    }
+    Ok(None) => return Err(GetMediaFileError::NotFound),
+    Ok(Some(result)) => result,
+  };
+
+  //if let Some(moderator_fields) = result.maybe_moderator_fields.as_ref() {
+  //  // NB: The moderator fields will always be present before removal
+  //  // We don't want non-mods seeing stuff made by banned users.
+  //  if (moderator_fields.model_creator_is_banned || moderator_fields.result_creator_is_banned_if_user)
+  //      && !is_moderator{
+  //    return Err(GetMediaFileError::NotFound);
+  //  }
+  //}
+
+  //if !is_moderator {
+  //  result.maybe_moderator_fields = None;
+  //}
+
+  let public_bucket_path = MediaFileBucketPath::from_object_hash(
+    &result.public_bucket_directory_hash,
+    result.maybe_public_bucket_prefix.as_deref(),
+    result.maybe_public_bucket_extension.as_deref())
+      .get_full_object_path_str()
+      .to_string();
+
+  let response = GetMediaFileSuccessResponse {
+    success: true,
+    media_file: MediaFile {
+      token: result.token,
+      media_type: result.media_type,
+      public_bucket_path,
+      maybe_creator_user: UserDetailsLight::from_optional_db_fields_owned(
+        result.maybe_creator_user_token,
+        result.maybe_creator_username,
+        result.maybe_creator_display_name,
+        result.maybe_creator_gravatar_hash,
+      ),
+      creator_set_visibility: result.creator_set_visibility,
+      created_at: result.created_at,
+      updated_at: result.updated_at,
+    },
+  };
+
+  let body = serde_json::to_string(&response)
+    .map_err(|e| GetMediaFileError::ServerError)?;
+
+  Ok(HttpResponse::Ok()
+    .content_type("application/json")
+    .body(body))
+}
