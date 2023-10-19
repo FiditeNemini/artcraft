6d01506bd17c2fdff22557762a5c2d0d8892cb4c
Brandon Thomas <echelon@gmail.com>
2023-10-19 05:53:44 -0400

    fix cookie path

--- crates/service/plugins/users/src/cookies/anonymous_visitor_tracking/avt_cookie_manager.rs
+++ crates/service/plugins/users/src/cookies/anonymous_visitor_tracking/avt_cookie_manager.rs
@@ -39,10 +39,9 @@ impl AvtCookieManager {
     Ok(Cookie::build(VISITOR_COOKIE_NAME, jwt_string)
         .secure(make_secure) // HTTPS-only
         .permanent()
+        .path("/") // NB: Otherwise it'll be set to `/v1`
         //.domain(&self.cookie_domain)
-        //.path("/")
         //.http_only(true) // Not exposed to Javascript
-        //.expires(OffsetDateTime::now_utc() + time::Duration::days(365))
         //.same_site(SameSite::Lax)
         .finish())
   }
