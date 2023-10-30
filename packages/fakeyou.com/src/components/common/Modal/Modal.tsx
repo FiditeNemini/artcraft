708861a7606c100abc1862e92e1ad666ad81c68f
BFlat <bombayapirat@gmail.com>
2023-10-31 05:09:25 +0700

    disabled button prop

--- packages/fakeyou.com/src/components/common/Modal/Modal.tsx
+++ packages/fakeyou.com/src/components/common/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import Button from "../Button";
 import { faTrashAlt } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
