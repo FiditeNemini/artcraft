2b884ebe5191b3b6892f684319b82b51c31f7e65
Brandon Thomas <echelon@gmail.com>
2023-11-09 03:40:49 -0500

    Move metadata into polymorphic join tables

--- _sql/migrations/2023-11-09-055228_model_weights/up.sql
+++ _sql/migrations/2023-11-09-055228_model_weights/up.sql
@@ -158,59 +158,62 @@ CREATE TABLE model_weights (
   -- is_front_page_featured BOOLEAN NOT NULL DEFAULT FALSE,
 
 
-  -- ========== TTS MODEL METADATA ==========
-
-  -- This is for Tacotron2, specifically.
-  -- The name of the type of pipeline to use, eg "legacy_fakeyou", "legacy_vocodes", "english_v1", etc.
-  -- New pipelines may be added at any point in the future and the source of truth for them will mostly
-  -- live in the `storyteller-ml` repository.
-  -- Older models and brand new models will have this field set to null, and we'll infer which value to
-  -- lazily backfill based on the creation date and language setting.
-  -- tts_text_pipeline_type VARCHAR(64) DEFAULT NULL,
-
-  -- If true, multiply the mel outputs before being vocoded by a globally default constant.
-  tts_use_default_mel_multiply_factor BOOLEAN NOT NULL DEFAULT FALSE,
-
-  -- If not null, multiply the mel outputs before being vocoded by this value.
-  -- This is used instead of the default if `tts_use_default_mel_multiply_factor` is
-  -- set (ie. `tts_use_default_mel_multiply_factor` is ignored and this custom value is used instead)
-  maybe_tts_custom_mel_multiply_factor DOUBLE DEFAULT NULL,
-
-  -- If set, use a different pretrained vocoder.
-  -- If not set, use the website default. (Currently HifiGan-SS)
-  maybe_tts_default_pretrained_vocoder VARCHAR(64) DEFAULT NULL,
-
-  -- If set, we'll use a custom vocoder (from table vocoder_models) instead of a default vocoder.
-  -- If not set, we'll use `maybe_default_pretrained_vocoder` (or the website default).
-  maybe_tts_custom_vocoder_token VARCHAR(32) DEFAULT NULL,
-
-
-  -- ========== VOICE CONVERSION MODEL METADATA ==========
-
-  -- RVC (v2) specific - whether the model has an index file associated with it
-  -- These files improve the quality of the results.
-  vc_has_index_file BOOLEAN NOT NULL DEFAULT FALSE,
-
-
-  -- ========== VOCODER MODEL METADATA ==========
-
-  -- Whether the vocoder is recommended for use
-  -- vocoder_is_staff_recommended BOOLEAN NOT NULL DEFAULT FALSE,
-
-
-  -- ========== VOICE MODEL METADATA (TTS and Voice Conversion Models) ==========
-
-  -- The full IETF BCP47 language tag (eg. en, en-US, es-419, ja-JP, pt, etc.)
-  -- (Not that it matters apart from categorization, since voice conversion is universal.)
-  voice_ietf_language_tag VARCHAR(64) NOT NULL DEFAULT 'en',
-
-  -- The IETF BCP47 language tag's primary language subtag (eg. "es-419" becomes "es")
-  -- (Not that it matters apart from categorization, since voice conversion is universal.)
-  voice_ietf_primary_language_subtag VARCHAR(12) NOT NULL DEFAULT 'en',
-
-  -- RVC (v2) specific - whether the model has an index file associated with it
-  -- These files improve the quality of the results.
-  voice_has_index_file BOOLEAN NOT NULL DEFAULT FALSE,
+-- NB: I think this should actually move into polymorphic join tables, so that we don't create a telescopic list of
+-- columns.
+--
+--  -- ========== TTS MODEL METADATA ==========
+--
+--  -- This is for Tacotron2, specifically.
+--  -- The name of the type of pipeline to use, eg "legacy_fakeyou", "legacy_vocodes", "english_v1", etc.
+--  -- New pipelines may be added at any point in the future and the source of truth for them will mostly
+--  -- live in the `storyteller-ml` repository.
+--  -- Older models and brand new models will have this field set to null, and we'll infer which value to
+--  -- lazily backfill based on the creation date and language setting.
+--  -- tts_text_pipeline_type VARCHAR(64) DEFAULT NULL,
+--
+--  -- If true, multiply the mel outputs before being vocoded by a globally default constant.
+--  tts_use_default_mel_multiply_factor BOOLEAN NOT NULL DEFAULT FALSE,
+--
+--  -- If not null, multiply the mel outputs before being vocoded by this value.
+--  -- This is used instead of the default if `tts_use_default_mel_multiply_factor` is
+--  -- set (ie. `tts_use_default_mel_multiply_factor` is ignored and this custom value is used instead)
+--  maybe_tts_custom_mel_multiply_factor DOUBLE DEFAULT NULL,
+--
+--  -- If set, use a different pretrained vocoder.
+--  -- If not set, use the website default. (Currently HifiGan-SS)
+--  maybe_tts_default_pretrained_vocoder VARCHAR(64) DEFAULT NULL,
+--
+--  -- If set, we'll use a custom vocoder (from table vocoder_models) instead of a default vocoder.
+--  -- If not set, we'll use `maybe_default_pretrained_vocoder` (or the website default).
+--  maybe_tts_custom_vocoder_token VARCHAR(32) DEFAULT NULL,
+--
+--
+--  -- ========== VOICE CONVERSION MODEL METADATA ==========
+--
+--  -- RVC (v2) specific - whether the model has an index file associated with it
+--  -- These files improve the quality of the results.
+--  vc_has_index_file BOOLEAN NOT NULL DEFAULT FALSE,
+--
+--
+--  -- ========== VOCODER MODEL METADATA ==========
+--
+--  -- Whether the vocoder is recommended for use
+--  -- vocoder_is_staff_recommended BOOLEAN NOT NULL DEFAULT FALSE,
+--
+--
+--  -- ========== VOICE MODEL METADATA (TTS and Voice Conversion Models) ==========
+--
+--  -- The full IETF BCP47 language tag (eg. en, en-US, es-419, ja-JP, pt, etc.)
+--  -- (Not that it matters apart from categorization, since voice conversion is universal.)
+--  voice_ietf_language_tag VARCHAR(64) NOT NULL DEFAULT 'en',
+--
+--  -- The IETF BCP47 language tag's primary language subtag (eg. "es-419" becomes "es")
+--  -- (Not that it matters apart from categorization, since voice conversion is universal.)
+--  voice_ietf_primary_language_subtag VARCHAR(12) NOT NULL DEFAULT 'en',
+--
+--  -- RVC (v2) specific - whether the model has an index file associated with it
+--  -- These files improve the quality of the results.
+--  voice_has_index_file BOOLEAN NOT NULL DEFAULT FALSE,
 
 
   -- ========== VECTOR CLOCK ==========
