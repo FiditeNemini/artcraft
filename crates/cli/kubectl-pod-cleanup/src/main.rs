fc344557be22203ddb564ebb4422d64ddb596aea
Brandon Thomas <echelon@gmail.com>
2023-11-09 23:13:08 -0500

    Delete models from AWS EFS host

--- crates/cli/kubectl-pod-cleanup/src/main.rs
+++ crates/cli/kubectl-pod-cleanup/src/main.rs
@@ -1,3 +1,20 @@
+//! kube-pod-cleanup
+//!
+//! Rationale: In production, dead/evicted pods stick around for eternity.
+//! Within a matter of days, thousands of dead pods can clutter the output of `kubectl`,
+//! making it operationally difficult to maintain the cluster.
+//!
+//! This tool quickly dispenses of garbage pods. (Though we may still want to keep them
+//! for debugging actual issues.)
+//!
+//! To install as a system binary, do the following:
+//!
+//!  cargo install --path ./crates/cli/kubectl_pod_cleanup --bin kubectl-pod-cleanup
+//!
+//! (Cargo install seems to currently require the --path argument in a workspace,
+//! as tracked by this issue: https://stackoverflow.com/a/76271890 )
+//!
+
 use std::thread;
 use std::time::Duration;
 
@@ -12,23 +29,6 @@ pub mod delete_pods;
 pub mod delete_pods_threaded;
 pub mod list_pods;
 pub mod pod_store;
-
-/// kube-pod-cleanup
-///
-/// Rationale: In production, dead/evicted pods stick around for eternity.
-/// Within a matter of days, thousands of dead pods can clutter the output of `kubectl`,
-/// making it operationally difficult to maintain the cluster.
-///
-/// This tool quickly dispenses of garbage pods. (Though we may still want to keep them
-/// for debugging actual issues.)
-///
-/// To install as a system binary, do the following:
-///
-///  cargo install --path ./crates/cli/kubectl_pod_cleanup --bin kubectl-pod-cleanup
-///
-/// (Cargo install seems to currently require the --path argument in a workspace,
-/// as tracked by this issue: https://stackoverflow.com/a/76271890 )
-///
 
 pub fn main() -> AnyhowResult<()> {
   easyenv::init_all_with_default_logging(Some(DEFAULT_RUST_LOG));
