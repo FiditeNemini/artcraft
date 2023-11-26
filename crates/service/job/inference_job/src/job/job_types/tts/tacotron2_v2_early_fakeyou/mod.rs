824d113dfcf38f6a5a353be1eb9ac24370439fc4
Brandon Thomas <echelon@gmail.com>
2023-11-26 17:29:17 -0500

    port some dependencies of tts-inference-job to inference-job

--- crates/service/job/inference_job/src/job/job_types/tts/tacotron2_v2_early_fakeyou/mod.rs
+++ crates/service/job/inference_job/src/job/job_types/tts/tacotron2_v2_early_fakeyou/mod.rs
@@ -3,3 +3,5 @@ pub mod read_metadata_file;
 pub mod seconds_to_decoder_steps;
 pub mod tacotron2_dependencies;
 pub mod tacotron2_inference_command;
+pub mod tacotron2_inference_sidecar_client;
+pub mod tacotron2_sidecar_health_check_client;
--- crates/service/job/inference_job/src/job/job_types/tts/tacotron2_v2_early_fakeyou/tacotron2_dependencies.rs
+++ crates/service/job/inference_job/src/job/job_types/tts/tacotron2_v2_early_fakeyou/tacotron2_dependencies.rs
@@ -1,5 +1,8 @@
 use errors::AnyhowResult;
+
 use crate::job::job_types::tts::tacotron2_v2_early_fakeyou::tacotron2_inference_command::Tacotron2InferenceCommand;
+use crate::job::job_types::tts::tacotron2_v2_early_fakeyou::tacotron2_inference_sidecar_client::Tacotron2InferenceSidecarClient;
+use crate::job::job_types::tts::tacotron2_v2_early_fakeyou::tacotron2_sidecar_health_check_client::Tacotron2SidecarHealthCheckClient;
 
 pub struct Tacotron2Dependencies {
   pub inference_command: Tacotron2InferenceCommand,
@@ -12,10 +15,20 @@ pub struct Tacotron2Dependencies {
 
   /// Common pretrained hifigan super resolution vocoder filename
   pub hifigan_superres_vocoder_model_filename: String,
+
+  /// Dependencies that are for the sidecar version of TT2
+  pub sidecar: SidecarDeps,
+}
+
+pub struct SidecarDeps {
+  pub inference_client: Tacotron2InferenceSidecarClient,
+  pub health_check_client: Tacotron2SidecarHealthCheckClient,
 }
 
 impl Tacotron2Dependencies {
   pub fn setup() -> AnyhowResult<Self> {
+
+    // The following are for TT2 that existed in inference-job but that are in an unknown state:
 
     let waveglow_vocoder_model_filename = easyenv::get_env_string_or_default(
       "TTS_WAVEGLOW_VOCODER_MODEL_FILENAME", "waveglow.pth");
@@ -26,11 +39,26 @@ impl Tacotron2Dependencies {
     let hifigan_superres_vocoder_model_filename = easyenv::get_env_string_or_default(
       "TTS_HIFIGAN_SUPERRES_VOCODER_MODEL_FILENAME", "hifigan_superres.pth");
 
+    // The following are for TT2 that was directly ported from tts-inference-job
+
+    let sidecar_hostname =
+        easyenv::get_env_string_required("TTS_INFERENCE_SIDECAR_HOSTNAME")?;
+
+    let inference_client =
+        Tacotron2InferenceSidecarClient::new(&sidecar_hostname);
+
+    let health_check_client=
+        Tacotron2SidecarHealthCheckClient::new(&sidecar_hostname)?;
+
     Ok(Self {
       inference_command: Tacotron2InferenceCommand::from_env()?,
       waveglow_vocoder_model_filename,
       hifigan_vocoder_model_filename,
       hifigan_superres_vocoder_model_filename,
+      sidecar: SidecarDeps {
+        inference_client,
+        health_check_client,
+      }
     })
   }
 }
