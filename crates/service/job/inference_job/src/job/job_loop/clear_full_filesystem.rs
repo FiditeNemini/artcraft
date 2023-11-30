ff4a3115d285f62c586559a6930070e8dd37b441
Brandon Thomas <echelon@gmail.com>
2023-11-30 02:34:33 -0500

    clear up more directories when file system is full

--- crates/service/job/inference_job/src/job/job_loop/clear_full_filesystem.rs
+++ crates/service/job/inference_job/src/job/job_loop/clear_full_filesystem.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use log::warn;
 
 use collections::multiple_random_from_vec::multiple_random_from_vec;
@@ -5,12 +7,18 @@ use errors::AnyhowResult;
 use jobs_common::semi_persistent_cache_dir::SemiPersistentCacheDir;
 
 pub fn clear_full_filesystem(cache_dir: &SemiPersistentCacheDir) -> AnyhowResult<()> {
-  warn!("Deleting cached VC models to free up disk space.");
+  warn!("Deleting cached models to free up disk space.");
 
-  let model_dir = cache_dir.voice_conversion_model_directory();
+  decimate_directory(cache_dir.custom_vocoder_model_directory())?;
+  decimate_directory(cache_dir.tts_synthesizer_model_directory())?;
+  decimate_directory(cache_dir.voice_conversion_model_directory())?;
 
+  Ok(())
+}
+
+fn decimate_directory(path: &Path) -> AnyhowResult<()> {
   // TODO: When this is no longer sufficient, delete other types of locally-cached data.
-  let paths = std::fs::read_dir(model_dir)?
+  let paths = std::fs::read_dir(path)?
       .map(|res| res.map(|e| e.path()))
       .collect::<Result<Vec<_>, std::io::Error>>()?;
 
@@ -21,7 +29,7 @@ pub fn clear_full_filesystem(cache_dir: &SemiPersistentCacheDir) -> AnyhowResult
   for model_to_delete in models_to_delete {
     warn!("Deleting cached model file: {:?}", model_to_delete);
 
-    let full_model_path = cache_dir.voice_conversion_model_path(model_to_delete);
+    let full_model_path = path.join(model_to_delete);
     std::fs::remove_file(full_model_path)?;
   }
 
