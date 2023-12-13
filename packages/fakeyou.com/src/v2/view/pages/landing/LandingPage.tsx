83f6ecd4c7dd7306ccaa7cb481bad5e558f88442
BFlat <bombayapirat@gmail.com>
2023-12-14 01:33:25 +0700

    beta badge for voice designer

--- packages/fakeyou.com/src/v2/view/pages/landing/LandingPage.tsx
+++ packages/fakeyou.com/src/v2/view/pages/landing/LandingPage.tsx
@@ -12,7 +12,11 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { WebUrl } from "../../../../common/WebUrl";
-import { faFileArrowUp, faSparkles } from "@fortawesome/pro-solid-svg-icons";
+import {
+  faFileArrowUp,
+  faFlask,
+  faSparkles,
+} from "@fortawesome/pro-solid-svg-icons";
 import { PosthogClient } from "@storyteller/components/src/analytics/PosthogClient";
 import { FooterNav } from "v2/view/nav/FooterNav";
 import Alert from "components/common/Alert/Alert";
@@ -311,8 +315,8 @@ function LandingPage(props: Props) {
                 <div className="flex-grow-1">
                   <div className="mb-1">
                     <span className="badge-new d-inline-flex align-items-center mb-2 me-2">
-                      <FontAwesomeIcon icon={faSparkles} className="me-1" />
-                      {t("productNewTag")}
+                      <FontAwesomeIcon icon={faFlask} className="me-1" />
+                      BETA
                     </span>
 
                     <h4 className="fw-bold text-white d-inline-flex align-items-center mb-0">
