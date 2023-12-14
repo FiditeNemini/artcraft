e9cd505e0bbe5a2c6a547e0fc75d84f3ec026f5c
Brandon Thomas <echelon@gmail.com>
2023-12-14 18:11:34 -0500

    Fix clap in various CLI tools post 2.x -> 4.x upgrade

--- crates/cli/dev_database_seed/src/cli_args.rs
+++ crates/cli/dev_database_seed/src/cli_args.rs
@@ -1,28 +1,17 @@
-use clap::{App, Arg};
+use clap::Parser;
 
 use errors::AnyhowResult;
 
+#[derive(Parser, Debug)]
+#[command(name="dev-database-seed")]
 pub struct CliArgs {
+  #[arg(name="seed_cloud_bucket", long="bucket", help="Seed the cloud bucket with files", required=false, default_value_t=false)]
   pub seed_cloud_bucket: bool,
+
+  #[arg(name="seed_elasticsearch", long="elasticsearch", help="Seed the local elasticsearch", required=false, default_value_t=false)]
   pub seed_elasticsearch: bool,
 }
 
 pub fn parse_cli_args() -> AnyhowResult<CliArgs> {
-  let matches = App::new("dev-database-seed")
-      .arg(Arg::with_name("seed_cloud_bucket")
-          .long("bucket")
-          .help("Seed the cloud bucket with files")
-          .takes_value(false)
-          .required(false))
-      .arg(Arg::with_name("seed_elasticsearch")
-        .long("elasticsearch")
-        .help("Seed the local elasticsearch")
-        .takes_value(false)
-        .required(false))
-      .get_matches();
-
-  Ok(CliArgs {
-    seed_cloud_bucket: matches.is_present("seed_cloud_bucket"),
-    seed_elasticsearch: matches.is_present("seed_elasticsearch"),
-  })
+  Ok(CliArgs::parse())
 }
