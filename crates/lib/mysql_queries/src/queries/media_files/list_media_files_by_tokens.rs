5a4d2a8c065e208f994749ddf421ff715e347b72
Brandon Thomas <echelon@gmail.com>
2023-12-14 15:15:31 -0500

    media files query by tokens (WHERE IN clause)

--- crates/lib/mysql_queries/src/queries/media_files/list_media_files_by_tokens.rs
+++ crates/lib/mysql_queries/src/queries/media_files/list_media_files_by_tokens.rs
@@ -2,142 +2,131 @@ use chrono::{DateTime, Utc};
 use sqlx::{Acquire, FromRow, MySql, MySqlConnection, MySqlPool, QueryBuilder, Row};
 use sqlx::mysql::MySqlRow;
 
-use enums::by_table::model_weights::{
-  weights_category::WeightsCategory,
-  weights_types::WeightsType,
-};
+use enums::by_table::media_files::media_file_origin_category::MediaFileOriginCategory;
+use enums::by_table::media_files::media_file_origin_model_type::MediaFileOriginModelType;
+use enums::by_table::media_files::media_file_origin_product_category::MediaFileOriginProductCategory;
+use enums::by_table::media_files::media_file_type::MediaFileType;
 use enums::traits::mysql_from_row::MySqlFromRow;
 use errors::AnyhowResult;
-use tokens::tokens::model_weights::ModelWeightToken;
+use tokens::tokens::media_files::MediaFileToken;
 use tokens::tokens::users::UserToken;
 
 #[derive(Serialize)]
-pub struct WeightsByTokensRecord {
-  pub token: ModelWeightToken,
+pub struct MediaFilesByTokensRecord {
+  pub token: MediaFileToken,
 
-  pub weights_type: WeightsType,
-  pub weights_category: WeightsCategory,
+  pub origin_category: MediaFileOriginCategory,
+  pub origin_product_category: MediaFileOriginProductCategory,
+  pub maybe_origin_model_type: Option<MediaFileOriginModelType>,
+  pub maybe_origin_model_token: Option<String>,
 
-  pub title: String,
+  pub media_type: MediaFileType,
+  pub public_bucket_directory_hash: String,
+  pub maybe_public_bucket_prefix: Option<String>,
+  pub maybe_public_bucket_extension: Option<String>,
 
-  pub maybe_thumbnail_token: Option<String>,
-
-  pub creator_user_token: UserToken,
-  pub creator_username: String,
-  pub creator_display_name: String,
-  pub creator_email_gravatar_hash: String,
-
-  pub private_bucket_hash: String,
-  pub maybe_private_bucket_prefix: Option<String>,
-  pub maybe_private_bucket_extension: Option<String>,
-
-  pub cached_user_ratings_total_count: u32,
-  pub cached_user_ratings_positive_count: u32,
-  pub cached_user_ratings_negative_count: u32,
-  pub maybe_cached_user_ratings_ratio: Option<f32>,
-  pub cached_user_ratings_last_updated_at: DateTime<Utc>,
+  pub maybe_creator_user_token: Option<UserToken>,
+  pub maybe_creator_username: Option<String>,
+  pub maybe_creator_display_name: Option<String>,
+  pub maybe_creator_email_gravatar_hash: Option<String>,
 
   pub created_at: DateTime<Utc>,
   pub updated_at: DateTime<Utc>,
-  pub user_deleted_at: Option<DateTime<Utc>>,
-  pub mod_deleted_at: Option<DateTime<Utc>>,
 }
 
-pub async fn list_weights_by_tokens(
+pub async fn list_media_files_by_tokens(
   mysql_pool: &MySqlPool,
-  weight_tokens: &[ModelWeightToken],
+  media_file_tokens: &[MediaFileToken],
   can_see_deleted: bool
-) -> AnyhowResult<Vec<WeightsByTokensRecord>> {
+) -> AnyhowResult<Vec<MediaFilesByTokensRecord>> {
 
   let mut connection = mysql_pool.acquire().await?;
 
-  let raw_weights: Vec<RawWeightJoinUser> = get_raw_weights_by_tokens(&mut connection, weight_tokens, can_see_deleted).await?;
+  let raw_media_files: Vec<RawMediaFileJoinUser> = get_raw_media_files_by_tokens(&mut connection, media_file_tokens, can_see_deleted).await?;
 
-  Ok(map_to_weights(raw_weights))
+  Ok(map_to_media_files(raw_media_files))
 }
 
-async fn get_raw_weights_by_tokens(
+async fn get_raw_media_files_by_tokens(
   connection: &mut MySqlConnection,
-  weight_tokens: &[ModelWeightToken],
+  media_file_tokens: &[MediaFileToken],
   can_see_deleted: bool
-) -> AnyhowResult<Vec<RawWeightJoinUser>> {
+) -> AnyhowResult<Vec<RawMediaFileJoinUser>> {
 
   let connection = connection.acquire().await?;
 
   let mut query_builder : QueryBuilder<MySql> = if can_see_deleted {
     QueryBuilder::new(r#"
       SELECT
-          mw.token,
-          mw.title,
-          mw.weights_type,
-          mw.weights_category,
-          mw.maybe_thumbnail_token,
-          users.token as creator_user_token,
-          users.username as creator_username,
-          users.display_name as creator_display_name,
-          users.email_gravatar_hash as creator_email_gravatar_hash,
-          mw.private_bucket_hash,
-          mw.maybe_private_bucket_prefix,
-          mw.maybe_private_bucket_extension,
-          mw.cached_user_ratings_negative_count,
-          mw.cached_user_ratings_positive_count,
-          mw.cached_user_ratings_total_count,
-          mw.maybe_cached_user_ratings_ratio,
-          mw.cached_user_ratings_last_updated_at,
-          mw.created_at,
-          mw.updated_at,
-          mw.user_deleted_at,
-          mw.mod_deleted_at
-      FROM model_weights as mw
-      JOIN users
-          ON users.token = mw.creator_user_token
+          m.token,
+
+          m.origin_category,
+          m.origin_product_category,
+          m.maybe_origin_model_type,
+          m.maybe_origin_model_token,
+
+          users.token as maybe_creator_user_token,
+          users.username as maybe_creator_username,
+          users.display_name as maybe_creator_display_name,
+          users.email_gravatar_hash as maybe_creator_email_gravatar_hash,
+
+          m.media_type,
+          m.public_bucket_directory_hash,
+          m.maybe_public_bucket_prefix,
+          m.maybe_public_bucket_extension,
+
+          m.created_at,
+          m.updated_at
+
+      FROM media_files as m
+      LEFT OUTER JOIN users
+          ON users.token = m.maybe_creator_user_token
       WHERE
-          mw.creator_set_visibility = "public"
-          AND mw.token IN (
+          m.creator_set_visibility = "public"
+          AND m.token IN (
       "#,
     )
 
   } else {
     QueryBuilder::new(r#"
       SELECT
-          mw.token,
-          mw.title,
-          mw.weights_type,
-          mw.weights_category,
-          mw.maybe_thumbnail_token,
-          users.token as creator_user_token,
-          users.username as creator_username,
-          users.display_name as creator_display_name,
-          users.email_gravatar_hash as creator_email_gravatar_hash,
-          mw.private_bucket_hash,
-          mw.maybe_private_bucket_prefix,
-          mw.maybe_private_bucket_extension,
-          mw.cached_user_ratings_negative_count,
-          mw.cached_user_ratings_positive_count,
-          mw.cached_user_ratings_total_count,
-          mw.maybe_cached_user_ratings_ratio,
-          mw.cached_user_ratings_last_updated_at,
-          mw.created_at,
-          mw.updated_at,
-          mw.user_deleted_at,
-          mw.mod_deleted_at
-      FROM model_weights as mw
-      JOIN users
-          ON users.token = mw.creator_user_token
+          m.token,
+
+          m.origin_category,
+          m.origin_product_category,
+          m.maybe_origin_model_type,
+          m.maybe_origin_model_token,
+
+          users.token as maybe_creator_user_token,
+          users.username as maybe_creator_username,
+          users.display_name as maybe_creator_display_name,
+          users.email_gravatar_hash as maybe_creator_email_gravatar_hash,
+
+          m.media_type,
+          m.public_bucket_directory_hash,
+          m.maybe_public_bucket_prefix,
+          m.maybe_public_bucket_extension,
+
+          m.created_at,
+          m.updated_at
+
+      FROM media_files as m
+      LEFT OUTER JOIN users
+          ON users.token = m.maybe_creator_user_token
       WHERE
-          mw.creator_set_visibility = "public"
-          AND mw.user_deleted_at IS NULL
-          AND mw.mod_deleted_at IS NULL
-          AND mw.token IN (
+          m.creator_set_visibility = "public"
+          AND m.user_deleted_at IS NULL
+          AND m.mod_deleted_at IS NULL
+          AND m.token IN (
       "#
     )
   };
 
-  query_builder.push(token_predicate(weight_tokens));
+  query_builder.push(token_predicate(media_file_tokens));
 
   query_builder.push(")");
 
-  let query = query_builder.build_query_as::<RawWeightJoinUser>();
+  let query = query_builder.build_query_as::<RawMediaFileJoinUser>();
 
   let results = query.fetch_all(connection).await?;
 
@@ -146,7 +135,7 @@ async fn get_raw_weights_by_tokens(
 
 /// Return a comma-separated predicate, since SQLx does not yet support WHERE IN(?) for Vec<T>, etc.
 /// Issue: https://github.com/launchbadge/sqlx/issues/875
-fn token_predicate(tokens: &[ModelWeightToken]) -> String {
+fn token_predicate(tokens: &[MediaFileToken]) -> String {
   tokens.iter()
       .map(|ty| ty.as_str())
       .map(|ty| format!("\"{}\"", ty))
@@ -154,73 +143,57 @@ fn token_predicate(tokens: &[ModelWeightToken]) -> String {
       .join(", ")
 }
 
-fn map_to_weights(dataset:Vec<RawWeightJoinUser>) -> Vec<WeightsByTokensRecord> {
-  let weights: Vec<WeightsByTokensRecord> = dataset
+fn map_to_media_files(dataset:Vec<RawMediaFileJoinUser>) -> Vec<MediaFilesByTokensRecord> {
+  let media_files: Vec<MediaFilesByTokensRecord> = dataset
       .into_iter()
-      .map(|dataset: RawWeightJoinUser| {
-        WeightsByTokensRecord {
-          token: dataset.token,
-          title: dataset.title,
-          weights_type: dataset.weights_type,
-          weights_category: dataset.weights_category,
-          maybe_thumbnail_token: dataset.maybe_thumbnail_token,
+      .map(|media_file: RawMediaFileJoinUser| {
+        MediaFilesByTokensRecord {
+          token: media_file.token,
 
-          creator_user_token: dataset.creator_user_token,
-          creator_username:dataset.creator_username,
-          creator_display_name:dataset.creator_display_name,
-          creator_email_gravatar_hash:dataset.creator_email_gravatar_hash,
+          origin_category: media_file.origin_category,
+          origin_product_category: media_file.origin_product_category,
+          maybe_origin_model_type: media_file.maybe_origin_model_type,
+          maybe_origin_model_token: media_file.maybe_origin_model_token,
 
-          private_bucket_hash: dataset.private_bucket_hash,
-          maybe_private_bucket_prefix: dataset.maybe_private_bucket_prefix,
-          maybe_private_bucket_extension: dataset.maybe_private_bucket_extension,
+          maybe_creator_user_token: media_file.maybe_creator_user_token,
+          maybe_creator_username: media_file.maybe_creator_username,
+          maybe_creator_display_name: media_file.maybe_creator_display_name,
+          maybe_creator_email_gravatar_hash: media_file.maybe_creator_email_gravatar_hash,
 
-          cached_user_ratings_negative_count: dataset.cached_user_ratings_negative_count,
-          cached_user_ratings_positive_count: dataset.cached_user_ratings_positive_count,
-          cached_user_ratings_total_count: dataset.cached_user_ratings_total_count,
+          media_type: media_file.media_type,
+          public_bucket_directory_hash: media_file.public_bucket_directory_hash,
+          maybe_public_bucket_prefix: media_file.maybe_public_bucket_prefix,
+          maybe_public_bucket_extension: media_file.maybe_public_bucket_extension,
 
-          maybe_cached_user_ratings_ratio: dataset.maybe_cached_user_ratings_ratio,
-          cached_user_ratings_last_updated_at: dataset.cached_user_ratings_last_updated_at,
-
-          created_at: dataset.created_at,
-          updated_at: dataset.updated_at,
-          user_deleted_at: dataset.user_deleted_at,
-          mod_deleted_at: dataset.mod_deleted_at,
+          created_at: media_file.created_at,
+          updated_at: media_file.updated_at,
         }
       }).collect();
 
-  weights
+  media_files
 }
 
   #[derive(Serialize)]
-  pub struct RawWeightJoinUser {
-    pub token: ModelWeightToken,
+  pub struct RawMediaFileJoinUser {
+    pub token: MediaFileToken,
 
-    pub weights_type: WeightsType,
-    pub weights_category: WeightsCategory,
-    
-    pub title: String,
-    
-    pub maybe_thumbnail_token: Option<String>,
+    pub origin_category: MediaFileOriginCategory,
+    pub origin_product_category: MediaFileOriginProductCategory,
+    pub maybe_origin_model_type: Option<MediaFileOriginModelType>,
+    pub maybe_origin_model_token: Option<String>,
 
-    pub creator_user_token: UserToken,
-    pub creator_username: String,
-    pub creator_display_name: String,
-    pub creator_email_gravatar_hash: String,
+    pub maybe_creator_user_token: Option<UserToken>,
+    pub maybe_creator_username: Option<String>,
+    pub maybe_creator_display_name: Option<String>,
+    pub maybe_creator_email_gravatar_hash: Option<String>,
 
-    pub private_bucket_hash: String,
-    pub maybe_private_bucket_prefix: Option<String>,
-    pub maybe_private_bucket_extension: Option<String>,
-    
-    pub cached_user_ratings_total_count: u32,
-    pub cached_user_ratings_positive_count: u32,
-    pub cached_user_ratings_negative_count: u32,
-    pub maybe_cached_user_ratings_ratio: Option<f32>,
-    pub cached_user_ratings_last_updated_at: DateTime<Utc>,
-    
+    pub media_type: MediaFileType,
+    pub public_bucket_directory_hash: String,
+    pub maybe_public_bucket_prefix: Option<String>,
+    pub maybe_public_bucket_extension: Option<String>,
+
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
-    pub user_deleted_at: Option<DateTime<Utc>>,
-    pub mod_deleted_at: Option<DateTime<Utc>>,
 }
 
 // NB(bt,2023-12-05): There's an issue with type hinting in the `as` clauses with QueryBuilder (or
@@ -235,30 +208,25 @@ fn map_to_weights(dataset:Vec<RawWeightJoinUser>) -> Vec<WeightsByTokensRecord> 
 // This results in the automatic mapping not being able to be found by name (for macro derive), and
 // in the manual case `row.try_get()` etc. won't have the correct column name (since the name is the
 // full "as" clause).
-impl FromRow<'_, MySqlRow> for RawWeightJoinUser {
+impl FromRow<'_, MySqlRow> for RawMediaFileJoinUser {
   fn from_row(row: &MySqlRow) -> Result<Self, sqlx::Error> {
     Ok(Self {
-      token: ModelWeightToken::new(row.try_get("token")?),
-      weights_type: WeightsType::try_from_mysql_row(row, "weights_type")?,
-      weights_category: WeightsCategory::try_from_mysql_row(row, "weights_category")?,
-      title: row.try_get("title")?,
-      maybe_thumbnail_token: row.try_get("maybe_thumbnail_token")?,
-      creator_user_token: UserToken::new_from_str(row.try_get("creator_user_token")?),
-      creator_username: row.try_get("creator_username")?,
-      creator_display_name: row.try_get("creator_display_name")?,
-      creator_email_gravatar_hash: row.try_get("creator_email_gravatar_hash")?,
-      private_bucket_hash: row.try_get("private_bucket_hash")?,
-      maybe_private_bucket_prefix: row.try_get("maybe_private_bucket_prefix")?,
-      maybe_private_bucket_extension: row.try_get("maybe_private_bucket_extension")?,
-      cached_user_ratings_total_count: row.try_get("cached_user_ratings_total_count")?,
-      cached_user_ratings_positive_count: row.try_get("cached_user_ratings_positive_count")?,
-      cached_user_ratings_negative_count: row.try_get("cached_user_ratings_negative_count")?,
-      maybe_cached_user_ratings_ratio: row.try_get("maybe_cached_user_ratings_ratio")?,
-      cached_user_ratings_last_updated_at: row.try_get("cached_user_ratings_last_updated_at")?,
+      token: MediaFileToken::new(row.try_get("token")?),
+      origin_category: MediaFileOriginCategory::try_from_mysql_row(row, "origin_category")?,
+      origin_product_category: MediaFileOriginProductCategory::try_from_mysql_row(row, "origin_product_category")?,
+      maybe_origin_model_type: MediaFileOriginModelType::try_from_mysql_row_nullable(row, "maybe_origin_model_type")?,
+      maybe_origin_model_token: row.try_get("maybe_origin_model_token")?,
+      maybe_creator_user_token: row.try_get::<Option<String>, _>("maybe_creator_user_token")?
+          .and_then(|token| Some(UserToken::new_from_str(&token))),
+      maybe_creator_username: row.try_get("maybe_creator_username")?,
+      maybe_creator_display_name: row.try_get("maybe_creator_display_name")?,
+      maybe_creator_email_gravatar_hash: row.try_get("maybe_creator_email_gravatar_hash")?,
+      media_type: MediaFileType::try_from_mysql_row(row, "media_type")?,
+      public_bucket_directory_hash: row.try_get("public_bucket_directory_hash")?,
+      maybe_public_bucket_prefix: row.try_get("maybe_public_bucket_prefix")?,
+      maybe_public_bucket_extension: row.try_get("maybe_public_bucket_extension")?,
       created_at: row.try_get("created_at")?,
       updated_at: row.try_get("updated_at")?,
-      user_deleted_at: row.try_get("user_deleted_at")?,
-      mod_deleted_at: row.try_get("mod_deleted_at")?,
     })
   }
 }
