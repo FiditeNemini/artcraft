271a80498a7e82d82bff4d9dcee93e92708f5f32
BFlat <bombayapirat@gmail.com>
2023-12-19 11:24:17 +0700

    video card update

--- packages/fakeyou.com/src/components/common/Card/VideoCard/VideoCard.tsx
+++ packages/fakeyou.com/src/components/common/Card/VideoCard/VideoCard.tsx
@@ -3,16 +3,19 @@ import { useHistory } from "react-router-dom";
 import Card from "../Card";
 import useTimeAgo from "hooks/useTimeAgo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlayCircle } from "@fortawesome/pro-solid-svg-icons";
+import { faArrowRight, faPlayCircle } from "@fortawesome/pro-solid-svg-icons";
 import Badge from "components/common/Badge";
 import LikeButton from "components/common/LikeButton";
+import Button from "components/common/Button";
+import CreatorName from "../CreatorName";
 
 interface VideoCardProps {
   data: any;
   type: "media" | "weights";
+  showCreator?: boolean;
 }
 
-export default function VideoCard({ data, type }: VideoCardProps) {
+export default function VideoCard({ data, type, showCreator }: VideoCardProps) {
   const history = useHistory();
 
   const handleCardClick = () => {
@@ -53,12 +56,29 @@ export default function VideoCard({ data, type }: VideoCardProps) {
                   {data.weight_name}
                 </h6>
                 <p className="fs-7 opacity-75">{timeAgo}</p>
-                <div className="mt-2" onClick={handleInnerClick}>
-                  <LikeButton
-                    onToggle={handleLike}
-                    likeCount={data.likes}
-                    overlay={true}
-                  />
+                <hr className="my-2" />
+                <div
+                  className="d-flex align-items-center gap-2"
+                  onClick={handleInnerClick}
+                >
+                  {showCreator && (
+                    <div className="flex-grow-1">
+                      <CreatorName
+                        displayName={data.maybe_creator_user.display_name}
+                        gravatarHash={data.maybe_creator_user.gravatar_hash}
+                        avatarIndex={
+                          data.maybe_creator_user.default_avatar.image_index
+                        }
+                        backgroundIndex={
+                          data.maybe_creator_user.default_avatar.color_index
+                        }
+                      />
+                    </div>
+                  )}
+
+                  <div>
+                    <LikeButton onToggle={handleLike} likeCount={data.likes} />
+                  </div>
                 </div>
               </div>
             </div>
@@ -79,6 +99,14 @@ export default function VideoCard({ data, type }: VideoCardProps) {
             <div className="d-flex align-items-center">
               <div className="d-flex flex-grow-1">
                 <Badge label="Video" color="purple" overlay={true} />
+                <Button
+                  icon={faArrowRight}
+                  iconFlip={true}
+                  variant="link"
+                  label="Use"
+                  onClick={handleCardClick}
+                  className="fs-7"
+                />
               </div>
             </div>
             <FontAwesomeIcon icon={faPlayCircle} className="card-video-play" />
