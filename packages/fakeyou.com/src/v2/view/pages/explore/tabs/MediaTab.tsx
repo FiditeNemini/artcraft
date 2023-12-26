75113c292547876901c84cfbc1e0a8db34e19217
BFlat <bombayapirat@gmail.com>
2023-12-26 21:58:43 +0700

    explore media sort fix

--- packages/fakeyou.com/src/v2/view/pages/explore/tabs/MediaTab.tsx
+++ packages/fakeyou.com/src/v2/view/pages/explore/tabs/MediaTab.tsx
@@ -51,9 +51,9 @@ export default function MediaTab() {
   ];
 
   const sortOptions = [
-    { value: "newest", label: "Newest" },
-    { value: "oldest", label: "Oldest" },
-    { value: "mostliked", label: "Most Liked" },
+    { value: false, label: "Newest" },
+    { value: true, label: "Oldest" },
+    // { value: "mostliked", label: "Most Liked" },
   ];
 
   return (
