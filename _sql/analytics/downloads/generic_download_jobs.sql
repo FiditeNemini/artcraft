d11c236257b0682aa4b4409ff2848b94d0c9cd86
Brandon Thomas <echelon@gmail.com>
2024-01-11 16:25:38 -0500

    analyze download jobs

--- _sql/analytics/downloads/generic_download_jobs.sql
+++ _sql/analytics/downloads/generic_download_jobs.sql
@@ -2,9 +2,21 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
 
+-- Summary stats on downloads
 select
     distinct download_type,
              count(*) as created
 from generic_download_jobs
 where created_at > (CURDATE() - INTERVAL 60 DAY)
 group by download_type;
+
+-- Find recent failed RVC downloads
+select
+    token,
+    title,
+    download_url,
+    status
+from generic_download_jobs
+where download_type = 'rvc_v2'
+and status IN ('started', 'attempt_failed')
+and created_at > (CURDATE() - INTERVAL 2 DAY);
