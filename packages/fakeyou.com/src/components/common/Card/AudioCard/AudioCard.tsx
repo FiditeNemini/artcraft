01b00ea3cf8840707b9828e2b9a76c9d6c0e0dbd
Brandon Thomas <echelon@gmail.com>
2024-01-11 15:49:23 -0500

    fixes

--- packages/fakeyou.com/src/components/common/Card/AudioCard/AudioCard.tsx
+++ packages/fakeyou.com/src/components/common/Card/AudioCard/AudioCard.tsx
@@ -125,8 +125,6 @@ export default function AudioCard({
 
               <div>
 
-                />
-
                 <LikeButton {...{
                   likeCount: data.likes,
                   ...ratings.makeProps({
