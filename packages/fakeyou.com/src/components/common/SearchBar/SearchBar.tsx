fa7419f908fef0a85fc7c57d09a737521af492f3
BFlat <bombayapirat@gmail.com>
2024-01-11 17:30:37 +0700

    weights search

--- packages/fakeyou.com/src/components/common/SearchBar/SearchBar.tsx
+++ packages/fakeyou.com/src/components/common/SearchBar/SearchBar.tsx
@@ -1,10 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Button from "../Button";
 import { faSearch } from "@fortawesome/pro-solid-svg-icons";
-import {
-  SearchTtsModels,
-  TtsModel,
-} from "@storyteller/components/src/api/tts/SearchTtsModels";
+import { SearchWeights } from "@storyteller/components/src/api/weights/SearchWeights";
+import { Weight } from "@storyteller/components/src/api/weights/GetWeight";
 import SearchResultsDropdown from "./SearchResultsDropdown";
 import SearchField from "./SearchField";
 import "./SearchBar.scss";
@@ -28,7 +26,7 @@ export default function SearchBar({
   let location = useLocation();
 
   const { searchTerm, setSearchTerm } = useSearch();
-  const [foundTtsModels, setFoundTtsModels] = useState<TtsModel[]>([]);
+  const [foundTtsModels, setFoundTtsModels] = useState<Weight[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const isOnSearchPage = location.pathname.startsWith("/search");
 
@@ -47,7 +45,7 @@ export default function SearchBar({
 
       setIsLoading(true);
 
-      let response = await SearchTtsModels(request);
+      let response = await SearchWeights(request);
 
       if (response.success) {
         let models = [...response.models];
