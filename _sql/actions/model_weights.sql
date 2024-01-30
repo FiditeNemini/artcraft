7248425f3062f20af9dbafaf647637cef7a40862
Brandon Thomas <echelon@gmail.com>
2024-01-30 17:37:44 -0500

    takedown requests

---
+++ _sql/actions/model_weights.sql
@@ -0,0 +1,12 @@
+-- noinspection SqlDialectInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+update model_weights
+set
+    mod_deleted_at = NOW()
+where token IN (
+    'weight_tnhacmxb9qwqrygc3pxvk3ds6',
+    'weight_9g1pghkc0xhfy5n9m36b11vds'
+);
+
