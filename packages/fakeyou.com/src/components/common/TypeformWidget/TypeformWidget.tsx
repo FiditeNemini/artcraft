9a9582485d822bb608a0cc3d4573755003958e00
grungla <mhmichaelhuynh@gmail.com>
2024-01-30 00:14:06 -0500

    Add Typeform

---
+++ packages/fakeyou.com/src/components/common/TypeformWidget/TypeformWidget.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Widget } from "@typeform/embed-react";
+import ls, { get, set } from "local-storage";
+
+export default function TypeformWidget() {
+  return (
+    <div>
+      {!get<boolean>("isSubmit") ? (
+        <Widget
+          id="UfvpJUpF?utm_source=xxxxx"
+          height={400}
+          opacity={80}
+          onSubmit={() => {
+            set<boolean>("isSubmit", true);
+            console.log("Form submitted!");
+            // window.location.reload();
+          }}
+          hideHeaders
+          // disableAutoFocus
+          enableSandbox
+        />
+      ) : null}
+    </div>
+  );
+}
