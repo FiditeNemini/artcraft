5c181750823bfafed110a722ce0f9827ab76bbb6
BFlat <bombayapirat@gmail.com>
2024-01-31 12:44:21 +0700

    text-align responsive fix

--- packages/fakeyou.com/src/components/layout/PageHeaderWithImage/PageHeaderWithImage.tsx
+++ packages/fakeyou.com/src/components/layout/PageHeaderWithImage/PageHeaderWithImage.tsx
@@ -21,7 +21,7 @@ function PageHeaderWithImage(props: Props) {
       <div className="row gx-3">
         <div className="col-12 col-md-7 py-3 py-lg-4">
           <div className="py-3">
-            <h1 className="fw-bold text-center text-md-start d-flex align-items-center">
+            <h1 className="fw-bold text-center text-md-start d-flex justify-content-center justify-content-md-start align-items-center">
               {props.titleIcon && (
                 <FontAwesomeIcon icon={props.titleIcon} className="me-3 fs-2" />
               )}
