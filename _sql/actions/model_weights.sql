387cb22e65d39e97a3025b45fe89a47230155594
Brandon Thomas <echelon@gmail.com>
2024-01-31 02:37:07 -0500

    more takedown requests

--- _sql/actions/model_weights.sql
+++ _sql/actions/model_weights.sql
@@ -7,6 +7,7 @@ set
     mod_deleted_at = NOW()
 where token IN (
     'weight_tnhacmxb9qwqrygc3pxvk3ds6',
-    'weight_9g1pghkc0xhfy5n9m36b11vds'
+    'weight_9g1pghkc0xhfy5n9m36b11vds',
+    'weight_5k0kzkftr4x39xyc9yg5p9c86'
 );
 
