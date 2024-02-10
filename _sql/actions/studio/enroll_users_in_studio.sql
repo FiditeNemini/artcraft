e0f141fa74ff607b715b5c9453559f5610bc64d6
Brandon Thomas <echelon@gmail.com>
2024-02-10 16:14:45 -0500

    enable michael's account

--- _sql/actions/studio/enroll_users_in_studio.sql
+++ _sql/actions/studio/enroll_users_in_studio.sql
@@ -6,6 +6,7 @@
 -- SELECT token, username FROM users WHERE username IN (
 UPDATE users SET can_access_studio = true WHERE username IN (
     'bflat',
+    'crossproduct1',
     'echelon',
     'fyscott',
     'olivicmic',
