17b38a62cf4b66dbb68c04765e0211b86d33cdae
Brandon Thomas <echelon@gmail.com>
2024-02-13 19:36:38 -0500

    add more staff accounts

--- _sql/actions/studio/enroll_users_in_studio.sql
+++ _sql/actions/studio/enroll_users_in_studio.sql
@@ -3,14 +3,19 @@
 -- noinspection SqlResolveForFile
 
 -- Staff
+-- teddanson is victor's alt
 -- SELECT token, username FROM users WHERE username IN (
 UPDATE users SET can_access_studio = true WHERE username IN (
     'bflat',
+    'brandon',
+    'crossproduct',
     'crossproduct1',
     'echelon',
     'fyscott',
     'olivicmic',
     'saltacc',
+    'teddanson',
+    'vegito1089',
     'wilwong',
     'yae_ph',
     'zzz_last_item'
