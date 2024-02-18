07444c0087795b7e512d17cf55b5f28e5440cc36
BFlat <bombayapirat@gmail.com>
2024-02-18 16:02:18 +0700

    batch media update

--- packages/fakeyou.com/src/v2/view/pages/media/components/SdBatchMediaPanel/SdBatchMediaPanel.tsx
+++ packages/fakeyou.com/src/v2/view/pages/media/components/SdBatchMediaPanel/SdBatchMediaPanel.tsx
@@ -10,38 +10,19 @@ import { Swiper as SwiperType } from "swiper/types";
 import "./SdBatchMediaPanel.scss";
 
 interface SdCoverImagePanelProps {
-  src?: string;
-  alt?: string;
+  images: string[];
 }
 
-export default function SdBatchMediaPanel({
-  src,
-  alt,
-}: SdCoverImagePanelProps) {
+export default function SdBatchMediaPanel({ images }: SdCoverImagePanelProps) {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
 
   const handleSwiper = (swiper: SwiperType) => {
     setThumbsSwiper(swiper);
   };
 
-  const images = [
-    "https://swiperjs.com/demos/images/nature-1.jpg",
-    "https://swiperjs.com/demos/images/nature-2.jpg",
-    "https://swiperjs.com/demos/images/nature-3.jpg",
-    "https://swiperjs.com/demos/images/nature-4.jpg",
-    "https://swiperjs.com/demos/images/nature-5.jpg",
-    "https://swiperjs.com/demos/images/nature-6.jpg",
-    "https://swiperjs.com/demos/images/nature-7.jpg",
-    "https://swiperjs.com/demos/images/nature-8.jpg",
-  ];
-
   return (
     <Panel padding={true}>
       <Swiper
-        // style={{
-        //   "--swiper-navigation-color": "#fff",
-        //   "--swiper-pagination-color": "#fff",
-        // }}
         loop={true}
         spaceBetween={10}
         navigation={true}
