336996e62e0b5fd265da49114dddf520fd833c2c
Victor <victor@storyteller.ai>
2024-02-19 17:14:49 -0800

    multi-select styling fixes

--- packages/fakeyou.com/src/v2/view/pages/dev_upload/DevMediaInput.tsx
+++ packages/fakeyou.com/src/v2/view/pages/dev_upload/DevMediaInput.tsx
@@ -4,6 +4,7 @@ import { EntityFilterOptions, EntityInputMode, EntityModeProp, ListEntityFilters
 import { useModal, useSession } from "hooks";
 import { InferenceJobsModal } from "components/modals";
 import { Button, SegmentButtons, TempInput, TempSelect } from "components/common";
+import "./DevEntityInput.scss";
 
 interface Props {
   value?: any;
@@ -40,10 +41,10 @@ export default function DevMediaInput({ value }: Props) {
 
   const openModal = () => open({ component: InferenceJobsModal });
 
-  return studioAccessCheck(<div {...{ className: "fy-engine-compositor"}}>
-    <div {...{ className: "panel engine-compositor-container" }}>
+  return studioAccessCheck(<div {...{ className: "fy-engine-compositor p-3"}}>
+    <h2>{ [526,187].map((num = 0) => String.fromCodePoint(128000 + num)) }</h2>
+    <div {...{ className: "panel p-3 fy-basic-grid" }}>
       <header>
-        <h2>{ [526,187].map((num = 0) => String.fromCodePoint(128000 + num)) }</h2>
         <SegmentButtons {...{ onChange: ({ target }: { target: any }) => modeSet(target.value), options, value: mode }}/>
         <TempInput {...{ value: owner, onChange: ({ target }: { target: any }) => ownerSet(target.value), placeholder: "User" }}/>
       </header>
@@ -55,14 +56,14 @@ export default function DevMediaInput({ value }: Props) {
      <EntityInput {...{
         accept: ["bvh","glb","gltf"],
         aspectRatio: "landscape",
-        label: "not now",
+        label: "Choose entity",
         // label: `Choose ${ ["","media file","weight"][entityType] }`,
         onChange,
         owner,
         search: "Dream",
         type: "media"
       }}/>
-      <Button {...{ className: "mt-3", label: "Open modal", onClick: openModal, variant: "primary" }}/>
+      <Button {...{ label: "Open modal", onClick: openModal, variant: "primary" }}/>
      <div>
       { mediaToken }
      </div>
