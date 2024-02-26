45e54da9c13f6f5c35d4200116e14ae51ddb7099
Brandon Thomas <echelon@gmail.com>
2024-02-26 18:29:24 -0500

    add video upload handler to docs

--- crates/service/web/storyteller_web/src/api_doc.rs
+++ crates/service/web/storyteller_web/src/api_doc.rs
@@ -40,6 +40,8 @@ use crate::http_server::endpoints::media_files::list::list_media_files_by_batch_
 use crate::http_server::endpoints::media_files::list::list_media_files_for_user::*;
 use crate::http_server::endpoints::media_files::upload::upload_error::MediaFileUploadError;
 use crate::http_server::endpoints::media_files::upload::upload_media_file_handler::*;
+use crate::http_server::endpoints::media_files::upload_video::upload_error::VideoMediaFileUploadError;
+use crate::http_server::endpoints::media_files::upload_video::upload_video_media_file_handler::*;
 use crate::http_server::endpoints::prompts::get_prompt::*;
 use crate::http_server::endpoints::user_bookmarks::batch_get_user_bookmarks_handler::*;
 use crate::http_server::endpoints::user_bookmarks::create_user_bookmark_handler::*;
@@ -74,6 +76,7 @@ use crate::http_server::web_utils::response_success_helpers::*;
     crate::http_server::endpoints::media_files::list::list_media_files_by_batch_token::list_media_files_by_batch_token_handler,
     crate::http_server::endpoints::media_files::list::list_media_files_for_user::list_media_files_for_user_handler,
     crate::http_server::endpoints::media_files::upload::upload_media_file_handler::upload_media_file_handler,
+    crate::http_server::endpoints::media_files::upload_video::upload_video_media_file_handler::upload_video_media_file_handler,
     crate::http_server::endpoints::prompts::get_prompt::get_prompt_handler,
     crate::http_server::endpoints::user_bookmarks::batch_get_user_bookmarks_handler::batch_get_user_bookmarks_handler,
     crate::http_server::endpoints::user_bookmarks::create_user_bookmark_handler::create_user_bookmark_handler,
@@ -230,6 +233,8 @@ use crate::http_server::web_utils::response_success_helpers::*;
     SetUserRatingResponse,
     StatusDetailsResponse,
     StorytellerStreamPlan,
+    VideoMediaFileUploadError,
+    UploadVideoMediaSuccessResponse,
     TerminateInferenceJobError,
     TerminateInferenceJobPathInfo,
     TerminateInferenceJobSuccessResponse,
