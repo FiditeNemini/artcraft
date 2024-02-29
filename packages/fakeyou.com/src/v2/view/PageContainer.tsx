9da610b8a6615a2c825b7385876513ce592e2fc6
Victor <victor@storyteller.ai>
2024-02-28 22:14:55 -0800

    page container temporary addition

--- packages/fakeyou.com/src/v2/view/PageContainer.tsx
+++ packages/fakeyou.com/src/v2/view/PageContainer.tsx
@@ -123,6 +123,7 @@ import TextToImagePage from "./pages/text_to_image/TextToImagePage";
 import DomainConfigProvider from "context/DomainConfigContext";
 import DevUpload from "./pages/dev_upload/DevUpload";
 import DevMediaInput from "./pages/dev_upload/DevMediaInput";
+// import DevTTS from "./pages/dev_tts/DevTTS";
 
 interface Props {
   sessionWrapper: SessionWrapper;
@@ -746,7 +747,7 @@ class PageContainer extends React.Component<
                     />
                   </Route>
 
-                  <Route path="/studio">
+                  <Route path="/studio/:mediaToken?">
                     <StorytellerStudioListPage
                       sessionWrapper={this.props.sessionWrapper}
                       sessionSubscriptionsWrapper={
@@ -783,6 +784,11 @@ class PageContainer extends React.Component<
                     <DevMediaInput />
                   </Route>
 
+{
+                  // <Route path="/dev-tts">
+                  //   <DevTTS />
+                  // </Route>
+}
                   {/* Route for initial voice creation */}
                   <Route exact path="/voice-designer/create">
                     <VoiceDesignerFormPage
