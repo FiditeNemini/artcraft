f43e4d262c5faf1c84a3d5f04de1c08ee8b6b0a8
dev <test@local.host>
2024-03-19 18:02:23 -0400

    test 2

--- app/pages/Cube.tsx
+++ app/pages/Cube.tsx
@@ -6,29 +6,32 @@ export class SceneWithCube {
   renderer: THREE.WebGLRenderer;
   cube: THREE.Mesh;
   stopped: boolean;
+  callback: ({x,y}:{x:number;y:number})=>void;
 
   update(){
-    
-    console.log("animating in Cube");
-
     if(!this.stopped){
       this.cube.rotation.x += 0.01;
       this.cube.rotation.y += 0.01;
+      if(this.callback && typeof this.callback === "function")
+        this.callback({
+          x: this.cube.rotation.x,
+          y: this.cube.rotation.y
+        })
     }
     this.renderer.render(this.scene, this.camera);
   }
 
-  constructor() {
+  constructor(callback: ({x,y}:{x:number;y:number})=>void) {
     const width = 1000;
     const height = 750;
 
+    this.callback = callback;
     this.stopped = false;
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
-    // mountRef?.current?.appendChild(renderer.domElement);
   
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
--- app/pages/PageCube.tsx
+++ app/pages/PageCube.tsx
@@ -2,14 +2,21 @@ import {
   useRef,
   useEffect,
   useState,
+  useCallback,
 } from 'react';
 
 import { SceneWithCube } from './Cube';
 
 const PageCube = () => {
-  const mountRef = useRef<HTMLDivElement>(null);
+  const mountRef = useRef<HTMLDivElement | null>(null);
   const sceneRef = useRef<SceneWithCube | null>(null);
-
+  const [{
+    x:rotationX,
+    y:rotationY
+  }, setRotations] = useState<{
+    x: number | undefined,
+    y:number | undefined
+  }>({x:undefined, y:undefined});
 
   const toggleStopped = ()=>{
    if(sceneRef.current!==null){
@@ -17,31 +24,35 @@ const PageCube = () => {
    }
   };
 
+  const listeningXYRotation = useCallback((
+    {x,y}:{x:number;y:number}
+  )=>{
+    setRotations({x:x, y:y})
+  },[]);
+
   useEffect(() => {
-    sceneRef.current = new SceneWithCube();
-    mountRef?.current?.appendChild(sceneRef.current.renderer.domElement);
+    sceneRef.current = new SceneWithCube(listeningXYRotation);
+    mountRef.current?.appendChild(sceneRef.current.renderer.domElement);
     
     const animate = function () {
       requestAnimationFrame(animate);
-      sceneRef?.current?.update();
+      sceneRef.current?.update();
     };
     animate();
 
     // Clean up
     return () => {
       if(sceneRef.current)
-        mountRef?.current?.removeChild(sceneRef.current.renderer.domElement);
+        mountRef.current?.removeChild(sceneRef.current.renderer.domElement);
     };
   }, []);
-
-  // useEffect(()=>{
-  //   sc
-  // }, [stopped])
 
   return (
     <>
       <div className="Cube" ref={mountRef} />
       <button onClick={toggleStopped}> STOP </button>
+      <p>{rotationX ? rotationX : ""}</p>
+      <p>{rotationY ? rotationY : ""}</p>
     </>
   );
 
