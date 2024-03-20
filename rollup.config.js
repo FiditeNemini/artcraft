ccd588b6d1dee052ccb889e7855852eafe459af1
dev <test@local.host>
2024-03-19 22:11:34 -0400

    testing another module

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,23 @@
-import postcss from 'rollup-plugin-postcss';
+// import postcss from 'rollup-plugin-postcss';
+import styles from "rollup-plugin-styles";
+
 
 export default {
   plugins: [
-    postcss({
+    // postcss({
+    //   modules: true,
+    //   plugins: []
+    // }),
+    styles({
       modules: true,
-      plugins: []
+      // // ...or with custom options
+      // modules: {},
+      // ...additionally using autoModules
+      autoModules: true,
+      // // ...with custom regex
+      // autoModules: /\.mod\.\S+$/,
+      // // ...or custom function
+      // autoModules: id => id.includes(".modular."),
     })
   ]
 }
