caf416dd02f316dbe7c350a3f3ea0ad20115ed55
wil <wil@storyteller.ai>
2024-03-23 12:37:38 -0400

    put down icons for video controls

--- app/pages/PageEnigma/comps/ControlsVideo/ControlsVideo.tsx
+++ app/pages/PageEnigma/comps/ControlsVideo/ControlsVideo.tsx
@@ -1,6 +1,22 @@
+import {
+  faBackwardFast,
+  faBackwardStep,
+  faForwardFast,
+  faForwardStep,
+  faPlay,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const ControlsVideo = ()=>{
   return(
-    <div className="fixed top-3/4 left-1/2 -translate-x-1/2 -mt-10 bg-ui-panel border-t border-x border-ui-panel-border rounded-t-md p-2 text-white">Video Controls</div>
+    <div className="fixed top-3/4 left-1/2 -translate-x-1/2 -mt-10 bg-ui-panel border-t border-x border-ui-panel-border rounded-t-md px-6 py-2 text-white">
+      <div className='flex gap-6 content-center	'>
+        <FontAwesomeIcon className="h-6" icon={faBackwardFast} />
+        <FontAwesomeIcon className="h-6" icon={faBackwardStep} />
+        <FontAwesomeIcon className="h-6" icon={faPlay} />
+        <FontAwesomeIcon className="h-6" icon={faForwardStep} />
+        <FontAwesomeIcon className="h-6"icon={faForwardFast} />
+      </div>
+    </div>
   );
 };
