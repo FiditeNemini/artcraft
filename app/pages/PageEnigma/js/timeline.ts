77e53da58c009f11db872b98b9d0bed1e7abd1f9
Michael Chung <6252325+ArEnSc@users.noreply.github.com>
2024-03-23 15:55:25 -0400

    Pushing timeline alpha

--- app/pages/PageEnigma/js/timeline.ts
+++ app/pages/PageEnigma/js/timeline.ts
@@ -1,106 +1,193 @@
-// Write unsubcribe and run the remainder of the clip tech.
+import { AnyJson } from "three/examples/jsm/nodes/core/constants.js"
+import { ClipOffset } from "../datastructures/clips/clip_offset"
 
-// export class Clip {
-//     version: number;
-//     clip_id: number;
+export class TimelineCurrentReactState {
+    isEditable:boolean
+    constructor() {
+        this.isEditable = true // can add clips to it
+    }
+}
+
+export class TimeLine {
+    timelineItems: ClipOffset[]
+    runningClips: ClipOffset[]
+
+    //timerID: NodeJS.Timeout | null
+    timeLineLimit: number
+    scrubberPosition: number
+    isPlaying: boolean
+
+    timelineState: TimelineCurrentReactState
     
-//     media_token_id: number;
-//     type: 'animation' | 'audio' | 'position_rotation';
-//     time_start_offset: number;
-//     length_override: number | null;
+    constructor() {
+        this.timelineItems = []
+        // this.timerID = null
+        this.timeLineLimit = 1000 * 10 // 10 seconds
+        this.runningClips = []
+        this.isPlaying = false
+        this.scrubberPosition = 0 // in ms into the tl
+        this.timelineState = new TimelineCurrentReactState()
+    }
 
-//     constructor(
-//         version: number,
-//         clip_id: number,
-//         media_token_id: number,
-//         type: 'animation' | 'audio' | 'position_rotation',
-//         time_start_offset: number,
-//         length_override: number | null
-//     ) {
-//         this.version = version;
-//         this.clip_id = clip_id;
-//         this.media_token_id = media_token_id;
-//         this.type = type;
-//         this.time_start_offset = time_start_offset;
-//         this.length_override = length_override;
-//     }
-// }
-
-
-// export class Clip2 {
-//     name: string
-//     startOffset: number
-//     timerID: NodeJS.Timeout | null
-//     lengthOfClip: number
-//     scrubberEndingPosition: number
-
-//     constructor(name: string, startOffset: number, lengthOfClip: number) {
-//         this.startOffset = startOffset
-//         this.lengthOfClip = lengthOfClip
-//         this.name = name
-//         this.timerID = null
-
-//         this.scrubberEndingPosition = 100
-//     }
-
-//     async play(): Promise<void> {
-//         console.log(`Playing ${this.name}`)
-//         this.timerID = setInterval(async () => {
-//             console.log(`Running async ${this.name}`)
-//             this.stop()
-//         }, this.lengthOfClip)
-//     }
-
-//     async stop(): Promise<void> {
-//         clearInterval(this.timerID!)
-//         console.log(`Stopping ${this.name}`)
-//     }
-// }
-
-// export class API2 {
-//     timelineItems: Clip[]
-//     timerID: NodeJS.Timeout | null
-//     timeLineLimit: number
-    
-//     runningClips: Clip[]
-
-//     constructor() {
-//         this.timelineItems = []
-//         this.timerID = null
-//         this.timeLineLimit = 1000 * 10
-//         this.runningClips = []
-//     }
-
-//     async addPlayableClip(clip: Clip): Promise<void> {
-//         this.timelineItems.push(clip)
-//     }
-
-//     async start(): Promise<void> {
-//         let start = 0
-//         const updateInterval = 100
-//         const timerID = setInterval(async () => {
-//             console.log(`Current Time:${start}`)
-            
-//             // get last updated time when stopped
-//             for (const element of this.timelineItems) {
-//                 if (element.startOffset >= start) {
-//                     element.play()
-//                     // remove the element
-//                     this.timelineItems = this.timelineItems.filter(item => item !== element)
-//                 }
-//             }
-//             if (start == this.timeLineLimit || this.timelineItems.length == 0) {
-//                 this.stop()
-//             }
-            
-//             start += updateInterval
-//         }, updateInterval)
+    // public streaming events into the timeline from
+    async setScrubberPosition(offset:number) {
+        this.scrubberPosition = offset // in ms
+    }
         
-//         this.timerID = timerID
-//     }
+    async addPlayableClip(clip: ClipOffset): Promise<void> {
+        this.timelineItems.push(clip)
+    }
 
-//     async stop(): Promise<void> {
-//         console.log(`Stopping Timeline`)
-//         clearInterval(this.timerID!)
-//     }
+    async deleteClip(clip_uuid: string): Promise<void> {
+
+    }
+
+    // this will update the state of the clips based off uuid
+    async modifyClip(clip_uuid: string, updates: AnyJson): Promise<void> {
+
+    }
+
+    // timeline controls this.
+    async scrub(offset_in_ms:number): Promise<void> {
+        // only stream through to the position and rotation keyframes
+    }
+
+    // should play from the clip that is closest to the to scrubber
+    async play(): Promise<void> {
+        console.log(`Starting Timeline`)
+        this.isPlaying = true
+        // const updateInterval = 100
+        // const timerID = setInterval(async () => {
+        //     console.log(`Current Time:${start}`)
+            
+        //     // get last updated time when stopped
+        //     for (const element of this.timelineItems) {
+        //         if (element.start_offset >= start) {
+        //             // run async
+        //             // element.play()
+        //             // remove the element
+        //             this.timelineItems = this.timelineItems.filter(item => item !== element)
+        //         }
+        //     }
+        //     if (start == this.timeLineLimit || this.timelineItems.length == 0) {
+        //         this.stop()
+        //     }
+            
+        //     start += updateInterval
+        // }, updateInterval)
+        
+        // this.timerID = timerID
+    }
+
+
+    // called by the editor update loop on each frame
+    async update() {
+        if (this.isPlaying == false) return; // start and stop 
+
+        for (const element of this.timelineItems) {
+            if (element.start_offset >= this.scrubberPosition) {
+                // run async
+                // element.play()
+                // remove the element from the list
+                this.timelineItems = this.timelineItems.filter(item => item !== element)
+            }
+            if (this.scrubberPosition == this.timeLineLimit) { // stops at where clips should // cannot throw clip
+                this.stop()
+            }
+        }
+    }
+
+    async stop(): Promise<void> {
+        this.isPlaying = false
+        console.log(`Stopping Timeline`)
+    }
+}
+
+// How much timeline precision we have
+const percision = 100 // using a fake update loop mock
+
+// Visual verification tests.
+function CheckIfBasicAudioClipWorks() {
+    const api = new TimeLine()
+    api.addPlayableClip(new ClipOffset(1.0,'audio',1,0))
+    api.play()
+}
+
+function CheckIfBasicTransformClipWorks() {
+    const api = new TimeLine()
+    api.addPlayableClip(new ClipOffset(1.0,'transform',2,0))
+    api.play()
+}
+
+function CheckIfBasicClipWorks() {
+    const api = new TimeLine()
+    api.addPlayableClip(new ClipOffset(1.0,'animation',3,0))
+    api.play()
+}
+
+// function CheckIfBasicClipWorks3SecondsAfterTimelineStops() {
+//     const api = new TimeLine()
+//     api.addPlayableClip(new ClipOffset("clip1", 0, 10000))
+//     api.play()
 // }
+
+// function CheckIfTwoClipsAtTheSameTimeWorks() {
+//     const api = new TimeLine()
+//     api.addPlayableClip(new ClipOffset("clip1", 0, 1000))
+//     api.play()
+// }
+
+// function CheckIfTwoClipsOneAfterAnotherWorks() {
+//     const api = new TimeLine()
+//     api.addPlayableClip(new ClipOffset("clip1", 0, 1000))
+//     api.addPlayableClip(new ClipOffset("clip2", 0, 2000))
+//     api.play()
+// }
+
+// function CheckIfTimeLineStopBeforeClipPlays() {
+//     const api = new TimeLine()
+//     api.addPlayableClip(new ClipOffset("clip3",0,1000))
+//     api.play()
+//     setInterval(async ()=> {
+//         api.stop()
+//     },1100)
+//     console.log("Stopped")
+// }
+
+// function CheckIfTimeLineStartAfterClipPlays() {
+//     const api = new TimeLine() 
+//     api.addPlayableClip(new ClipOffset("clip3",0,1000))
+//     api.play()
+//     setInterval(async ()=> {
+//         api.stop()
+//     },500)
+//     console.log("Stopped")
+// }
+
+// function CheckIfClipsPlayAllTogetherConcurrently() {
+//     const api = new TimeLine() 
+//     api.addPlayableClip(new ClipOffset("clip1",0,1000))
+//     api.addPlayableClip(new ClipOffset("clip2",0,1000))
+//     api.addPlayableClip(new ClipOffset("clip3",0,1000))
+//     api.addPlayableClip(new ClipOffset("clip4",0,1000))
+//     api.play()
+// }
+
+// function CheckIfClipsPlayAllTogether() {
+//     const api = new TimeLine() 
+//     api.addPlayableClip(new TrackClip("clip1",0,1000))
+//     api.addPlayableClip(new TrackClip("clip2",0,1000))
+//     api.addPlayableClip(new TrackClip("clip3",0,1000))
+//     api.addPlayableClip(new TrackClip("clip4",0,1000))
+//     api.play()
+// }
+
+CheckIfBasicClipWorks()
+//CheckIfTwoClipsAtTheSameTimeWorks()
+// CheckIfTwoClipsOneAfterAnotherWorks()
+// CheckIfTimeLineStopBeforeClipPlays()
+// CheckIfTimeLineStartAfterClipPlays()
+// CheckIfClipsPlayAllTogetherConcurrently()
+
+// CheckIfClipsPlayAllTogether()
+
