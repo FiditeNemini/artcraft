f34d348df6b345636195e1d13912a35204d25916
FromTheLOLAshes <from.the.lol.ashes@gmail.com>
2024-03-25 06:21:48 +0700

    add back the test buttons

--- app/pages/PageEnigma/comps/Timeline/Timeline.tsx
+++ app/pages/PageEnigma/comps/Timeline/Timeline.tsx
@@ -3,6 +3,7 @@ import Editor from "../../js/editor";
 import { Character } from "./Character";
 import { useCallback, useContext } from "react";
 import { TrackContext } from "~/contexts/TrackContext";
+import { Button } from "~/components";
 
 export const Timeline = ({
   editorCurrent,
@@ -13,6 +14,14 @@ export const Timeline = ({
 }) => {
   const { characters, updateCharacters } = useContext(TrackContext);
 
+  const handleButtonLoad = () => {
+    document.getElementById("load-upload")?.click();
+  };
+  const handleButtonRender = () => {
+    editorCurrent?.togglePlayback();
+  };
+  const handleButtonPlay = () => {};
+
   const updateClip = useCallback(
     (id: string, offset: number, length: number) => {
       updateCharacters({ type: "animations", id, offset, length });
@@ -20,9 +29,15 @@ export const Timeline = ({
     [],
   );
 
-  const width = (window.innerWidth - 40) / 10 - 2;
+  // const width = (window.innerWidth - 40) / 10 - 2;
+  const width = 1500;
   return (
     <LowerPanel>
+      <div className="fixed" style={{top: 72, left: 1000}}>
+      <Button onClick={handleButtonLoad}>Load</Button>
+      <Button onClick={handleButtonRender}>Render</Button>
+      <Button onClick={handleButtonPlay}>Play</Button>
+      </div>
       <div className="h-10 border-b border-ui-panel-border"></div>
       <input
         style={{ display: "none" }}
