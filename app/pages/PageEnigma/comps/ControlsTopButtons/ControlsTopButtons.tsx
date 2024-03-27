1fad2c5353e848dde06a210f70034fc4f0f15e00
wil <wil@storyteller.ai>
2024-03-27 14:41:01 -0400

    load scene token button

--- app/pages/PageEnigma/comps/ControlsTopButtons/ControlsTopButtons.tsx
+++ app/pages/PageEnigma/comps/ControlsTopButtons/ControlsTopButtons.tsx
@@ -6,15 +6,21 @@ import { EngineContext } from "../../contexts/EngineContext";
 export const ControlsTopButtons = () => {
   const editorEngine = useContext(EngineContext);
   const [sceneName, setSceneName] = useState<string>("");
+  const [sceneToken, setSceneToken] = useState<string>("");
 
   const handleButtonSave = () => {
     console.log(`SceneName is ${sceneName}`);
     // editorEngine?.save();
   };
 
-  const handleButtonCameraView = () => {
-    editorEngine?.change_camera_view();
+  const handleButtonLoadScene = () => {
+    console.log(`Scene Token is ${sceneToken}`);
+    // editorEngine?.save();
   };
+
+  // const handleButtonCameraView = () => {
+  //   editorEngine?.change_camera_view();
+  // };
 
   const handleButtonPlayBack = () => {
     editorEngine?.start_playback();
@@ -24,46 +30,64 @@ export const ControlsTopButtons = () => {
   };
   const handleButtonRender = () => {
     console.log("Saving GLB to server for reference");
-    editorEngine._upload_for_testing();
+    editorEngine?._upload_for_testing();
     //editorEngine?.togglePlayback();
   };
   const handleButtonPlay = () => {};
 
   return (
-    <div className="flex gap-2 pl-3 pt-3">
-      <Button variant="secondary" onClick={handleButtonPlayBack}>
-        Toggle Camera View
-      </Button>
+    <div className="flex flex-col gap-2 pl-3 pt-3">
+      <div className="flex gap-2">
+        <ButtonDialogue
+            buttonProps={{
+              variant: "secondary",
+              label: "Load Scene",
+            }}
+            title="Load Scene"
+            confirmButtonProps={{
+              label: "Load",
+              disabled: sceneToken === "" ? true : false,
+              onClick: handleButtonLoadScene,
+            }}
+          >
+          <Input label="Please Enter a name for your scene" />
+        </ButtonDialogue>
 
-      <ButtonDialogue
-        buttonProps={{
-          variant: "secondary",
-          label: "Save Scene",
-        }}
-        title="Save Scene"
-        confirmButtonProps={{
-          label: "Save",
-          disabled: sceneName === "" ? true : false,
-          onClick: handleButtonSave,
-        }}
-      >
-        <Input label="Please Enter a name for your scene" />
-      </ButtonDialogue>
+        <ButtonDialogue
+          buttonProps={{
+            variant: "secondary",
+            label: "Save Scene",
+          }}
+          title="Save Scene"
+          confirmButtonProps={{
+            label: "Save",
+            disabled: sceneName === "" ? true : false,
+            onClick: handleButtonSave,
+          }}
+        >
+          <Input label="Please Enter a name for your scene" />
+        </ButtonDialogue>
 
-      <ButtonDialogue
-        buttonProps={{
-          variant: "secondary",
-          label: "Help",
-        }}
-        title="Help"
-      >
-        <p>Do you need help?</p>
-        <p>Ask Michael about this project</p>
-        <p>Ask Miles about ThreeJS</p>
-        <p>Ask Wil about React</p>
-      </ButtonDialogue>
+        <ButtonDialogue
+          buttonProps={{
+            variant: "secondary",
+            label: "Help",
+          }}
+          title="Help"
+        >
+          <p>Do you need help?</p>
+          <p>Ask Michael about this project</p>
+          <p>Ask Miles about ThreeJS</p>
+          <p>Ask Wil about React</p>
+        </ButtonDialogue>
+      </div>
 
-      <div className="fixed flex gap-2" style={{ top: 124, left: 12 }}>
+      <div className="flex gap-2">
+        <Button variant="secondary" onClick={handleButtonPlayBack}>
+          Toggle Camera View
+        </Button>
+      </div>
+      <div className="flex gap-2">
         <Button onClick={handleButtonLoad}>Load</Button>
         <Button onClick={handleButtonRender}>Render</Button>
         <Button onClick={handleButtonPlay}>Play</Button>
