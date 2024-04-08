63b680ad73c205110c1dccc976e948e732a9095d
Michael Chung <6252325+ArEnSc@users.noreply.github.com>
2024-04-08 16:30:57 -0400

    minizing app

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig({
         });
       },
     },],
+    build: {
+      minify: "terser",
+    },
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+    },
 });
