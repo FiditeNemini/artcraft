e6d4d0bdc246358b29fc45375fd5c1bf70a19362
bflatastic <bombayapirat@gmail.com>
2024-04-08 11:46:13 +0700

    filter tabs fix

--- app/pages/PageEnigma/comps/SidePanelTabs/itemTabs/filterTabsClasses.ts
+++ app/pages/PageEnigma/comps/SidePanelTabs/itemTabs/filterTabsClasses.ts
@@ -1,6 +1,7 @@
 export const filterTabBase =
-  "text-nowrap rounded-lg border-2 border-assets-background bg-assets-background px-3 py-2 text-sm font-medium transition-all duration-150";
+  "text-nowrap rounded-lg border-2 border-assets-background bg-assets-background px-3 py-2 text-sm font-medium transition-all duration-150 hover:bg-assets-background/80 hover:border-assets-background/0";
 
-export const filterTabActive = "border-2 border-brand-primary";
+export const filterTabActive =
+  "border-2 border-brand-primary hover:border-brand-primary hover:bg-assets-background";
 
 export const filterTabDisabled = "disabled:text-gray-500";
