ac45f9cd41cda0793c8fe7c9fef1efa426d58b2c
Michael Chung <6252325+ArEnSc@users.noreply.github.com>
2024-04-11 13:56:57 -0400

    Scene loading works

--- app/pages/PageEnigma/PageEnigmaComponent.tsx
+++ app/pages/PageEnigma/PageEnigmaComponent.tsx
@@ -5,6 +5,8 @@ import { AppUiContext } from "~/contexts/AppUiContext";
 import { EngineContext } from "~/contexts/EngineContext";
 import { PageEditor } from "~/pages/PageEnigma/PageEditor";
 import { PageStyling } from "~/pages/PageEnigma/PageStyling";
+import { useParams } from "@remix-run/react";
+
 
 export const PageEnigmaComponent = () => {
   useSignals();
@@ -12,13 +14,20 @@ export const PageEnigmaComponent = () => {
   const [, dispatchAppUiState] = useContext(AppUiContext);
   const editor = useContext(EngineContext);
   const [page, setPage] = useState("edit");
-
+  const params = useParams();
+  console.log(params);
+  
   useEffect(() => {
     if (editor && editor.can_initialize && dispatchAppUiState !== null) {
       console.log("initializing Editor");
+      
+  // get scene token for editor is it is provided
+     console.log(`params?!?!: ${params["sceneToken"]}`);
+     const sceneToken = params["sceneToken"]
+      console.log(sceneToken)
       editor.initialize({
-        dispatchAppUiState,
-      });
+        dispatchAppUiState
+      },sceneToken);
     }
   }, [editor, dispatchAppUiState]);
 
