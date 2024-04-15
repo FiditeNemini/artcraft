7b98afbac27f8e6f37822fc3fef47b6d88e656e8
Brandon Thomas <echelon@gmail.com>
2024-04-15 12:15:58 -0400

    allow studio on all hostnames

--- packages/components/src/utils/StudioRolloutHostnameAllowed.ts
+++ packages/components/src/utils/StudioRolloutHostnameAllowed.ts
@@ -7,13 +7,14 @@ export function StudioRolloutHostnameAllowed() : boolean {
 }
 
 function StudioRolloutHostnameCheck() : boolean {
-    // Do not allow "production" or "staging" domains to use studio yet.
-    // Other domains (local development and feature branches) are allowed.
-    switch (window.location.hostname) {
-        case 'fakeyou.com':
-        case 'storyteller.ai':
-            return false;
-        default:
-            return true;
-    }
+    //// Do not allow "production" or "staging" domains to use studio yet.
+    //// Other domains (local development and feature branches) are allowed.
+    //switch (window.location.hostname) {
+    //    case 'fakeyou.com':
+    //    case 'storyteller.ai':
+    //        return false;
+    //    default:
+    //        return true;
+    //}
+    return true;
 }
