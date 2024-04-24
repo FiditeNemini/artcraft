dea106a61253ce85a904a25ac358f0e3e9120b6f
Victor <victor@storyteller.ai>
2024-04-24 15:30:11 -0700

    restored primitive cover images

---
+++ app/pages/PageEnigma/models/inferenceJobs.ts
@@ -0,0 +1,20 @@
+export enum FrontendInferenceJobType {
+  FaceAnimation,
+  TextToSpeech,
+  VoiceConversion,
+  VoiceDesignerCreateVoice,
+  VoiceDesignerTts,
+  ImageGeneration,
+  VideoMotionCapture,
+  ConvertFbxtoGltf,
+  EngineComposition,
+  VideoWorkflow,
+  VideoStyleTransfer,
+}
+export interface InferenceJob {
+  version: number;
+  job_id: string;
+  job_type: FrontendInferenceJobType;
+  job_status: string;
+  result?: any;
+}
