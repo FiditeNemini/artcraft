759c3a9601b5b2f8dae942c6891845ec6491505d
Brandon Thomas <echelon@gmail.com>
2024-05-07 11:27:20 -0700

    query to make files public

--- _sql/actions/media_files/set_all_user_videos_public.sql
+++ _sql/actions/media_files/set_all_user_videos_public.sql
@@ -2,11 +2,19 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
 
--- Set all featured media items public
+-- Set all user videos public
 -- NB: using join since subquery was doing a full table scan despite looking reasonable.
-UPDATE media_files m INNER JOIN featured_items items
-ON items.entity_token = m.token
-AND items.entity_type = 'media_file'
+UPDATE media_files m INNER JOIN users
+ON m.maybe_creator_user_token = users.token
+AND m.media_class  = 'video'
   SET m.creator_set_visibility = 'public'
 WHERE m.creator_set_visibility IN ('private', 'hidden')
-AND items.deleted_at IS NULL;
+AND users.username IN (
+  'devdude123',
+  'dreambig',
+  'gateway',
+  'heart_ribbon',
+  'postproduction',
+  'tellstories',
+  'yae_ph'
+)
