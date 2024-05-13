505c36784b5d66b362a96c6f7eb30f732ba2011b
BFlat <bombayapirat@gmail.com>
2024-05-14 06:42:26 +0700

    prelaunch landing init

--- packages/fakeyou.com/src/v2/view/pages/landing/storyteller/PrelaunchLanding/PrelaunchLanding.tsx
+++ packages/fakeyou.com/src/v2/view/pages/landing/storyteller/PrelaunchLanding/PrelaunchLanding.tsx
@@ -1,19 +1,169 @@
 // Grid.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PrelaunchLanding.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/pro-solid-svg-icons";
+import { BucketConfig } from "@storyteller/components/src/api/BucketConfig";
+import { set } from "date-fns";
+
+interface MediaItem {
+  token: string;
+  public_bucket_path: string;
+  maybe_creator: {
+    username: string;
+  };
+}
 
 const PrelaunchLanding: React.FC = () => {
-  const totalGrids = 30;
-  const blankGrids = [13, 14, 15, 19, 20];
+  const totalGrids = 63;
+  const [mediaItems, setMediaItems] = useState<MediaItem[]>([]);
+  const [blankGrids, setBlankGrids] = useState<number[]>([15, 16, 17, 22, 23]);
+  const [screenSize, setScreenSize] = useState("lg");
+
+  useEffect(() => {
+    fetchMediaItems();
+    updateGridsForScreenSize();
+    window.addEventListener("resize", updateGridsForScreenSize);
+    return () => window.removeEventListener("resize", updateGridsForScreenSize);
+  }, []);
+
+  const updateGridsForScreenSize = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      setBlankGrids([7, 8, 9, 10, 11, 12]);
+      setScreenSize("sm");
+    } else if (screenWidth < 992) {
+      setBlankGrids([12, 13, 14, 17, 18, 19]);
+      setScreenSize("md");
+    } else {
+      setBlankGrids([15, 16, 17, 22, 23]);
+      setScreenSize("lg");
+    }
+  };
+
+  const getCustomGridItemIndex = () => {
+    switch (screenSize) {
+      case "lg":
+        return 15;
+      case "md":
+        return 12;
+      case "sm":
+        return 7;
+      default:
+        return 15;
+    }
+  };
+
+  const fetchMediaItems = async () => {
+    try {
+      const response = await fetch(
+        "https://api.storyteller.ai/v1/media_files/list_featured?page_size=63&filter_media_classes=video"
+      );
+      const data = await response.json();
+      if (data.success) {
+        setMediaItems(data.results);
+      }
+    } catch (error) {
+      console.error("Error fetching media items:", error);
+    }
+  };
 
   const renderGridItems = () => {
     const gridItems = [];
+    const customGridItemIndex = getCustomGridItemIndex();
+
     for (let i = 1; i <= totalGrids; i++) {
       const isBlank = blankGrids.includes(i);
+      if (screenSize === "sm") {
+        if (i === 10) {
+          gridItems.push(
+            <div
+              key="10-12"
+              className={`grid-item triple-grid grid-item-custom ${
+                isBlank ? "blank" : ""
+              }`}
+            >
+              <button className="waitlist-button">
+                Join the Waitlist
+                <FontAwesomeIcon icon={faArrowRight} className="ms-2 fs-6" />
+              </button>
+            </div>
+          );
+          i++;
+          continue;
+        }
+      } else if (screenSize === "md") {
+        if (i === 17) {
+          gridItems.push(
+            <div
+              key="17-19"
+              className={`grid-item triple-grid grid-item-custom ${
+                isBlank ? "blank" : ""
+              }`}
+            >
+              <button className="waitlist-button">
+                Join the Waitlist
+                <FontAwesomeIcon icon={faArrowRight} className="ms-2 fs-6" />
+              </button>
+            </div>
+          );
+          i += 2;
+          continue;
+        }
+      } else {
+        if (i === 22) {
+          gridItems.push(
+            <div
+              key="22-23"
+              className={`grid-item double-grid grid-item-custom ${
+                isBlank ? "blank" : ""
+              }`}
+            >
+              <button className="waitlist-button">
+                Join the Waitlist
+                <FontAwesomeIcon icon={faArrowRight} className="ms-2 fs-6" />
+              </button>
+            </div>
+          );
+          i++;
+          continue;
+        }
+      }
+
+      const mediaItem = mediaItems[i - 1];
+      const bucketConfig = new BucketConfig();
+      const gifUrl = mediaItem
+        ? bucketConfig.getCdnUrl(
+            mediaItem.public_bucket_path + "-thumb.gif",
+            360,
+            20
+          )
+        : "";
+
       gridItems.push(
-        <div key={i} className={`grid-item ${isBlank ? "blank" : ""}`}>
-          {!isBlank ? (
-            <img src="/images/dummy-image.jpg" className="w-100 h-100" />
+        <div
+          key={i}
+          className={`grid-item ${isBlank ? "blank" : ""} ${
+            i === customGridItemIndex ? "grid-item-custom" : ""
+          }`.trim()}
+        >
+          {isBlank ? (
+            i === customGridItemIndex ? (
+              <img
+                className="logo"
+                src="/fakeyou/Storyteller-Logo-1.png"
+                alt="Storyteller Logo"
+              />
+            ) : null
+          ) : mediaItem ? (
+            <img
+              onClick={() =>
+                window.open(`https://storyteller.ai/media/${mediaItem.token}`)
+              }
+              src={gifUrl}
+              className="w-100 h-100 object-fit-cover"
+              alt={`Video by ${mediaItem.maybe_creator.username}`}
+            />
           ) : null}
         </div>
       );
@@ -21,15 +171,7 @@ const PrelaunchLanding: React.FC = () => {
     return gridItems;
   };
 
-  return (
-    <div className="grid-container">
-      {renderGridItems()}
-
-      <img className="logo" src="/fakeyou/Storyteller-Logo-1.png" />
-
-      <button className="waitlist-button">Join the Waitlist</button>
-    </div>
-  );
+  return <div className="grid-container">{renderGridItems()}</div>;
 };
 
 export default PrelaunchLanding;
