07590b23168b8d235001217b28c5fb4a8cf1036b
bflatastic <bombayapirat@gmail.com>
2024-05-22 23:31:20 +0700

    api storyteller ai

--- packages/components/src/api/GetApiHost.ts
+++ packages/components/src/api/GetApiHost.ts
@@ -3,7 +3,8 @@
 const domainWithoutPort = document.location.host.split(":")[0];
 
 const hostConfig = (host: string, useSsl = true) => ({
-  formatUrl: (endpoint = "") => `${ useSsl ? "https" : "http" }://${ host + endpoint }`,
+  formatUrl: (endpoint = "") =>
+    `${useSsl ? "https" : "http"}://${host + endpoint}`,
   host,
   useSsl,
 });
@@ -15,20 +16,19 @@ export default function GetApiHost() {
       return hostConfig("api.fakeyou.com");
     case "storyteller.ai":
     case "staging.storyteller.ai":
-      //return hostConfig("api.storyteller.ai");
-      return hostConfig("api.fakeyou.com");
+      return hostConfig("api.storyteller.ai");
     case "storyteller.stream": // Storyteller.stream is deprecated and will be decommissioned in the future.
       return hostConfig("api.storyteller.stream");
     case "devproxy.fakeyou.com":
       return hostConfig("api.fakeyou.com");
     case "devproxy.storyteller.ai":
       //return hostConfig("api.storyteller.ai");
-      return hostConfig("api.fakeyou.com");
+      return hostConfig("api.storyteller.ai");
     case "dev.fakeyou.com":
-      return hostConfig("api.dev.fakeyou.com:12345",false); // false disables SSL
+      return hostConfig("api.dev.fakeyou.com:12345", false); // false disables SSL
     default:
-      return document.location.host.includes("localhost") ? 
-        hostConfig("localhost:12345",document.location.protocol === "https:") :
-        hostConfig("api.fakeyou.com"); // default
+      return document.location.host.includes("localhost")
+        ? hostConfig("localhost:12345", document.location.protocol === "https:")
+        : hostConfig("api.fakeyou.com"); // default
   }
-};
+}
