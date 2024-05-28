e8d7b48d1cb586240af6dc3b46a039811d89dd44
Brandon Thomas <echelon@gmail.com>
2024-05-27 23:51:36 -0400

    Obsolete: Delete _develop/tools/testing/curl directory

--- _develop/tools/testing/curl/login_bad_password.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# LOCALHOST ONLY
-
-curl -vvv \
-  -H "content-type: application/json" \
-  --data '{"username_or_email": "echelon", "password": "BAD_PASSWORD"}' \
-  http://localhost:12345/login
