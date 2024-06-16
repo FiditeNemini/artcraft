2cb19357ab9c0caa48bfa8af0e1391fc333313d6
Brandon Thomas <echelon@gmail.com>
2024-06-16 13:41:06 -0400

    bump versions

--- script/execute_dependency_fix.sh
+++ script/execute_dependency_fix.sh
@@ -12,7 +12,8 @@ echo 'Looking for unused dependencies'
 
 # https://github.com/est31/cargo-udeps
 # https://stackoverflow.com/a/72082679
-cargo  udeps --all-targets
+# Needs nightly to run
+cargo +nightly udeps --all-targets
 
 # TODO: Ask for user input before applying some automated heuristics
 
