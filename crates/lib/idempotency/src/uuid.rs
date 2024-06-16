cd848319ebb350c7604d313d2cc548b13dc069d2
Brandon Thomas <echelon@gmail.com>
2024-06-16 14:41:22 -0400

    update more dependencies

--- crates/lib/idempotency/src/uuid.rs
+++ crates/lib/idempotency/src/uuid.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 pub fn generate_random_uuid() -> String {
   let uuid = Uuid::new_v4();
   let mut buffer = Uuid::encode_buffer();
-  let hyphenated_uuid = uuid.to_hyphenated()
+  let hyphenated_uuid = uuid.hyphenated()
       .encode_lower(&mut buffer);
   hyphenated_uuid.to_string()
 }
