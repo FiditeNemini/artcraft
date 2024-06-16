3aca9b909006fa01992a73597c87aa4c003d1f0e
Brandon Thomas <echelon@gmail.com>
2024-06-16 13:58:45 -0400

    actix upgrades

--- script/update_actix.sh
+++ script/update_actix.sh
@@ -2,10 +2,24 @@
 
 set -euxo pipefail
 
-echo 'Updating actix dependencies'
+echo 'Upgrading actix dependencies in Cargo.toml files'
+# Error: dependencies actix-codec, actix-macros, actix-multipart-derive, actix-router, actix-server don't exist
+cargo upgrade \
+  --verbose \
+  -p actix \
+  -p actix-files \
+  -p actix-http \
+  -p actix-multipart \
+  -p actix-rt \
+  -p actix-web \
+  -p actix-web-actors \
+  -p actix-web-lab
+
+echo 'Updating actix dependencies in Cargo.lock'
 
 # TODO: actix-cors update was causing issues
-cargo update --verbose \
+cargo update \
+  --verbose \
   actix \
   actix-codec \
   actix-files \
