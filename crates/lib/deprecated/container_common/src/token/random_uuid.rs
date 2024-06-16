826d710979a5d039df0eb6b7499529aaa8c87f41
Brandon Thomas <echelon@gmail.com>
2024-06-16 14:17:08 -0400

    more package upgrades

--- crates/lib/deprecated/container_common/src/token/random_uuid.rs
+++ crates/lib/deprecated/container_common/src/token/random_uuid.rs
@@ -3,7 +3,16 @@ use uuid::Uuid;
 pub fn generate_random_uuid() -> String {
   let uuid = Uuid::new_v4();
   let mut buffer = Uuid::encode_buffer();
-  let hyphenated_uuid = uuid.to_hyphenated()
+  let hyphenated_uuid = uuid.hyphenated()
     .encode_lower(&mut buffer);
   hyphenated_uuid.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_generate_random_uuid() {
+    let uuid = super::generate_random_uuid();
+    assert_eq!(uuid.len(), 36);
+  }
 }
