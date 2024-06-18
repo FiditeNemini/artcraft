6dd5aa30165ad0dc36b322819ee934a687f9cd33
ArEnSc <6252325+ArEnSc@users.noreply.github.com>
2024-06-17 23:55:53 -0400

    Fix content type (#489)

--- app/pages/PageEnigma/Editor/api_manager.ts
+++ app/pages/PageEnigma/Editor/api_manager.ts
@@ -295,6 +295,7 @@ export class APIManager {
       credentials: "include",
       headers: {
         Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: formData,
     });
@@ -401,6 +402,7 @@ export class APIManager {
       credentials: "include",
       headers: {
         Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: json_data,
     });
