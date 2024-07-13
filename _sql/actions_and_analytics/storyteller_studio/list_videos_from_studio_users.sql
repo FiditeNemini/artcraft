4146e922d8de9fa1be772798caebb7d77afdacc6
Brandon Thomas <echelon@gmail.com>
2024-07-12 23:48:46 -0400

    analytics query

--- _sql/actions_and_analytics/storyteller_studio/list_videos_from_studio_users.sql
+++ _sql/actions_and_analytics/storyteller_studio/list_videos_from_studio_users.sql
@@ -16,3 +16,35 @@ and m.is_intermediate_system_file = false
 and m.media_class = 'video'
 group by u.token
 order by video_count desc;
+
+-- Without internal users
+select
+  u.username,
+  count(*) as video_count,
+  u.maybe_source,
+  u.created_at
+from media_files as m
+join users as u
+on m.maybe_creator_user_token = u.token
+where u.can_access_studio = true
+and m.created_at > (CURDATE() - INTERVAL 14 DAY)
+and m.is_intermediate_system_file = false
+and m.media_class = 'video'
+and u.username NOT IN (
+  'bflat',
+  'candyfoxxx',
+  'crossproduct1',
+  'echelon',
+  'endtimes',
+  'heart_ribbon',
+  'kasisnu',
+  'mechacosm',
+  'moonchamp',
+  'olivicmic',
+  'printrman',
+  'vegito1089',
+  'wilwong',
+  'yae_ph'
+)
+group by u.token
+order by created_at desc;
