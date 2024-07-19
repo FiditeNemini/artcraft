705013f5e0fc19bc918541985dc326224f567adf
Brandon Thomas <echelon@gmail.com>
2024-07-18 21:31:20 -0400

    top models analytics query

--- _sql/actions_and_analytics/model_weights/model_weights.sql
+++ _sql/actions_and_analytics/model_weights/model_weights.sql
@@ -19,3 +19,29 @@ where
   and m.mod_deleted_at IS NULL
 order by m.title asc;
 
+-- Top models within time period
+-- NB: id threshold is to avoid a full table scan
+select
+    mw.token,
+    mw.title,
+    mw.weights_type,
+    mw.weights_category,
+    u.username as creator_username,
+    r.use_count,
+    mw.created_at,
+    mw.user_deleted_at,
+    mw.mod_deleted_at
+from (
+    select maybe_model_token, count(*) as use_count
+    from generic_inference_jobs
+    where maybe_model_token IS NOT NULL
+        and created_at > NOW() - INTERVAL 1 DAY
+        and id > 40000000
+    group by maybe_model_token
+) as r
+join model_weights as mw
+    on mw.token = r.maybe_model_token
+join users as u
+    on u.token = mw.creator_user_token
+order by r.use_count desc
+    limit 100;
