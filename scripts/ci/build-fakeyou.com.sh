316e53030850c2e350826ebe697c23a0fb16da93
Brandon Thomas <echelon@gmail.com>
2024-07-29 09:46:39 -0400

    organize build scripts

--- scripts/ci/build-fakeyou.com.sh
+++ scripts/ci/build-fakeyou.com.sh
@@ -3,35 +3,37 @@
 
 set -euxo pipefail
 
+function replace_commit_ref {
+  # Add the GIT SHA to the build
+  # This must be done before everything else, or it will get cached with the build.
+  # (This might be making the builds less performant?)
+  # COMMIT_REF is defined by Netlify to be the commit SHA
+  # We want a short 8 character reference.
+  SHORT_SHA=$(echo "${COMMIT_REF}" | cut -c1-8)
+  find . -type f -exec sed -i "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} +
+  # The above command won't work with Mac's version of find/sed. The following is a Mac-friendly version:
+  # find . -type f -exec sed -i '' -e "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} + 
+}
+
+function build_project {
+  yarn install 
+  yarn test-fakeyou --verbose
+  # --ignore-engines: https://stackoverflow.com/a/59615348
+  yarn build-fakeyou --verbose --ignore-optional --ignore-engines
+}
+
 echo "Current working directory:"
 pwd
 
-# Add the GIT SHA to the build
-# This must be done before everything else, or it will get cached with the build.
-# (This might be making the builds less performant?)
-# COMMIT_REF is defined by Netlify to be the commit SHA
-# We want a short 8 character reference.
 echo "Labelling build with short SHA..."
-SHORT_SHA=$(echo "${COMMIT_REF}" | cut -c1-8)
-find . -type f -exec sed -i "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} +
-# The above command won't work with Mac's version of find/sed. The following is a Mac-friendly version:
-# find . -type f -exec sed -i '' -e "s/CURRENT_STORYTELLER_VERSION/${SHORT_SHA}/g" {} + 
+replace_commit_ref
 
-echo "Building website..."
-
-# Move to project root
+echo "Building project..."
 pushd src/website
-
-yarn install 
-
-# Run tests first
-yarn test-fakeyou --verbose
-
-# --ignore-engines: https://stackoverflow.com/a/59615348
-yarn build-fakeyou --verbose --ignore-optional --ignore-engines
-
+build_project
 popd
 
+echo "Copying built artifacts..."
 mkdir fakeyou.com
 mv src/website/packages/fakeyou.com/build/ fakeyou.com/build/
 
