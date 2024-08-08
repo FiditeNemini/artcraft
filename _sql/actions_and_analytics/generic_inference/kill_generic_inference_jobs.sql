6d4adfe60fa8b60f7445afd07bb36f4e16f0d7da
Brandon Thomas <echelon@gmail.com>
2024-08-08 17:44:39 -0400

    cleanup analytics queries

--- _sql/actions_and_analytics/generic_inference/kill_generic_inference_jobs.sql
+++ _sql/actions_and_analytics/generic_inference/kill_generic_inference_jobs.sql
@@ -2,31 +2,30 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlResolveForFile
 
--- Kill routed jobs
-update generic_inference_jobs set status = 'dead' where maybe_routing_tag IS NOT NULL limit 100;
-
--- Get pending jobs
-select count(*) from generic_inference_jobs where status = 'pending';
-
--- Kill outstanding jobs
+-- Kill *ALL* pending / outstanding jobs of any type
 update generic_inference_jobs
 set status = 'dead'
 where status IN ('pending', 'started', 'attempt_failed');
 
--- Kill outstanding jobs of type
+-- Kill all routed jobs
 update generic_inference_jobs
 set status = 'dead'
-where status IN ('pending', 'started', 'attempt_failed')
-and maybe_model_type IN ('sad_talker', 'so_vits_svc');
+where maybe_routing_tag IS NOT NULL;
 
--- Tacotron 2 TTS
+-- Kill all Tacotron TTS jobs
 update generic_inference_jobs
 set status = 'dead'
 where status IN ('pending', 'started', 'attempt_failed')
   and maybe_model_type IN ('tacotron2');
 
--- Sad Talker Face Animation
+-- Kill all Sad Talker Face Animation jobs
 update generic_inference_jobs
 set status = 'dead'
 where status IN ('pending', 'started', 'attempt_failed')
   and maybe_model_type IN ('sad_talker');
+
+-- Kill all jobs of several types
+update generic_inference_jobs
+set status = 'dead'
+where status IN ('pending', 'started', 'attempt_failed')
+  and maybe_model_type IN ('sad_talker', 'so_vits_svc');
