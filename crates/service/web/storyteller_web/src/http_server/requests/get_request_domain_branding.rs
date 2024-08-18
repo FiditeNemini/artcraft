7c6356b70126dbb1865d996f10e49c8e8fc21b1b
Brandon Thomas <echelon@gmail.com>
2024-08-18 03:46:49 -0400

    hostname detection not working with URI - use host header

--- crates/service/web/storyteller_web/src/http_server/requests/get_request_domain_branding.rs
+++ crates/service/web/storyteller_web/src/http_server/requests/get_request_domain_branding.rs
@@ -1,3 +1,4 @@
+use actix_web::http::header::HOST;
 use actix_web::HttpRequest;
 use log::info;
 
@@ -8,54 +9,115 @@ pub enum DomainBranding {
 }
 
 pub fn get_request_domain_branding(http_request: &HttpRequest) -> Option<DomainBranding> {
-  let maybe_hostname = http_request.uri()
-      .host();
+  // NB: http_request.uri() does not include the hostname - it only includes the path (!)
+  let maybe_host_header = http_request.headers()
+      .get(HOST)
+      .map(|header| header.to_str().ok())
+      .flatten();
 
-  let maybe_branding = maybe_hostname.and_then(|host| match host {
-        host if host.contains("fakeyou") => Some(DomainBranding::FakeYou),
-        host if host.contains("storyteller") => Some(DomainBranding::Storyteller),
-        _ => None,
-      });
+  if let Some(branding) = match_possible_hostname(maybe_host_header) {
+    info!("Host header: {:?} Branding for hostname: {:?}", maybe_host_header, branding);
+    return Some(branding);
+  }
 
-  info!("Hostname: {:?} Branding for hostname: {:?}", maybe_hostname, maybe_branding);
+  // NB: This may not actually work against real requests. It fails in local dev. See above comment.
+  let maybe_hostname = http_request.uri().host();
 
-  maybe_branding
+  if let Some(branding) = match_possible_hostname(maybe_hostname) {
+    info!("URI hostname: {:?} Branding for hostname: {:?}", maybe_hostname, branding);
+    return Some(branding);
+  }
+
+  None
+}
+
+fn match_possible_hostname(maybe_hostname: Option<&str>) -> Option<DomainBranding> {
+  let hostname = match maybe_hostname {
+    Some(hostname) => hostname,
+    None => return None,
+  };
+  match hostname {
+    host if host.contains("fakeyou") => Some(DomainBranding::FakeYou),
+    host if host.contains("storyteller") => Some(DomainBranding::Storyteller),
+    _ => None,
+  }
 }
 
 #[cfg(test)]
 mod tests {
   use actix_web::test::TestRequest;
+
   use crate::http_server::requests::get_request_domain_branding::{DomainBranding, get_request_domain_branding};
 
-  #[test]
-  fn fakeyou_dot_com() {
-    let request = TestRequest::get()
-        .uri("https://fakeyou.com")
-        .to_http_request();
-    assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
+  mod host_header {
+    use super::*;
+
+    #[test]
+    fn fakeyou_dot_com() {
+      let request = TestRequest::get()
+          .insert_header(("host", "fakeyou.com"))
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
+    }
+
+    #[test]
+    fn api_dot_fakeyou_dot_com() {
+      let request = TestRequest::get()
+          .insert_header(("host", "api.fakeyou.com"))
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
+    }
+
+    #[test]
+    fn storyteller_dot_ai() {
+      let request = TestRequest::get()
+          .insert_header(("host", "storyteller.ai"))
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
+    }
+
+    #[test]
+    fn api_dot_storyteller_dot_ai() {
+      let request = TestRequest::get()
+          .insert_header(("host", "api.storyteller.ai"))
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
+    }
   }
 
-  #[test]
-  fn api_dot_fakeyou_dot_com() {
-    let request = TestRequest::get()
-        .uri("https://api.fakeyou.com")
-        .to_http_request();
-    assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
-  }
+  mod uri {
+    use super::*;
 
-  #[test]
-  fn storyteller_dot_ai() {
-    let request = TestRequest::get()
-        .uri("https://storyteller.ai")
-        .to_http_request();
-    assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
-  }
+    #[test]
+    fn fakeyou_dot_com() {
+      let request = TestRequest::get()
+          .uri("https://fakeyou.com")
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
+    }
 
-  #[test]
-  fn api_dot_storyteller_dot_ai() {
-    let request = TestRequest::get()
-        .uri("https://api.storyteller.ai")
-        .to_http_request();
-    assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
+    #[test]
+    fn api_dot_fakeyou_dot_com() {
+      let request = TestRequest::get()
+          .uri("https://api.fakeyou.com")
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::FakeYou));
+    }
+
+    #[test]
+    fn storyteller_dot_ai() {
+      let request = TestRequest::get()
+          .uri("https://storyteller.ai")
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
+    }
+
+    #[test]
+    fn api_dot_storyteller_dot_ai() {
+      let request = TestRequest::get()
+          .uri("https://api.storyteller.ai")
+          .to_http_request();
+      assert_eq!(get_request_domain_branding(&request), Some(DomainBranding::Storyteller));
+    }
   }
 }
