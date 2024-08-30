fb92ce810ca8580bba6c85ecf20ed8f6f5f4c2d3
wil <wil@storyteller.ai>
2024-08-29 23:56:06 -0400

    less panels

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -23,7 +23,15 @@ export const Main = withProtectionRoute(() => {
         ref={konaContainerCallbackRef}
         className="col-span-12 col-start-1 row-span-12 row-start-1"
       />
-      <PanelRight>
+      <div className="col-span-3 col-start-10 row-span-1 row-start-1">
+        <div className="flex items-center justify-end gap-4 pr-4 pt-2">
+          <div className="w-1/2">
+            <img src="/brand/Storyteller-Logo.png" alt="Storyteller Logo" />
+          </div>
+          <ProfileDropdown />
+        </div>
+      </div>
+      {/* <PanelRight>
         <div className="flex items-center gap-4">
           <div className="w-1/2">
             <img src="/brand/Storyteller-Logo.png" alt="Storyteller Logo" />
@@ -34,9 +42,11 @@ export const Main = withProtectionRoute(() => {
         <hr className="my-2 border-ui-divider" />
         <p>you are logged in as {userInfo.value?.display_name}</p>
         <button onClick={() => logout()}>Logout</button>
-      </PanelRight>
+      </PanelRight> */}
       <PanelBottom>
-        <p>Panel Bottom</p>
+        <div className="flex h-full items-center justify-center">
+          <p>If you need to put buttons somewhere, this could be the spot.</p>
+        </div>
       </PanelBottom>
     </div>
   );
