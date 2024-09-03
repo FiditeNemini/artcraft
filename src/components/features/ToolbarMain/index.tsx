f40724f6d1a1885a200ac4a1572dac27ccb59933
wil <wil@storyteller.ai>
2024-09-03 15:41:51 -0400

    all icon buttons for main toolbar

--- src/components/features/ToolbarMain/index.tsx
+++ src/components/features/ToolbarMain/index.tsx
@@ -5,8 +5,11 @@ import {
   faBars,
   faDownload,
   faFloppyDisk,
+  faFilePlus,
+  faHand,
   faLocationArrow,
-  faPlusLarge,
+  faPlus,
+  faMinus,
   faSquareDashed,
 } from "@fortawesome/pro-thin-svg-icons";
 
@@ -21,22 +24,32 @@ export const ToolbarMain = () => {
   // } = layout;
   return (
     <div className="col-span-12 col-start-1 row-span-1 row-start-12 justify-center">
-      <div className="m-auto flex w-fit items-center justify-center gap-2 rounded-2xl border border-ui-border bg-ui-panel p-2">
+      <div className="m-auto flex w-fit items-center divide-x divide-ui-border rounded-2xl border border-ui-border bg-ui-panel p-2">
         {/* <p>
           Layout is {windowWidth.value} x {windowHeight.value},{" "}
           <b>{isMobile.value ? "Mobile" : "Not Mobile"}</b>
         </p> */}
-        <ToolbarButtons icon={faBars} />
-        <ToolbarButtons
-          icon={faLocationArrow}
-          iconProps={{ className: "fa-flip-horizontal" }}
-        />
-        <ToolbarButtons icon={faSquareDashed} />
-        <ToolbarButtons icon={faPlusLarge} />
-        <ToolbarButtons icon={faArrowRotateLeft} />
-        <ToolbarButtons icon={faArrowRotateRight} />
-        <ToolbarButtons icon={faFloppyDisk} />
-        <ToolbarButtons icon={faDownload} />
+        <div className="flex items-center gap-2 px-2">
+          <ToolbarButtons icon={faBars} />
+          <ToolbarButtons
+            icon={faLocationArrow}
+            iconProps={{ className: "fa-flip-horizontal" }}
+          />
+          <ToolbarButtons icon={faSquareDashed} />
+          <ToolbarButtons icon={faFilePlus} />
+        </div>
+        <div className="flex items-center gap-2 px-2">
+          <ToolbarButtons icon={faMinus} />
+          <span>100%</span>
+          <ToolbarButtons icon={faPlus} />
+          <ToolbarButtons icon={faHand} />
+        </div>
+        <div className="flex items-center gap-2 px-2">
+          <ToolbarButtons icon={faArrowRotateLeft} />
+          <ToolbarButtons icon={faArrowRotateRight} />
+          <ToolbarButtons icon={faFloppyDisk} />
+          <ToolbarButtons icon={faDownload} />
+        </div>
       </div>
     </div>
   );
