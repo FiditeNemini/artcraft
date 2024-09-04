c1ada22d1a63e6c57993fa201cc588778a72eb5e
wil <wil@storyteller.ai>
2024-09-04 18:09:47 -0400

    change transition style

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,5 +1,4 @@
 export const paperWrapperStyles =
   "rounded-2xl border border-ui-border bg-ui-panel p-2 shadow-xl";
 
-export const transitionTimingStyles =
-  "transition transition-all duration-150 ease-in-out";
+export const transitionTimingStyles = "transition duration-150 ease-in-out";
