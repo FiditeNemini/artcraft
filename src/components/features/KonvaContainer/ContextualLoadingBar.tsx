92a408fcc934651581a15184cb2fdb803d5c995b
wil <wil@storyteller.ai>
2024-09-04 19:34:39 -0400

    hooks for testing rerender efficiency

--- src/components/features/KonvaContainer/ContextualLoadingBar.tsx
+++ src/components/features/KonvaContainer/ContextualLoadingBar.tsx
@@ -3,9 +3,17 @@ import { useSignals } from "@preact/signals-react/runtime";
 import { LoadingBar } from "~/components/ui";
 import { loadingBar } from "~/signals/konvaContextuals";
 
+// import { useSignalRenderCounter } from "~/hooks/useSignalRenderCounter";
+
 export const ContextualLoadingBar = () => {
   useSignals();
-  console.log("ContextualLoadingBar");
+
+  // TODO: Testing the signal render efficiency
+  // useSignalRenderCounter<typeof loadingBar.signal.value>(
+  //   "ContextualLoadingBar",
+  //   loadingBar.signal,
+  // );
+
   const props = loadingBar.signal.value;
 
   return <LoadingBar {...props} />;
--- src/hooks/useRenderCounter.ts
+++ src/hooks/useRenderCounter.ts
@@ -3,8 +3,10 @@ import { useRef } from "react";
 export const useRenderCounter = (componentName: string) => {
   const renderCount = useRef(0);
   renderCount.current++;
-  if (renderCount.current === 1 && import.meta.env.DEV) {
-    console.log(`${componentName} rerendered ${renderCount.current} times`);
+  if (renderCount.current === 1) {
+    if (import.meta.env.DEV) {
+      console.log(`${componentName} rerendered ${renderCount.current} times`);
+    }
   } else {
     console.warn(`${componentName} rerendered ${renderCount.current} times`);
   }
