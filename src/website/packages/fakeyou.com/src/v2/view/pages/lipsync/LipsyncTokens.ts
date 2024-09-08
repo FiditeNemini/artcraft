1a1e3d5a1dff9420a8e850b6e62647e268fd22e1
Brandon Thomas <echelon@gmail.com>
2024-09-08 05:23:11 -0400

    more lipsync templates, redirect beta product

--- src/website/packages/fakeyou.com/src/v2/view/pages/lipsync/LipsyncTokens.ts
+++ src/website/packages/fakeyou.com/src/v2/view/pages/lipsync/LipsyncTokens.ts
@@ -2,5 +2,22 @@
 // Lipsync buttons on TTS results will only show if that voice token is in this map
 
 export const LipsyncTokenMap: { [key: string]: string } = {
-  weight_hz7g8f1j4psrsw2sv67e4y61q: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  // Mariano Closs
+  weight_42axnmna7stqy9h16j5nqef6k: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  weight_fte9rq3em7vv9rqex0f8cvwdx: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  weight_hz7g8f1j4psrsw2sv67e4y61q: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs (most popular)
+  weight_rhfg4chgrp42bnha8kqfrtmcq: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  // Donald Trump
+  weight_ppqs5038bvkm6wc29w0xfebzy: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_x6r5w2tsxgcrrsgweva6dkrqj: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_vrx7j407cxk45jenkrd769h9b: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_59fz13xqa6g43dfwfnbdn56fb: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_jazc270pdr3qe0yer61a5cvh5: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_q24qbmxgrv2s6yka8nwdmw1aq: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_ekwz4cqek5rrdez73rgy843ft: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  // Wednesday Addams (Jenna Ortega)
+  weight_kksrk14y802808as3d6mphedm: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
+  weight_abe03ym9bztnzvx462kkc3fak: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
+  weight_cspd7e4wpjnrv1ygsa19b0gff: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
+  weight_jvdn28ebg186b63hyk6z5kpv7: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
 };
