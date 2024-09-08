ae7293ee6a4393425160ca2a0220bc272ea9e94e
Brandon Thomas <echelon@gmail.com>
2024-09-08 05:42:38 -0400

    more voices

--- src/website/packages/fakeyou.com/src/v2/view/pages/lipsync/LipsyncTokens.ts
+++ src/website/packages/fakeyou.com/src/v2/view/pages/lipsync/LipsyncTokens.ts
@@ -2,22 +2,30 @@
 // Lipsync buttons on TTS results will only show if that voice token is in this map
 
 export const LipsyncTokenMap: { [key: string]: string } = {
-  // Mariano Closs
-  weight_42axnmna7stqy9h16j5nqef6k: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
-  weight_fte9rq3em7vv9rqex0f8cvwdx: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
-  weight_hz7g8f1j4psrsw2sv67e4y61q: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs (most popular)
-  weight_rhfg4chgrp42bnha8kqfrtmcq: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
   // Donald Trump
-  weight_ppqs5038bvkm6wc29w0xfebzy: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  weight_ppqs5038bvkm6wc29w0xfebzy: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump (most popular)
   weight_x6r5w2tsxgcrrsgweva6dkrqj: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
   weight_vrx7j407cxk45jenkrd769h9b: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
   weight_59fz13xqa6g43dfwfnbdn56fb: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
   weight_jazc270pdr3qe0yer61a5cvh5: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
   weight_q24qbmxgrv2s6yka8nwdmw1aq: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
   weight_ekwz4cqek5rrdez73rgy843ft: "m_vrqwr74atwg9btfcbzx6d7ehzbjmsd", // Donald Trump
+  // Mariano Closs
+  weight_hz7g8f1j4psrsw2sv67e4y61q: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs (most popular)
+  weight_42axnmna7stqy9h16j5nqef6k: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  weight_fte9rq3em7vv9rqex0f8cvwdx: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  weight_rhfg4chgrp42bnha8kqfrtmcq: "m_zx1fyp6t7n8b46swz087xkapa7s2vs", // Mariano Closs
+  // Rick Sanchez
+  weight_0f762jdzgsy1dhpb86qxy4ssm: "m_3b2yhzq4na7yg724k9tm4dn0513c36", // Rick Sanchez
+  weight_qfz806d82b0qmxfn3vzgmtyme: "m_3b2yhzq4na7yg724k9tm4dn0513c36", // Rick Sanchez
+  // Walter White
+  weight_0bsxzrwp6p77t67s8dcn607wf: "m_ft3y3vjyg2shssrqq0p51qh143pm0n", // Walter White (most popular)
+  weight_x6vgpe83p2g9pxehhaq4e8823: "m_ft3y3vjyg2shssrqq0p51qh143pm0n", // Walter White
+  weight_5r8ecacq30g9pnm6dvgp30f02: "m_ft3y3vjyg2shssrqq0p51qh143pm0n", // Walter White
+  weight_hect298085wsksre44ykq312t: "m_ft3y3vjyg2shssrqq0p51qh143pm0n", // Walter White
   // Wednesday Addams (Jenna Ortega)
-  weight_kksrk14y802808as3d6mphedm: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
-  weight_abe03ym9bztnzvx462kkc3fak: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
+  weight_kksrk14y802808as3d6mphedm: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega) (most popular)
+  weight_abe03ym9bztnzvx462kkc3fak: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega) "Merlina"
   weight_cspd7e4wpjnrv1ygsa19b0gff: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
   weight_jvdn28ebg186b63hyk6z5kpv7: "m_7ap2qssd4y5ew51dkx4awnng2key32", // Wednesday Addams (Jenna Ortega)
 };
