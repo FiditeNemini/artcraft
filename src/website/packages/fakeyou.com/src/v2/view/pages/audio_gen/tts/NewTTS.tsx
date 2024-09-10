33fb4b065652b6f4f4868a209aa4c2d7a72bc8f5
Brandon Thomas <echelon@gmail.com>
2024-09-09 22:31:52 -0400

    comments about voice tokens

--- src/website/packages/fakeyou.com/src/v2/view/pages/audio_gen/tts/NewTTS.tsx
+++ src/website/packages/fakeyou.com/src/v2/view/pages/audio_gen/tts/NewTTS.tsx
@@ -108,14 +108,14 @@ export default function NewTTS({ sessionSubscriptionsWrapper }: Props) {
   };
 
   const featuredVoiceTokens = [
-    "weight_qdtx8h0mvn945md9tm2zaq422",
-    "weight_f9wz0b1amfxhj1fnxxwtwn3rq",
-    "weight_0f762jdzgsy1dhpb86qxy4ssm",
-    "weight_7jk8mgwkzsycqrxmfw5q4245y",
-    "weight_3k28fws0v6r1ke3p0w0vw48gm",
-    "weight_dqcy6bxq1n79z5ahwpy01bvje",
-    "weight_msq6440ch8hj862nz5y255n8j",
-    "weight_6jvgbqkzschw55qdg7exnx7zx",
+    "weight_qdtx8h0mvn945md9tm2zaq422", // SpongeBob SquarePants (GptSoVits)
+    "weight_f9wz0b1amfxhj1fnxxwtwn3rq", // Zelda (Breath of the Wild) (GptSoVits)
+    "weight_0f762jdzgsy1dhpb86qxy4ssm", // Rick Sanchez (Version 2.0) (TT2)
+    "weight_7jk8mgwkzsycqrxmfw5q4245y", // Homer Simpson (GptSoVits)
+    "weight_3k28fws0v6r1ke3p0w0vw48gm", // Eric Cartman (New) (TT2)
+    "weight_dqcy6bxq1n79z5ahwpy01bvje", // Harry Potter (GptSoVits)
+    "weight_msq6440ch8hj862nz5y255n8j", // IShowSpeed (TT2)
+    "weight_6jvgbqkzschw55qdg7exnx7zx", // Moist Cr1TiKaL (New) (TT2)
   ];
 
   const handleSpeak = async (ev: React.FormEvent<HTMLFormElement>) => {
