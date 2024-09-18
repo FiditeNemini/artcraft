b02faced226d2304f55297e66aee08d7aab2b914
Brandon Thomas <echelon@gmail.com>
2024-09-18 17:58:17 -0400

    ad logging

--- test/worker.js
+++ test/worker.js
@@ -1,7 +1,10 @@
 onconnect = function (event) {
+  console.log('worker loaded');
+
   const port = event.ports[0];
 
   port.onmessage = function (e) {
+    console.log('worker message received');
     const workerResult = `Result: ${e.data[0] * e.data[1]}`;
     port.postMessage(workerResult);
   };
