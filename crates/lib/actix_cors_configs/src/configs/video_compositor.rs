d7155e55d7c0cc126d30cbe85da1d85ee8519c9a
Brandon Thomas <echelon@gmail.com>
2024-09-18 13:17:51 -0400

    cors for image board

--- crates/lib/actix_cors_configs/src/configs/video_compositor.rs
+++ crates/lib/actix_cors_configs/src/configs/video_compositor.rs
@@ -3,9 +3,15 @@ use actix_cors::Cors;
 use crate::util::netlify_branch_domain_matches::netlify_branch_domain_matches;
 
 pub fn add_video_compositor(cors: Cors, _is_production: bool) -> Cors {
-  cors.allowed_origin_fn(|origin, _req_head| {
+  cors
+      // Hypothetical domains
+      .allowed_origin("https://memeboard.ai")
+      .allowed_origin("https://dingboard.ai")
+      // Netlify project
+      .allowed_origin_fn(|origin, _req_head| {
         netlify_branch_domain_matches(origin, "storyteller-board.netlify.app")
       })
+      // Local development
       .allowed_origin("http://localhost:5173/")
 }
 
@@ -16,6 +22,22 @@ mod tests {
   use crate::cors::build_cors_config;
   use crate::testing::assert_origin_invalid;
   use crate::testing::assert_origin_ok;
+
+  mod domains {
+    use super::*;
+
+    #[actix_rt::test]
+    async fn memeboard() {
+      let production_cors = build_cors_config(ServerEnvironment::Production);
+      assert_origin_ok(&production_cors, "https://memeboard.ai").await;
+    }
+
+    #[actix_rt::test]
+    async fn dingboard() {
+      let production_cors = build_cors_config(ServerEnvironment::Production);
+      assert_origin_ok(&production_cors, "https://dingboard.ai").await;
+    }
+  }
 
   mod netlify {
     use super::*;
