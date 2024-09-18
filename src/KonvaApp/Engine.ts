816a087b6b726fdd617c1391d42a87175a0d0bbd
Michael Chung <6252325+ArEnSc@users.noreply.github.com>
2024-09-17 23:43:40 -0400

    Remove hack

--- src/KonvaApp/Engine.ts
+++ src/KonvaApp/Engine.ts
@@ -195,20 +195,15 @@ export class Engine {
       console.log("Engine heard AI Stylize request: ", data);
       //console.log(data);
       // Hack to ensure this doesn't break because UI BUG
-
-      if (this.ranOnce === true) {
-        try {
-          await this.renderEngine.startProcessing(data);
-        } catch (error) {
-          // throw error to retry
-          uiAccess.dialogueError.show({
-            title: "Generation Error",
-            message: error.toString(),
-          });
-        }
+      try {
+        await this.renderEngine.startProcessing(data);
+      } catch (error) {
+        // throw error to retry
+        uiAccess.dialogueError.show({
+          title: "Generation Error",
+          message: error.toString(),
+        });
       }
-
-      this.ranOnce = true;
     });
 
     // TODO: You may listen to all the image toolbar events here
