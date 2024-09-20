32c50cb3c6974f5d3fa7f27c7e074fb325888349
Brandon Thomas <echelon@gmail.com>
2024-09-19 23:38:04 -0400

    Google Sign In support (WIP)

---
+++ crates/lib/api_clients/google_sign_in/src/lib.rs
@@ -0,0 +1,22 @@
+//! google_sign_in
+//!
+//! The purpose of this library is to make single sign on (SSO) for Google easy to use.
+//!
+
+// Never allow these
+#![forbid(private_bounds)]
+#![forbid(private_interfaces)]
+#![forbid(unused_must_use)] // NB: It's unsafe to not close/check some things
+
+// Okay to toggle
+#![forbid(unreachable_patterns)]
+//#![forbid(unused_imports)]
+#![forbid(unused_mut)]
+//#![forbid(unused_variables)]
+
+// Always allow
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
+pub mod decode_token;
+pub mod jwk_to_public_key;
