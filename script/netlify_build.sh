d73d2a7ef3f91087d744a0d498dadc8cbda1653f
Brandon Thomas <echelon@gmail.com>
2024-10-01 19:14:23 -0400

    add a 404 page and a redirect for /login

--- script/netlify_build.sh
+++ script/netlify_build.sh
@@ -8,8 +8,12 @@ yarn run build2
 echo "Copy test files into dist"
 cp -r test/* dist/
 
-echo "Copy netlify headers config into dist"
+echo "Copy netlify configs into dist"
 cp _headers dist/
+cp _redirects dist/
+
+echo "Copy netlify 404.html page into dist"
+cp "404.html" dist/
 
 echo "List files after build"
 find dist/
