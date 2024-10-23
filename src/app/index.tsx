877f9f37bf12040fc77e0f59b4304778a00459c5
wil <wil@storyteller.ai>
2024-10-23 12:25:33 -0400

    added posthog (#57)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,34 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { posthog } from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 import "./global.css";
 import { useRenderCounter } from "~/hooks/useRenderCounter";
+
+function PostHogInit() {
+  const apiKey = "phc_jBFgac0mVALAFk3negnSfYCcHgvkT00yBLQDmCDYNBb";
+  posthog.init(apiKey, {
+    api_host: "https://us.i.posthog.com/",
+    ui_host: "https://us.i.posthog.com/",
+  });
+}
+
 const App = () => {
   const useStrictMode = false;
   useRenderCounter("App");
-
+  useEffect(() => {
+    PostHogInit();
+  }, []);
   if (useStrictMode) {
     return (
       <StrictMode>
-        <RouterProvider router={router} />
+        <PostHogProvider client={posthog}>
+          <RouterProvider router={router} />
+        </PostHogProvider>
       </StrictMode>
     );
   }
