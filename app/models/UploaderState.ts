503b739448831583e9588bb30cf97ef22945b2d3
Tanish Manku <tanish2k09.dev@gmail.com>
2025-01-09 16:04:40 -0800

    Add data field to uploader state
    Will be used upon success possibly for token storage

--- app/models/UploaderState.ts
+++ app/models/UploaderState.ts
@@ -3,6 +3,7 @@ import { UploaderStates } from "~/enums";
 export interface UploaderState {
   status: UploaderStates;
   errorMessage?: string;
+  data?: unknown;
 }
 
 export const initialUploaderState = {
