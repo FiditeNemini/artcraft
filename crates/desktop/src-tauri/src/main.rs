494e1e6b2b9e29a227de5df16164426db8a95386
Brandon Thomas <echelon@gmail.com>
2025-03-03 15:35:05 -0500

    release build profile, stop writing debug file at app startup

--- crates/desktop/src-tauri/src/main.rs
+++ crates/desktop/src-tauri/src/main.rs
@@ -4,12 +4,12 @@
 use std::fs::File;
 
 fn main() {
-  // NB: This is a test to see the behavior of the application with dynamic loading modules.
-  // We should be able to execute code before loading CUDA and other dependencies.
-  let r = File::create("program_started.txt");
-  match r {
-    Err(err) => println!("Failed to create program_started.txt: {}", err),
-    Ok(_) => {}
-  }
+  //// NB: This is a test to see the behavior of the application with dynamic loading modules.
+  //// We should be able to execute code before loading CUDA and other dependencies.
+  //let r = File::create("program_started.txt");
+  //match r {
+  //  Err(err) => println!("Failed to create program_started.txt: {}", err),
+  //  Ok(_) => {}
+  //}
   app_lib::run();
 }
