448c760339291b9a3b0fc829d43c9124369c0fd2
Brandon Thomas <echelon@gmail.com>
2025-04-07 16:35:00 -0400

    fix login

--- crates/desktop/tauri-4o/src/commands/open_login_command.rs
+++ crates/desktop/tauri-4o/src/commands/open_login_command.rs
@@ -1,5 +1,13 @@
 use log::{error, info};
-use tauri::{AppHandle, WindowBuilder};
+use once_cell::sync::Lazy;
+use tauri::{AppHandle, Manager, Url, WebviewUrl, WebviewWindowBuilder, WindowBuilder};
+use errors::AnyhowResult;
+
+const LOGIN_URL_STR : &str = "https://auth.openai.com/log-in";
+
+static LOGIN_URL: Lazy<Url> = Lazy::new(|| {
+  Url::parse("https://auth.openai.com/log-in").expect("URL should parse")
+});
 
 #[tauri::command]
 pub async fn open_login_command(
@@ -7,35 +15,39 @@ pub async fn open_login_command(
 ) -> Result<String, String> {
   info!("open_login_command called");
 
-  let _window_builder = WindowBuilder::new(&app, "login")
-      .always_on_top(true)
-      .title("Login")
-      .resizable(true)
-      .visible(true)
-      .build()
-      .map_err(|err| {
-        error!("Failed to create window: {}", err);
-        format!("Failed to create window: {}", err)
-      })?;
-
-
-  /*app.create_tao_window(move || {
-    let window = tauri::window::WindowBuilder::from_config(&app, &app.config().app.windows.get(0).unwrap().clone())
-
-    ("WGPU Target".to_string(), window_builder)
-  });*/
-/*    "login",
-    tauri::WindowUrl::App("https://google.com".into()),
-    tauri::WindowBuilder::new(
-      &app,
-      "login",
-      tauri::WindowUrl::App("https://google.com".into()),
-    )
-    .title("Login")
-    .inner_size(800.0, 600.0)
-    .decorations(true)
-    .resizable(true)
-  ).map_err(|err| format!("Failed to create window: {}", err))?;*/
+  do_open_login(&app)
+    .await
+    .map_err(|err| {
+      error!("Error opening login: {:?}", err);
+      format!("Error opening login: {:?}", err)
+    })?;
 
   Ok("result".to_string())
+}
+
+async fn do_open_login(app: &AppHandle) -> AnyhowResult<()> {
+  info!("Building login window...");
+
+  let url = WebviewUrl::External(LOGIN_URL.clone());
+
+  //let window = WindowBuilder::new(app, "login")
+  let window = WebviewWindowBuilder::new(app, "login", url)
+      .always_on_top(true)
+      .title("Login to OpenAI")
+      .center()
+      .resizable(true)
+      .visible(true)
+      .closable(true)
+      .focused(true)
+      .build()?;
+
+  //info!("Iterating webviews...");
+  //for (name, webview) in window.webviews() {
+  //  info!("Webview URL: {:?}", webview.url());
+  //  webview.navigate(LOGIN_URL.clone())?;
+  //  webview.eval(&format!("window.location.replace('{}');", LOGIN_URL_STR))?;
+  //}
+
+  info!("Done.");
+  Ok(())
 }
