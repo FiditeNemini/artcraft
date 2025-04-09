241a9d3031516595f811316973b8e7934dc9b7f7
Brandon Thomas <echelon@gmail.com>
2025-04-09 15:37:00 -0400

    log details

--- crates/service/job/inference_job/src/job/job_types/image_generation/process_single_ig_job.rs
+++ crates/service/job/inference_job/src/job/job_types/image_generation/process_single_ig_job.rs
@@ -1,3 +1,4 @@
+use log::info;
 use enums::by_table::generic_inference_jobs::inference_job_type::InferenceJobType;
 use enums::by_table::generic_inference_jobs::inference_model_type::InferenceModelType;
 use errors::anyhow;
@@ -11,15 +12,31 @@ use crate::job::job_types::image_generation::v2::process_single_sora_job::{proce
 use crate::state::job_dependencies::JobDependencies;
 
 pub async fn process_single_ig_job(job_dependencies: &JobDependencies, job: &AvailableInferenceJob) -> Result<JobSuccessResult, ProcessSingleJobError> {
+  info!(
+    "Processing single image generation job. \
+    \n - Job Token: {:?}\
+    \n - Job Type: {:?}\
+    \n - Inference Category: {:?}\
+    \n - Maybe Model Type: {:?}",
+    job.inference_job_token,
+    job.job_type,
+    job.inference_category,
+    job.maybe_model_type
+  );
+
   match job.job_type {
     InferenceJobType::ImageGenApi => {
+      info!("Processing image job as ImageGenApi");
       let args = ImageGenerationProcessArgs { job_dependencies, job };
       process_single_sora_job(&args).await
     },
-    _ => match job.maybe_model_type {
-      Some(InferenceModelType::StableDiffusion) => dispatch_sd_job(job_dependencies, job).await,
-      Some(other_model_type) => Err(ProcessSingleJobError::Other(anyhow!("Wrong model type for SD: {:?}", other_model_type))),
-      None => Err(ProcessSingleJobError::Other(anyhow!("SD model type not set"))),
+    _ => {
+      info!("Processing image job as **NOT** ImageGenApi. Type: {:?}", job.job_type);
+      match job.maybe_model_type {
+        Some(InferenceModelType::StableDiffusion) => dispatch_sd_job(job_dependencies, job).await,
+        Some(other_model_type) => Err(ProcessSingleJobError::Other(anyhow!("Wrong model type for SD: {:?}", other_model_type))),
+        None => Err(ProcessSingleJobError::Other(anyhow!("SD model type not set"))),
+      }
     },
   }
 }
