3d964c83c0821d95283e5ea914b79d07658ea2c9
Brandon Thomas <echelon@gmail.com>
2025-04-11 08:56:18 -0400

    test builds

--- frontend/apps/editor2d/script/netlify_build.sh
+++ frontend/apps/editor2d/script/netlify_build.sh
@@ -5,30 +5,16 @@ set -euxo pipefail
 echo "Run build script (TODO: Make strict)"
 nx build editor2d
 
-echo "Make dist dir"
-mkdir dist/
-
-#echo "Copy test files into dist"
-#cp -r test/* dist/
+echo "Change to project dir"
+pushd apps/editor2d/
 
 echo "Copy netlify configs into dist"
-cp apps/editor2d/_headers dist/
-cp apps/editor2d/_redirects dist/
+cp _headers dist/
+cp _redirects dist/
 
 echo "Copy netlify 404.html page into dist"
-cp apps/editor2d/404.html dist/
+cp "404.html" dist/
 
 echo "List files after build"
 find dist/
-
-echo "List other files(1)"
-ls -lA /
-
-echo "List other files(2)"
-pwd
-ls -lA .
-
-echo "Find"
-pwd
-find . | grep build
 
