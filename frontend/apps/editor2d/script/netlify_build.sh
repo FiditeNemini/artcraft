765b03c600f18647224b4fb504f50084667afacb
Brandon Thomas <echelon@gmail.com>
2025-04-11 05:28:44 -0400

    don't copy tests

--- frontend/apps/editor2d/script/netlify_build.sh
+++ frontend/apps/editor2d/script/netlify_build.sh
@@ -5,8 +5,8 @@ set -euxo pipefail
 echo "Run build script (TODO: Make strict)"
 nx build editor2d
 
-echo "Copy test files into dist"
-cp -r test/* dist/
+#echo "Copy test files into dist"
+#cp -r test/* dist/
 
 echo "Copy netlify configs into dist"
 cp _headers dist/
