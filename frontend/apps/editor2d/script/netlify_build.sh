f059e74b2633d9032420d842c0f7228614a1df01
Brandon Thomas <echelon@gmail.com>
2025-04-11 05:33:40 -0400

    copy headers

--- frontend/apps/editor2d/script/netlify_build.sh
+++ frontend/apps/editor2d/script/netlify_build.sh
@@ -9,7 +9,7 @@ nx build editor2d
 #cp -r test/* dist/
 
 echo "Copy netlify configs into dist"
-cp _headers dist/
+cp apps/editor2d/_headers dist/
 cp _redirects dist/
 
 echo "Copy netlify 404.html page into dist"
