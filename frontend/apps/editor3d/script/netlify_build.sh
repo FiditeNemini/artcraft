289ef6f352e4a24bd243ee8f294f102f6e366ae5
Brandon Thomas <echelon@gmail.com>
2025-04-13 21:01:09 -0400

    debug branch build (#507)

--- frontend/apps/editor3d/script/netlify_build.sh
+++ frontend/apps/editor3d/script/netlify_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euxo pipefail 
+#set -euxo pipefail 
 
 #   echo "Run build script"
 #   npm install
@@ -23,27 +23,74 @@ set -euxo pipefail
 #   # echo "List files after build"
 #   # find dist/
 
+#printf "==================================="
+#echo "List Env"
+#env
+
+printf "==================================="
 echo "Run build script (TODO: Make strict)"
 nx build editor3d
 
-echo "Change to project dir"
-pushd apps/editor3d/
+printf "==================================="
+echo "Current dir (1)"
+pwd
 
-echo "List directory files"
+printf "==================================="
+echo "List directory files (1)"
 pwd
 ls -lA .
 
-echo "List build directory files"
+printf "==================================="
+echo "List directory files (2)"
+#find / | grep build/server
+find / 2> /dev/null | grep build/server
+
+
+printf "==================================="
+echo "LIST DIRECTORY FILES (3)"
+#find / | grep build/server
+find / 2> /dev/null | grep EnvironmentVar
+find / 2> /dev/null | grep PageEnigma
+
+
+printf "==================================="
+echo "Find build manifest"
+find . | grep manifest.json
+
+printf "==================================="
+echo "Change to project dir"
+pushd apps/editor3d/
+
+printf "==================================="
+echo "Current dir (2)"
 pwd
+
+printf "==================================="
+echo "List directory files (4)"
+pwd
+ls -lA .
+
+printf "==================================="
+echo "List build directory files"
 ls -lA build/
 
+mkdir dist/
+
+printf "==================================="
+echo "Copy files"
+cp -r build/server/* dist/
+cp -r build/client/* dist/
+
 #echo "Copy netlify configs into dist"
-#cp _headers dist/
+cp _headers dist/
 #cp _redirects dist/
 
 #echo "Copy netlify 404.html page into dist"
 #cp "404.html" dist/
 
+
+printf "==================================="
 echo "List files after build"
 find dist/
+
 
