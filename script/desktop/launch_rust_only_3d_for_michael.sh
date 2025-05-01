a1c2b4f5c871c604f39155d5b89f4e66f9a839b7
Brandon Thomas <echelon@gmail.com>
2025-05-01 15:17:21 -0400

    revert script, add copy for michael

--- script/desktop/launch_rust_only_3d_for_michael.sh
+++ script/desktop/launch_rust_only_3d_for_michael.sh
@@ -2,6 +2,14 @@
 
 set -euxo pipefail
 
+# Kill any process running on port 5741, which will block startup
+if lsof -i tcp:5173 &>/dev/null; then
+  lsof -i tcp:5173 -t | xargs kill -9
+  echo "Killed process running on port 5173"
+else
+  echo "No process running on port 5173"
+fi
+
 root_dir=$(pwd)
 frontend_path="${root_dir}/frontend"
 rust_crate_path="${root_dir}/crates/desktop/tauri-artcraft"
@@ -15,7 +23,8 @@ config_path="${rust_crate_path}/tauri.artcraft_3d.no_dev.conf.toml"
 export TAURI_FRONTEND_PATH="${frontend_path}"
 export TAURI_APP_PATH="${rust_crate_path}"
 
-cargo tauri dev --no-dev-server \
+
+RUSTFLAGS="-Awarnings" cargo tauri dev --no-dev-server \
   --no-dev-server-wait \
   --no-watch \
   --config "${config_path}"
