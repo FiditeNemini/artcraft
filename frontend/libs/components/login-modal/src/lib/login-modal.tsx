0d33358eadee27b633069614461fa6c291555cbb
BFlat <bombayapirat@gmail.com>
2025-05-07 16:18:45 +0700

    upload flow update (#587)
    - changed and moved the main upload asset button to the left sidebar
    above the categories for better visibility
    - added individual upload buttons to all the category pages except
    skybox - they open the same upload modal but sepcific to its category
    only.
    - flow update: once an upload succeeds the upload modal closes itself
    and automatically reopens the asset modal to the mine tab and previously
    uploaded category (so users don't have to have a hard time finding or
    navigating to it)

--- frontend/libs/components/login-modal/src/lib/login-modal.tsx
+++ frontend/libs/components/login-modal/src/lib/login-modal.tsx
@@ -211,7 +211,7 @@ export function LoginModal({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 cursor-pointer bg-black/60" />
+          <div className="fixed inset-0 cursor-pointer bg-black/80" />
         </TransitionChild>
         <div className="fixed inset-0 flex items-center justify-center p-4">
           <TransitionChild
