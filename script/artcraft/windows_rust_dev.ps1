9dd16f2e80bb424b4abf7f4706289df67983357e
Brandon Thomas <echelon@gmail.com>
2025-05-07 14:42:40 -0400

    windows production build script

--- script/artcraft/windows_rust_dev.ps1
+++ script/artcraft/windows_rust_dev.ps1
@@ -5,7 +5,9 @@ Write-Host ""
 Write-Host "You'll need to launch the frontend dev server as a second script!"  -ForegroundColor red -BackgroundColor white
 Write-Host ""
 
+# This tells Tauri *which* frontend and *which* Rust app to use since we're in a monorepo with several apps.
 $env:TAURI_FRONTEND_PATH=".\frontend"
 $env:TAURI_APP_PATH=".\crates\desktop\artcraft"
 
+# The config file tells Tauri more instructions for the frontend build.
 cargo tauri dev --config ".\crates\desktop\artcraft\tauri.artcraft_3d.no_dev.conf.json"
