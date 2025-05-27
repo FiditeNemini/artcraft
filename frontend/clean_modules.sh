569622bcc08d9ba80dbdd93335109e6e4c627eab
Brandon Thomas <echelon@gmail.com>
2025-05-27 16:52:12 -0400

    make frontend/clean_modules.sh take the nuclear option

--- frontend/clean_modules.sh
+++ frontend/clean_modules.sh
@@ -2,9 +2,58 @@
 
 set -euxo pipefail
 
-echo "Removing Node Modules..."
+echo "Resetting NX..."
 npx nx reset
+
+echo "Removing NX cache directory..."
+rm -rf ./.nx/
+
+echo "Removing Node Modules directories..."
 rm -rf ./node_modules/
 rm -rf ./apps/editor2d/node_modules/
 rm -rf ./apps/editor3d/node_modules/
+
+echo "Removing Build directories..."
+rm -rf ./apps/editor2d/dist/
+rm -rf ./apps/editor3d/dist/
+rm -rf ./libs/api/dist/
+rm -rf ./libs/common/dist/
+rm -rf ./libs/components/badge/dist/
+rm -rf ./libs/components/button-dropdown/dist/
+rm -rf ./libs/components/button-icon-select/dist/
+rm -rf ./libs/components/button-icon/dist/
+rm -rf ./libs/components/button-link/dist/
+rm -rf ./libs/components/button/dist/
+rm -rf ./libs/components/camera-settings-modal/dist/
+rm -rf ./libs/components/close-button/dist/
+rm -rf ./libs/components/demo-modal/dist/
+rm -rf ./libs/components/file-uploader/dist/
+rm -rf ./libs/components/gallery-modal/dist/
+rm -rf ./libs/components/gravatar/dist/
+rm -rf ./libs/components/input/dist/
+rm -rf ./libs/components/label/dist/
+rm -rf ./libs/components/lightbox-modal/dist/
+rm -rf ./libs/components/list-dropdown/dist/
+rm -rf ./libs/components/loading-spinner/dist/
+rm -rf ./libs/components/loading/dist/
+rm -rf ./libs/components/login-modal/dist/
+rm -rf ./libs/components/menu-icon-selector/dist/
+rm -rf ./libs/components/modal/dist/
+rm -rf ./libs/components/pagination/dist/
+rm -rf ./libs/components/popover/dist/
+rm -rf ./libs/components/promptbox/dist/
+rm -rf ./libs/components/search/dist/
+rm -rf ./libs/components/select/dist/
+rm -rf ./libs/components/settings-modal/dist/
+rm -rf ./libs/components/slider-v2/dist/
+rm -rf ./libs/components/switch/dist/
+rm -rf ./libs/components/tab-selector/dist/
+rm -rf ./libs/components/toaster/dist/
+rm -rf ./libs/components/tooltip/dist/
+rm -rf ./libs/components/transition-dialogue/dist/
+rm -rf ./libs/components/upload-modal/dist/
+rm -rf ./libs/login/dist/
+rm -rf ./libs/soundboard/dist/
+rm -rf ./libs/tauri-api/dist/
+rm -rf ./libs/tauri-utils/dist/
 
