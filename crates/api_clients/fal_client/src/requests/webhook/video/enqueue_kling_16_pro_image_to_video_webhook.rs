aa03618d6cde821a3df89670aa9a07f1d3e01ae9
Brandon Thomas <echelon@gmail.com>
2025-08-26 19:13:15 -0400

    video end frame support (#791)

--- crates/api_clients/fal_client/src/requests/webhook/video/enqueue_kling_16_pro_image_to_video_webhook.rs
+++ crates/api_clients/fal_client/src/requests/webhook/video/enqueue_kling_16_pro_image_to_video_webhook.rs
@@ -5,8 +5,9 @@ use fal::endpoints::fal_ai::kling_video::v1_6::pro::image_to_video::{image_to_vi
 use fal::webhook::WebhookResponse;
 use reqwest::IntoUrl;
 
-pub struct Kling16ProArgs<'a, U: IntoUrl, V: IntoUrl> {
+pub struct Kling16ProArgs<'a, U: IntoUrl, T: IntoUrl, V: IntoUrl> {
   pub image_url: U,
+  pub end_frame_image_url: Option<T>,
   pub webhook_url: V,
   pub prompt: &'a str,
   pub api_key: &'a FalApiKey,
@@ -28,8 +29,8 @@ pub enum Kling16ProAspectRatio {
   TallNineSixteen, // 9:16
 }
 
-pub async fn enqueue_kling_16_pro_image_to_video_webhook<U: IntoUrl, V: IntoUrl>(
-  args: Kling16ProArgs<'_, U, V>
+pub async fn enqueue_kling_16_pro_image_to_video_webhook<U: IntoUrl, T: IntoUrl, V: IntoUrl>(
+  args: Kling16ProArgs<'_, U, T, V>
 ) -> Result<WebhookResponse, FalErrorPlus> {
   let duration = match args.duration {
     Kling16Duration::Default => None,
@@ -45,15 +46,18 @@ pub async fn enqueue_kling_16_pro_image_to_video_webhook<U: IntoUrl, V: IntoUrl>
 
   let image_url = args.image_url.as_str().to_string();
 
+  let tail_image_url = args.end_frame_image_url
+      .map(|url| url.as_str().to_string());
+
   let request = ProImageToVideoRequest {
     image_url,
+    tail_image_url,
     prompt: args.prompt.to_string(),
     aspect_ratio,
     duration,
     // Maybe expose these later
     cfg_scale: None,
     negative_prompt: None,
-    tail_image_url: None,
   };
 
   let result = image_to_video(request)
@@ -64,35 +68,34 @@ pub async fn enqueue_kling_16_pro_image_to_video_webhook<U: IntoUrl, V: IntoUrl>
   result.map_err(|err| classify_fal_error(err))
 }
 
-/*
 #[cfg(test)]
 mod tests {
   use crate::creds::fal_api_key::FalApiKey;
-  use crate::requests::webhook::video::enqueue_kling_16_image_to_video_webhook::{enqueue_kling_16_image_to_video_webhook, Kling16Args, Kling16Duration};
+  use crate::requests::webhook::video::enqueue_kling_16_pro_image_to_video_webhook::{enqueue_kling_16_pro_image_to_video_webhook, Kling16Duration, Kling16ProArgs, Kling16ProAspectRatio};
   use errors::AnyhowResult;
   use std::fs::read_to_string;
-  use testing::test_file_path::test_file_path;
+  use test_data::web::image_urls::{JUNO_AT_LAKE_IMAGE_URL, TALL_MOCHI_WITH_GLASSES_IMAGE_URL};
 
   #[tokio::test]
   #[ignore]
-  async fn test_kling16_video() -> AnyhowResult<()> {
-    let image = test_file_path("test_data/image/juno.jpg")?;
-
+  async fn test() -> AnyhowResult<()> {
     // XXX: Don't commit secrets!
     let secret = read_to_string("/Users/bt/Artcraft/credentials/fal_api_key.txt")?;
 
     let api_key = FalApiKey::from_str(&secret);
 
-    let args = Kling16Args {
-      image_path: image,
-      prompt: "a corgi looks out over the water",
+    let args = Kling16ProArgs {
+      image_url: TALL_MOCHI_WITH_GLASSES_IMAGE_URL,
+      end_frame_image_url: Some(JUNO_AT_LAKE_IMAGE_URL.to_string()),
+      prompt: "shiba in glasses runs to the lake and stands by the shore",
       api_key: &api_key,
       duration: Kling16Duration::Default,
+      aspect_ratio: Kling16ProAspectRatio::WideSixteenNine,
+      webhook_url: "https://example.com/webhook",
     };
 
-    let result = enqueue_kling_16_image_to_video_webhook(args).await?;
+    let result = enqueue_kling_16_pro_image_to_video_webhook(args).await?;
 
     Ok(())
   }
 }
-*/
--- crates/desktop/artcraft/src/core/commands/enqueue/image_to_video/generic/handle_video_artcraft.rs
+++ crates/desktop/artcraft/src/core/commands/enqueue/image_to_video/generic/handle_video_artcraft.rs
@@ -76,6 +76,7 @@ pub async fn handle_video_artcraft(
       let request = GenerateKling16ProImageToVideoRequest { 
         uuid_idempotency_token,
         media_file_token: request.image_media_token,
+        end_frame_image_media_token: request.end_frame_image_media_token,
         aspect_ratio: Some(GenerateKling16ProAspectRatio::WideSixteenNine),
         prompt: request.prompt,
         duration: None,
@@ -128,6 +129,7 @@ pub async fn handle_video_artcraft(
       let request = GenerateKling21ProImageToVideoRequest {
         uuid_idempotency_token,
         media_file_token: request.image_media_token,
+        end_frame_image_media_token: request.end_frame_image_media_token,
         aspect_ratio: Some(GenerateKling21ProAspectRatio::WideSixteenNine),
         prompt: request.prompt,
         duration: None,
@@ -154,6 +156,7 @@ pub async fn handle_video_artcraft(
       let request = GenerateSeedance10LiteImageToVideoRequest {
         uuid_idempotency_token,
         media_file_token: request.image_media_token,
+        end_frame_image_media_token: request.end_frame_image_media_token,
         prompt: request.prompt,
         resolution: None,
         duration: None,
