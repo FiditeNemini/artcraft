517abc046268eddd901c0de7ddab21f892e2c338
Brandon Thomas <echelon@gmail.com>
2025-09-23 23:07:35 -0400

    server route cleanup (#840)

---
+++ crates/service/web/storyteller_web/src/http_server/routes/legacy_routes/mod.rs
@@ -0,0 +1,8 @@
+pub (super) mod add_legacy_routes;
+mod beta_key_routes;
+mod control_plane_sora_routes;
+mod image_studio_routes;
+mod model_download_routes;
+mod studio_gen2_routes;
+mod workflow_routes;
+mod desktop_vc_app_routes;
