f435e9fb7e19bf9b7586022d2330cad298ad2057
Brandon Thomas <echelon@gmail.com>
2026-01-24 09:31:53 -0500

    cleanup dead shared_service_components crate

--- crates/lib/shared_service_components/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! This library contains code shared between `storyteller-web` and jobs.
-
-pub mod sora_redis_credentials;
-pub mod sora_redis_sentinel_refresh;
--- crates/lib/shared_service_components/src/sora_redis_credentials/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod keys;
-pub mod set_sora_credential_field_in_redis;
-pub mod get_sora_credentials_from_redis;
